9b194c63989bfc4456366761d14ec127c6b399e8
Carlos Hernandez <c-hernandez8@ti.com>
2010-06-16 17:01:27 -0400

    fix issue in dev_test.rb script

---
+++ LSP/TARGET/dev_test.rb
@@ -0,0 +1,51 @@
+require File.dirname(__FILE__)+'/../default_target_test'
+
+include LspTargetTestScript
+
+# Generate Linux shell script to be executed at DUT.
+# This function used the shell_script software asset, replace any ruby code and/or
+# test parameter references and creates test.sh  
+def run_generate_script
+  puts "\n LinuxTestScript::run_generate_script"
+  FileUtils.mkdir_p SiteInfo::LINUX_TEMP_FOLDER
+  in_file = File.new(File.join(@test_params.shell_script), 'r')
+  raw_test_lines = in_file.readlines
+  out_file = File.new(File.join(SiteInfo::LINUX_TEMP_FOLDER, 'test.sh'),'w')
+  #out_file.puts("#!/bin/bash \n")
+  out_file.puts("failtest() {")
+  out_file.puts("  echo 1 >&3")
+  out_file.puts("}")
+  raw_test_lines.each do |current_line|
+    out_file.puts(eval('"'+current_line.gsub("\\","\\\\\\\\").gsub('"','\\"')+'"'))
+  end
+  
+  
+  in_file.close
+  out_file.close
+end
+
+# Calls shell script (test.sh)
+def run_call_script
+  puts "\n LinuxTestScript::run_call_script"
+  @equipment['dut1'].send_cmd("cd #{@linux_dst_dir}",@equipment['dut1'].prompt)
+  @equipment['dut1'].send_cmd("chmod +x test.sh",@equipment['dut1'].prompt)
+  @equipment['dut1'].send_cmd("./test.sh 2> stderr.log > stdout.log 3> result.log",@equipment['dut1'].prompt)
+end
+
+# Determine test result outcome by checking if failtest() function was called or 
+# the script returned and error code
+def run_determine_test_outcome
+  puts "\n LinuxTestScript::run_determine_test_outcome"
+  @equipment['dut1'].send_cmd("echo $?",/^0$/m, 2)
+  returncode_check = @equipment['dut1'].timeout?
+  @equipment['dut1'].send_cmd("cat result.log",/^1$/m, 2)
+  failtest_check = !@equipment['dut1'].timeout?
+  
+  if returncode_check
+    return [FrameworkConstants::Result[:fail], "The shell script returned non-zero value"]
+  elsif failtest_check
+    return [FrameworkConstants::Result[:fail], "The shell script called failtest()"]
+  else
+    return [FrameworkConstants::Result[:pass], "Shell script returned 0 and did not call failtest()"]
+  end
+end
