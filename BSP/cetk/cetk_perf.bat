38a88655fa99e3a611eac63854519855e6ae56d7
Yan Liu <yan-liu@ti.com>
2010-11-10 17:55:20 -0500

    Fix test bat files to check if kernel is specified

--- BSP/cetk/cetk_perf.bat
+++ BSP/cetk/cetk_perf.bat
@@ -1,11 +1,7 @@
 @ECHO off
 echo "Testing EVM with IP:  #{@equipment['dut1'].telnet_ip}"
-echo "Testing Kernel image: #{@test_params.kernel}"
+echo "Testing Kernel image: #{@test_params.instance_variable_defined?(:@kernel) ? @test_params.kernel.to_s : ''}"
 echo "Command Line: #{@test_params.params_chan.cmdline[0]}"
 mkdir \Release
 #{@test_params.params_chan.cmdline[0]}
-
-
-
-
 
