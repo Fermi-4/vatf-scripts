cc01f972bccc6c9fdab5f5c879c7c52a51d8cd38
Elvita Lobo <elvita@ti.com>
2011-05-04 13:53:15 -0400

    LinuxHiPerfDSP: cleanup default_test_module

--- LinuxHiPerfDSP/boot/c6x_default_test_module.rb
+++ LinuxHiPerfDSP/boot/c6x_default_test_module.rb
@@ -23,15 +23,15 @@ module C6xTestScript
     
     def setup
       @equipment['dut1'].set_api('linux-c6x')
-      tester_from_cli  = @tester.downcase
-      target_from_db   = @test_params.target.downcase
       platform_from_db = @test_params.platform.downcase    
       nfs  = @test_params.nfs     if @test_params.instance_variable_defined?(:@nfs)
       bootblob = @test_params.var_bootblob     if @test_params.instance_variable_defined?(:@var_bootblob)
       bootblob_util =  @test_params.bootblob_util     if @test_params.instance_variable_defined?(:@bootblob_util)
       testdriver = @test_params.testdriver     if @test_params.instance_variable_defined?(:@testdriver)
-      kernel = @test_params.kernel     if @test_params.instance_variable_defined?(:@kernel)
-      kernel_name = @equipment['dut1'].params["kernel"] if defined? @equipment['dut1'].params["kernel"]
+      if @test_params.instance_variable_defined?(:@kernel)
+        kernel = @test_params.kernel     
+        kernel_name = @equipment['dut1'].params["kernel"] 
+      end
       nfs_root_path = @equipment['dut1'].nfs_root_path
       nfs_root_path_temp 	= nfs_root_path
       if @equipment.has_key?('server1')
@@ -144,54 +144,18 @@ module C6xTestScript
       
       # by now, the dut should already login and is up; if not, dut may hang.
       raise "UUT may be hanging!" if !is_uut_up?
-      # Copy executables to NFS server (if filesystem was not specified and there are @target_sources
-      if @test_params.params_chan.instance_variable_defined?(:@target_binaries) && @equipment.has_key?('server1') #&& !nfs && !@test_params.instance_variable_defined?(:@var_nfs) 
-          files_array = Array.new
-          src = @test_params.params_chan.target_binaries[0].to_s
-          debug_puts "target source folder is: #{src}"
-          @equipment['server1'].send_cmd("cd #{nfs_root_path_temp}/opt", @equipment['server1'].prompt, 30)
-          testbins = File.basename("#{@test_params.params_chan.target_binaries[0]}").match(/(\w+)\.tar\.gz/).captures[0]
-          if (File.exists?"#{samba_root_path_temp}\\opt\\#{testbins}")
-            @equipment['server1'].send_sudo_cmd("rm -rf #{testbins}")
-          end
-          @equipment['server1'].send_sudo_cmd("mkdir #{testbins}", @equipment['server1'].prompt, 30)
-          @equipment['server1'].send_sudo_cmd("chmod 777 #{testbins}", @equipment['server1'].prompt, 30)
-          @equipment['server1'].send_cmd("cd #{testbins}", @equipment['server1'].prompt, 30)
-          dst_path = "#{samba_root_path}\\opt\\#{testbins}\\#{File.basename(src)}"
-          debug_puts dst_path
-          BuildClient.copy(src, dst_path)
-          @equipment['server1'].send_cmd("tar -xvzf #{File.basename(src)}", @equipment['server1'].prompt, 30)        
-      end 
-      
-
-
-      # Leave target in appropriate directory
-   #   @equipment['dut1'].send_cmd("cd #{nfs_path}\n", /#{@equipment['dut1'].prompt}/, 10)  if ( @equipment.has_key?('server1') && !(nfs) && !(@test_params.instance_variable_defined?(:@var_nfs)) )
-      
+   
     end
 
     
     def clean
       debug_puts "default.clean"
-      kernel_modules = @test_params.kernel_modules   if @test_params.instance_variable_defined?(:@kernel_modules)
-      if kernel_modules
-        #kernel_modules_list = @test_params.params_chan.kernel_modules_list  
-        if @test_params.params_chan.instance_variable_defined?(:@kernel_modules_list)
-          @test_params.params_chan.kernel_modules_list.each {|mod|
-            mod_name = C6xKernelModuleNames::translate_mod_name(@test_params.platform, mod.strip)
-            @equipment['dut1'].send_cmd("rmmod #{mod_name}", /#{@equipment['dut1'].prompt}/, 30)  
-          }
-        end
-      end
 
     end
     
 
     
     def get_keys
-      # keys = @test_params.target.to_s + @test_params.dsp.to_s + @test_params.micro.to_s + 
-      # @test_params.platform.to_s + @test_params.os.to_s + @test_params.custom.to_s + 
-      # @test_params.microType.to_s + @test_params.configID.to_s
       keys = @test_params.platform.to_s
       keys
     end
@@ -216,14 +180,6 @@ module C6xTestScript
       end
     end
 	
-    def add_log_to_html(log_file_name)
-      # add log in result page
-      all_lines = ''
-      File.open(log_file_name, 'r').each {|line|
-        all_lines += line 
-      }
-      @results_html_file.add_paragraph(all_lines,nil,nil,nil)
-    end
     def debug_puts(message)
     if @show_debug_messages == true
       puts(message)
