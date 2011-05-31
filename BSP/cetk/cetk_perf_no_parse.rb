020ac7d17a0d06224052c2dbdba14382a8c88bb5
unknown <a0133110@.am.dhcp.ti.com>
2011-05-31 12:23:18 -0400

    Batch file for silverlight performance CETK test and corresponding ruby file which does not parse output but ensures that generated xml/csv is attached to test log. The cetk_perf_no_parse.rb can be used with other tests where output pattern cannot be generalized and need not be parsed for now.
    Signed-off-by: Yan Liu <yan-liu@ti.com>

---
+++ BSP/cetk/cetk_perf_no_parse.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__)+'/cetk_perf'
+require 'rexml/document'
+include REXML
+require 'iconv'
+#include WinceTestScript
+
+def parse_xml_data(log_file)
+    result = ""
+	perfdata = []
+return perfdata
+
+def parse_csv_data(log_file)
+    result = ""
+	perfdata = []
+return perfdata
+
+end 
---
+++ BSP/cetk/cetk_perf_silverlight.bat
@@ -0,0 +1,9 @@
+@ECHO off
+echo "Testing EVM with IP:  #{@equipment['dut1'].telnet_ip}"
+echo "Testing Kernel image: #{@test_params.instance_variable_defined?(:@kernel) ? @test_params.kernel.to_s : 'No kernel specified'}"
+echo "Command Line: #{@test_params.params_chan.cmdline[0]}"
+mkdir \Release
+copy \windows\*baml*.dll \release
+#{@test_params.params_chan.cmdline[0]}
+delete \release\*baml*.dll
+
