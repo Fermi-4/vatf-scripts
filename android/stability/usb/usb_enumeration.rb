101120a8a3c4b957ff8bf8343501d7b8246e8027
Yebio Mesfin <ymesfin@ti.com>
2011-06-10 14:19:38 -0400

    USB Enumeration Test updated.
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- android/stability/usb/usb_enumeration.rb
+++ android/stability/usb/usb_enumeration.rb
@@ -17,34 +17,34 @@ def run
   i=0
   successful_enums=0
   session_data_pointer=0
-  expected_enum_regex=get_expected_regex(@test_params.params_control.enum_strings)
   
   # Start from disconnect state
   @equipment['usb_sw'].select_input(0)   # 0 means don't select any input port.
   
   # Loop for Connect/Disconnect
-  @test_params.params_control.iterations[0].to_i.times do
-    sleep @test_params.params_control.wait_after_disconnect[0].to_i
+  @test_params.params_chan.iterations[0].to_i.times do
+    sleep 2
+    sleep @test_params.params_chan.wait_after_disconnect[0].to_i
     session_data_pointer = @equipment['dut1'].update_response.length
     
     # Connect
     @equipment['usb_sw'].select_input(@equipment['dut1'].params['usb_port'])
-    sleep @test_params.params_control.wait_after_connect[0].to_i
+    sleep @test_params.params_chan.wait_after_connect[0].to_i
     
     # Check device is enumerated
     ses_data = @equipment['dut1'].update_response
     enum_data = ses_data[session_data_pointer==0 ? 0: session_data_pointer-1, ses_data.length]
     @equipment['dut1'].log_info("Iteration #{i}: \n #{enum_data}")
-    successful_enums = successful_enums + 1 if expected_enum_regex.match(enum_data)
+    successful_enums = successful_enums + 1 if verify_devices_detected(enum_data) == 1 
     
     # Disconnect
     @equipment['usb_sw'].select_input(0)   # 0 means don't select any input port.
     i = i+1
   end
   
-  success_rate = (successful_enums.to_f / @test_params.params_control.iterations[0].to_f)*100.0
+  success_rate = (successful_enums.to_f / @test_params.params_chan.iterations[0].to_f)*100.0
   
-  if (success_rate >= @test_params.params_control.pass_rate[0].to_f)
+  if (success_rate >= @test_params.params_chan.pass_rate[0].to_f)
     set_result(FrameworkConstants::Result[:pass], "Success Enumeration rate=#{success_rate}")
   else
     set_result(FrameworkConstants::Result[:fail], "Success Enumeration rate=#{success_rate}")
@@ -53,8 +53,26 @@ def run
 end
 
 private 
-def get_expected_regex(expected_enum_array)
-  regex_array = expected_enum_array.join(".*")
-  /#{regex_array}/mi
-end
+
+def verify_devices_detected(enum_data) 
+ devices_numbers=Hash.new 
+ @test_params.params_chan.enum_strings.each{|current|
+   count = 0 
+   @test_params.params_chan.enum_strings.each{|elem|
+   if current.eql?(elem)
+    count = count + 1 
+   end   
+   }
+   devices_numbers[current] = count    
+ }
+ filtered_data = enum_data.gsub(/{#@test_params.params_chan.redundant[0]}/,"")
+ @test_params.params_chan.enum_strings.each{|current|
+ if enum_data.scan(/#{current}/).length != devices_numbers[current]
+  puts "#{current}  :::: #{enum_data.scan(/#{current}/).length} :: #{devices_numbers[current]}"
+  return 0
+ end 
+ }
+  return 1 
+end 
+
 
