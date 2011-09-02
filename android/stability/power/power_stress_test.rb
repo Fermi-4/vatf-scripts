89571ba8cc06865b06a52ab3c18468547a9ffd3a
Yebio Mesfin <ymesfin@ti.com>
2011-09-02 07:22:23 -0400

    Add power stress test for Android
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- android/stability/power/power_stress_test.rb
+++ android/stability/power/power_stress_test.rb
@@ -2,10 +2,12 @@ require File.dirname(__FILE__)+'/../../android_test_module'
 require File.dirname(__FILE__)+'/../../keyevents_module'
 require File.dirname(__FILE__)+'/../../power_events_module'
 require File.dirname(__FILE__)+'/../../power_module'
+require File.dirname(__FILE__)+'/../../../lib/multimeter_power'
 include AndroidTest
 include AndroidKeyEvents
 include PowerEventsModule
 include PowerModule
+include MultimeterModule
 
 require 'gnuplot.rb'
 
@@ -85,7 +87,7 @@ def run
   #sleep 80
   sleep 5
   # Get voltage values for all channels in a hash
-  volt_readings = run_get_multimeter_output      
+  volt_readings = run_get_multimeter_output(@equipment['ti_multimeter'])      
   # Calculate power consumption
   power_readings = calculate_mean_power_consumption(volt_readings)
   puts "MEAN VDD1 POWER READING #{power_readings['all_vvd1']}"
@@ -134,72 +136,6 @@ def save_results(power_consumption)
 return perf
 end
 
-def run_get_multimeter_output
-  sleep 5    # Make sure multimeter is configured and DUT is in the right state
-  volt_reading = []
-  counter=0
-  while counter < @test_params.params_chan.loop_count[0].to_i
-    Kernel.print("Collecting sample #{counter}\n")
-    @equipment['multimeter1'].send_cmd("READ?", /.+?,.+?,.+?,.+?,[^\r\n]+/, @test_params.params_chan.timeout[0].to_i, false)
-    d =  @equipment['multimeter1'].response
-    Kernel.print("#{d}\n")
-    volt_reading << @equipment['multimeter1'].response
-    counter += 1
-    #sleep 0.5
-  end
-  return sort_raw_data(volt_reading)
-end
-
-# Procedure to meaasure power on AM37x.
-# chan1= vdrop at vdd1 , chan2=vdrop at vdd2, chan3=ignore, chan4=vdd1, chan5=vdd2
-def sort_raw_data(volt_readings)
-  chan_all_volt_reading = Hash.new
-  chan_1_volt_readings = Array.new
-  chan_2_volt_readings = Array.new
-  chan_3_volt_readings = Array.new
-  chan_4_volt_readings = Array.new
-  chan_5_volt_readings = Array.new
-  chan_1_current_readings = Array.new
-  chan_2_current_readings = Array.new
-  volt_reading_array = Array.new
-  volt_readings.each do |current_line| 
-    current_line_arr = current_line.strip.split(/[,\r\n]+/)
-    if current_line_arr.length == 5 && current_line.match(/([+-]\d+\.\d+E[+-]\d+,){4}[+-]\d+\.\d+E[+-]\d+/)
-      volt_reading_array.concat(current_line_arr)
-    else 
-    puts "NOTHING #{current_line}"
-    end
-  end
-  volt_reading_array.each_index{|array_index|
-   mod = array_index % 5 
-   case mod
-     when  0
-     temp = volt_reading_array[array_index].gsub(/\+/,'').to_f + 0.00029
-     chan_1_volt_readings << temp
-     chan_1_current_readings << temp/0.05
-     when  1
-     temp = volt_reading_array[array_index].gsub(/\+/,'').to_f + 0.00029
-     chan_2_volt_readings << temp
-     chan_2_current_readings << temp/0.1
-     when  2
-     chan_3_volt_readings << volt_reading_array[array_index].gsub(/\+/,'').to_f + 0.00029
-     when  3
-     chan_4_volt_readings << volt_reading_array[array_index].gsub(/\+/,'').to_f + 0.00029
-     when  4
-     chan_5_volt_readings << volt_reading_array[array_index].gsub(/\+/,'').to_f + 0.00029
-   end 
-  }
-  # each reading for each channel
-  chan_all_volt_reading["chan_1"] = chan_1_volt_readings
-  chan_all_volt_reading["chan_2"] = chan_2_volt_readings
-  chan_all_volt_reading["chan_3"] = chan_3_volt_readings
-  chan_all_volt_reading["chan_4"] = chan_4_volt_readings
-  chan_all_volt_reading["chan_5"] = chan_5_volt_readings
-  chan_all_volt_reading["chan_1_current"] = chan_1_current_readings
-  chan_all_volt_reading["chan_2_current"] = chan_2_current_readings
-  
-  return chan_all_volt_reading
- end
 
 def calculate_mean_power_consumption(volt_reading)
   power_consumption = Hash.new
@@ -220,29 +156,6 @@ def calculate_mean_power_consumption(volt_reading)
   power_consumption['all_vvd2'] = vdd2_mean_power_reading
   power_consumption['all_vvd1_vdd2'] = vdd1_vdd2_mean_power_readings
   return power_consumption
-end 
-
-# This function plots the power consumpotion point by point for the whole clip duration.
-def power_consumption_plot(power_consumption)
-  plot_output = @files_dir+"/plot_#{@test_id}\.pdf"
-  max_range  =  (power_consumption['all_vvd1'].size).to_i - 1
-  puts "\nDEBUG\nmaxrange=#{max_range}=========\nDEBUG"
-  Gnuplot.open { |gp|
-    Gnuplot::Plot.new( gp ) { |plot|
-      plot.terminal "post eps colour size 13cm,10cm"
-      plot.output plot_output
-      plot.title  "POWER CONSUMPTION Vs Time"
-      plot.ylabel "Power (mw)"
-      plot.xlabel "Samples"
-            x = (0..max_range).collect { |v| v.to_f }
-      y = power_consumption['all_vvd1_vdd2'].collect { |v| v }
-      plot.data << Gnuplot::DataSet.new( [x, y]) { |ds|
-        ds.with = "lines"
-        ds.linewidth = 4
-      }
-      
-    }
-  }
 end 
 
 
