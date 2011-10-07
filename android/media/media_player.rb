5b1e36e7718f5c8a097320c5067e6e09d1c13f1e
gtadwlan001 <gtadwlan001@gtadwlan001_YM-desktop.(none)>
2011-10-07 05:08:39 -0400

    Added a number of function on sys_stats.rb to collect fps and different kinds of memory usages by a given processes.
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- android/media/media_player.rb
+++ android/media/media_player.rb
@@ -1,44 +1,92 @@
-require File.dirname(__FILE__)+'/../android_test_module' 
+require File.dirname(__FILE__)+'/../android_test_module'
+require File.dirname(__FILE__)+'/../../lib/sys_stats'
+require File.dirname(__FILE__)+'/../keyevents_module'  
 include AndroidTest
+include SystemStats
+include AndroidKeyEvents
 
 def run 
 fps_values = Array.new()
+process_pids = Hash.new
+top_stats = Hash.new
+put_screen_home = ["__back__","__back__","__back__","__back__"]
+send_events_for(put_screen_home) 
 endable_fps = "setprop debug.video.showfps 1"
 cmd = "shell " + endable_fps
 #send fps enable command 
 data = send_adb_cmd cmd
+
 cmd = "push " + @test_params.params_chan.host_file_path[0] + "/" + @test_params.params_chan.file_name[0] +  " " + @test_params.params_chan.target_file_path[0] + "/" + @test_params.params_chan.file_name[0]
-#send file push command 
+#send file push command
 data = send_adb_cmd cmd
 if data.scan(/[0-9]+\s*KB\/s\s*\([0-9]+\s*bytes\s*in/)[0] == nil
+  puts "EXITING EXITING EXITING "
   puts "#{data}"
   exit 
 end
 
-#cmd = "shell am start -W -n com.cooliris.media/.MovieView -a action.intent.anction.VIEW -d " + 
+cmd = "install " + @test_params.params_chan.apps_host_file_path[0] + "/" + @test_params.params_chan.app_name[0]
+data = send_adb_cmd cmd
+if data.scan(/[0-9]+\s*KB\/s\s*\([0-9]+\s*bytes\s*in/)[0] == nil and !data.to_s.include?("INSTALL_FAILED_ALREADY_EXISTS")
+ puts "#{data}"
+#   exit 
+end 
 
+#clear the old log
+cmd = "logcat  -d -c"
+data = send_adb_cmd  cmd
+
+#construct media command
 cmd = @test_params.params_chan.intent[0] + " " + @test_params.params_chan.target_file_path[0] + "/" + @test_params.params_chan.file_name[0]
 #send intent command to play the clip
 data = send_adb_cmd  cmd
-sleep 30
-cmd  = "logcat -d "
-response = send_adb_cmd cmd 
- response .split("\n").each{|line|
- if line.include?("FPS")
-  line = line.scan(/([0-9]*\.[0-9]+)/)
-  fps_values << line[0][0]
+#time delay before collecting data
+#to be done check the locat for sign of the process started
+#sleep @test_params.params_chan.sleep_time[0].to_i
+
+
+# read process ids for the given processes.
+process_pids = get_android_process_pids(@test_params.params_chan.processes_name)
+
+#start Application
+#pids must be supplied separated by comma
+# duration and intervals are in miliseconds. Interval and duration are parameters. Pids are caculated dynamically.
+# A libray which takes process name will connect the pids.
+# application start command    
+cmd = @test_params.params_chan.apps_intent[0] + " -e pids " + process_pids["pids"] + " -e duration " +  @test_params.params_chan.duration[0] + " -e interval " + @test_params.params_chan.interval[0]
+#to start application.
+
+data = send_adb_cmd  cmd
+
+#Read to result 
+top_stats =  get_android_top_stats(@test_params.params_chan.cpu_load_samples[0].to_i,@test_params.params_chan.processes_name[0], @test_params.params_chan.time[0])
+
+ if (@test_params.params_control.instance_variable_defined?(:@processes_name) && top_stats['process_cpu_loads'].length == 0)
+              set_result(FrameworkConstants::Result[:fail], 'Cpu load could not be obtained. Verify that process #{params_control.processes_name[0]} has started.')
+              puts 'Test failed: Cpu load could not be obtained. Verify that process #{params_control.processes_name[0]} has started.'
  end 
- }
- result,comment,perfdata = save_results(fps_values)
+ fps_values = get_fps
+ result,comment,perfdata = save_results(fps_values,top_stats['process_cpu_loads'],top_stats['process_mem_usage_rss'],process_pids)
  set_result(result,comment,perfdata)
 end 
 
 
 
 
-def save_results(fps_values)
+def save_results(fps_values,cpu_loads,mem_usage,process_pids)
    perf_data = [];
-   perf_data << {'name' => @test_params.params_chan.testname[0], 'value' =>fps_values, 'units' => "fps"}
+   perf_data << {'name' => "#{@test_params.params_chan.testname[0]}", 'value' =>fps_values, 'units' => "fps"} if fps_values.length != 0
+   perf_data << {'name' => "#{@test_params.params_chan.processes_name[0]} Cpu Load", 'value' =>cpu_loads, 'units' => "%"} if cpu_loads.length != 0
+   perf_data << {'name' =>  "#{@test_params.params_chan.processes_name[0]} Mem Usage(RSS)", 'value' =>mem_usage, 'units' => "%"} if mem_usage.length != 0
+
+#in this loop I will collect all meminfo for all processes 
+#this to be completed 
+@test_params.params_chan.processes_name.each{|process_name|
+  process_meminfo = get_android_process_meminfo(@test_params.params_chan.metrics,process_pids[process_name])
+ @test_params.params_chan.metrics.each{|metric|
+  perf_data << {'name' =>  "#{@test_params.params_chan.processes_name[0]} #{metric}", 'value' =>process_meminfo[metric], 'units' => "kb"} if process_meminfo[metric].length != 0
+}
+}
   # I have to add pass-fail criteria once known
   [FrameworkConstants::Result[:pass], "Test case PASS.",perf_data]
 end
--- lib/sys_stats.rb
+++ lib/sys_stats.rb
@@ -65,5 +65,97 @@ module SystemStats
     mem_stats
   end
 
+
+# Generic function to collect top query
+def get_android_top_stats(cpu_load_samples,process_name,time)
+    top_stats = Hash.new
+    top_result = Array.new 
+    top_stats['process_cpu_loads'] = Array.new
+    top_stats['process_mem_usage_rss'] = Array.new
+    process_cpu_loads = Array.new
+    process_mem_usage_rss = Array.new
+    cpu_info = ''
+    delay = [time.to_i/cpu_load_samples,1].max
+    #cpu_info = send_adb_cmd("shell top -d #{delay} -n #{cpu_load_samples-1}") # this is not working for now I have to get back to it
+    puts "Start collecting samples now please wait ........."
+    #cpu_info = `adb shell top -d #{delay} -n #{cpu_load_samples-1}`
+    cpu_info = send_adb_cmd("shell top -d #{delay} -n #{cpu_load_samples-1}")    
+    top_result = cpu_info.scan(/(\d+)%\s+[SR]\s+\d+\s+\d+K\s+(\d+)K\s+fg.*#{process_name}/i)   
+    #extract stat
+    top_result.each{|t|
+     process_cpu_loads <<  t[0][0]
+     process_mem_usage_rss <<  t[1] 
+    }
+   top_stats['process_cpu_loads'] = process_cpu_loads
+   top_stats['process_mem_usage_rss'] = process_mem_usage_rss
+   return top_stats
+end 
+
+# for certain process name we pass metrics and its process id
+def get_android_process_meminfo(metrics,pid)
+    puts "GETTING MEMINFO"
+    meminfos = Hash.new()
+    #cmd = "logcat  -d -s ProcessMemoryInfoService"
+    #response = send_adb_cmd cmd
+    #response = `adb logcat  -d -s ProcessMemoryInfoService`
+    response = send_adb_cmd("logcat  -d -s ProcessMemoryInfoService")
+    local_metrics = metrics
+    local_metrics = [metrics] if !metrics.kind_of?(Array)
+    local_metrics.each{|metric|
+    metric_values = Array.new
+    meminfo  = response.scan(/#{get_android_meminfo_regex(pid,metric)}/)
+    meminfo.each{|info|
+    metric_values <<  info[0] 
+    } 
+    meminfos[metric]  = metric_values
+    }
+    meminfos
+end 
+
+def get_android_meminfo_regex(pid,metric_name)
+regexs = Hash.new 
+regexs["dalvik private dirty"] =  "pid\\s+=\\s+#{pid}\\s+dalvik\\s+private\\s+dirty\\s+(\\d+)"
+regexs["dalvik_pss"] = "pid\\s+=\\s+#{pid}\\s+delvid\\s+pss\\s+(\\d+)"
+regexs["dalvik_shared_dirty"] = "pid\\s+=\\s+#{pid}\\s+delivik\\s+shared\\s+dirty\\s+(\\d+)"
+regexs["native_private_dirty"] = "pid\\s+=\\s+#{pid}\\s+native\\s+private\\s+dirty\\s+(\\d+)"
+regexs["native_pss"] = "pid\\s+=\\s+#{pid}\\s+native\\s+pss\\s+(\\d+)"
+regexs["native_shared_dirty"] = "pid\\s+=\\s+#{pid}\\s+native\\s+shared\\s+dirty\\s+(\\d+)"
+regexs["other_private_dirty"] = "pid\\s+=\\s+#{pid}\\s+other\\s+private\\s+dirty\\s+(\\d+)"
+regexs["other_pss"] = "pid\\s+=\\s+#{pid}\\s+other\\s+pss\\s+(\\d+)"
+regexs["other_shared_dirtyl"] = "pid\\s+=\\s+#{pid}\\s+other\\s+shared\\s+dirtyl\\s+(\\d+)"
+regexs["total_PrivateDirty"] = "pid\\s+=\\s+#{pid}\\s+Total\\s+PrivateDirty\\s+(\\d+)"
+regexs["total_pss"] = "pid\\s+=\\s+#{pid}\\s+total\\s+pss\\s+(\\d+)"
+regexs[metric_name]
+
+end 
+
+
+def get_fps 
+fps_values = Array.new()
+cmd  = "logcat -d "
+response = send_adb_cmd cmd 
+ response .split("\n").each{|line|
+ if line.include?("FPS")
+  line = line.scan(/([0-9]*\.[0-9]+)/)
+  fps_values << line[0][0]
+ end 
+ }
+ return fps_values
+end 
+
+def get_android_process_pids(process_names)
+    processes = Hash.new()
+    pids = "" 
+    #cpu_info =  `adb shell top -n 1`
+    cpu_info = send_adb_cmd("shell top -n 1")
+    process_names.each{|process|    
+    processes[process] = cpu_info.scan(/(\d+)\s+\d+%\s+[SR]\s+\d+\s+\d+K\s+\d+K\s+fg.*#{process}/i)[0][0].to_s
+    puts "PIDS PIDS #{processes[process]}"
+    pids = processes[process]+","
+    }
+   processes["pids"] = pids
+   processes
+end 
+
 end  # End of module
 
