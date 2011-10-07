babadad3959a19999bd7f96f0e809151cd4c4905
Carlos Hernandez <c-hernandez8@ti.com>
2011-10-06 22:33:41 -0400

    LSP:default_test_module.rb: Use primary and secondary bootloader
    Pass primary_bootloader and secondary_bootloader (if defined) to
    boot method. This is required to support loading bootloaders images.
    
    Signed-off-by: Carlos Hernandez <c-hernandez8@ti.com>

--- LSP/default_test_module.rb
+++ LSP/default_test_module.rb
@@ -72,25 +72,22 @@ module LspTestScript
       
         @new_keys = (@test_params.params_chan.instance_variable_defined?(:@bootargs))? (get_keys() + @test_params.params_chan.bootargs[0]) : (get_keys()) 
         if boot_required?(@old_keys, @new_keys) && @test_params.instance_variable_defined?(:@kernel)
-		boot_params = {'power_handler'=> @power_handler,
+		  boot_params = {'power_handler'=> @power_handler,
 			       'platform' => platform_from_db,
 			       'tester' => tester_from_cli,
 			       'target' => target_from_db ,
 			       'image_path' => @test_params.kernel,
-			       'server' => @equipment['server1'], 
-			       'nfs_root' => nfs_root_path_temp}
-		boot_params['bootargs'] = @test_params.params_chan.bootargs[0] if @test_params.params_chan.instance_variable_defined?(:@bootargs)
-		#@new_keys = (@test_params.params_chan.instance_variable_defined?(:@bootargs))? (get_keys() + @test_params.params_chan.bootargs[0]) : (get_keys()) 
-	      
-		#if boot_required?(@old_keys, @new_keys) # call bootscript if required
-		if @equipment['dut1'].respond_to?(:serial_port) && @equipment['dut1'].serial_port != nil
-		@equipment['dut1'].connect({'type'=>'serial'})
-		elsif @equipment['dut1'].respond_to?(:serial_server_port) && @equipment['dut1'].serial_server_port != nil
-			@equipment['dut1'].connect({'type'=>'serial'})
-		else
-		    raise "You need direct or indirect (i.e. using Telnet/Serial Switch) serial port connectivity to the board to boot. Please check your bench file" 
-		end
-		@equipment['dut1'].boot(boot_params) 
+                   'server' => @equipment['server1'], 
+			       'nfs_root' => nfs_root_path_temp,
+                   'staf_service_name' => @test_params.staf_service_name.to_s}
+		  boot_params['bootargs'] = @test_params.params_chan.bootargs[0] if @test_params.params_chan.instance_variable_defined?(:@bootargs)
+          boot_params['primary_bootloader'] = @test_params.primary_bootloader if @test_params.instance_variable_defined?(:@primary_bootloader)
+          boot_params['secondary_bootloader'] = @test_params.secondary_bootloader if @test_params.instance_variable_defined?(:@secondary_bootloader)
+		     
+      	  if !(@equipment['dut1'].respond_to?(:serial_port) && @equipment['dut1'].serial_port != nil) && !(@equipment['dut1'].respond_to?(:serial_server_port) && @equipment['dut1'].serial_server_port != nil)
+      		raise "You need direct or indirect (i.e. using Telnet/Serial Switch) serial port connectivity to the board to boot. Please check your bench file" 
+          end
+      	  @equipment['dut1'].boot(boot_params) 
         end
       end
       connect_to_equipment('dut1')
