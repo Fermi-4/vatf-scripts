d561ba9355d4e4cd50b85513531116123304bd1f
Elvita Lobo <elvita@ti.com>
2011-10-11 13:14:13 -0400

    VGDK:More Updates to scripts for Video MCSDK2.0

--- VGDK/boot_scripts/boot.rb
+++ VGDK/boot_scripts/boot.rb
@@ -11,19 +11,20 @@ require File.dirname(__FILE__)+'/dsp_glob_cfg.rb'
 include DSPGlobConfig
 module BootScripts
   def boot(dut,ftp_server,boot_params,power_handler)
-	power_port = dut.power_port
-	if power_port !=nil
-	   debug_puts 'Switching off @using power switch'
-	   power_handler.switch_off(power_port)
-	end
-	power_handler.switch_on(power_port)
-	sleep(60)
+    power_port = dut.power_port
+    if power_port !=nil
+       debug_puts 'Switching off @using power switch'
+       power_handler.switch_off(power_port)
+       sleep(60)
+    end
+    power_handler.switch_on(power_port)
+    sleep(60)
     dut.connect({'type'=>'serial'})
-	0.upto 5 do
-	  dut.send_cmd("\n",dut.prompt, 30)
-	  debug_puts 'Sending esc character'
-	  sleep 1
-	  break if !dut.timeout?
+    0.upto 5 do
+      dut.send_cmd("\n",dut.prompt, 30)
+      debug_puts 'Sending esc character'
+      sleep 1
+      break if !dut.timeout?
     end
 
     if(boot_params != nil and ftp_server != nil)
@@ -45,20 +46,19 @@ module BootScripts
   
   def download_dsp(dut,ftp_server,dsp)
     begin
-	  File.delete("#{ftp_server.tftp_path}\\tv01.ld") if File.exists?("#{ftp_server.tftp_path}\\tv01.ld") 
+      File.delete("#{ftp_server.tftp_path}\\sv01.ld") if File.exists?("#{ftp_server.tftp_path}\\sv01.ld") 
       FileUtils.cp(dsp,"#{ftp_server.tftp_path}\\")
-	  sleep(2)
+      sleep(2)
       Dir.chdir("#{ftp_server.tftp_path}\\")
-      File.rename("#{File.basename(dsp)}","tv01.ld")
+      File.rename("#{File.basename(dsp)}","sv01.ld")
     rescue SystemCallError
       $stderr.print "File IO failed" + $!
       raise
     end
-      dut.send_cmd("cd /APP",/.*/,2)
-      dut.send_cmd("cd dspi",/.*/,2)
-	  dut.send_cmd("rm tv01\.ld",/.*/,2)
-      dut.send_cmd("wget ftp\://#{ftp_server.telnet_login}\:#{ftp_server.telnet_passwd}@#{ftp_server.telnet_ip}/tv01.ld",/100%/,10)
-	  sleep(2)
+      dut.send_cmd("cd /root/app/video",/.*/,2)
+      dut.send_cmd("rm -f sv01\.ld",/.*/,2)
+      dut.send_cmd("wget ftp\://#{ftp_server.telnet_login}\:#{ftp_server.telnet_passwd}@#{ftp_server.telnet_ip}/sv01.ld",/100%/,10)
+      sleep(2)
     if(dut.timeout?)
       raise "wget: dsp failed"
     end 
@@ -66,20 +66,20 @@ module BootScripts
   
   def download_app(dut,ftp_server,app)
     begin
-	puts "Deleting #{ftp_server.tftp_path}\\dimtestvi"
-	  File.delete("#{ftp_server.tftp_path}\\dimtestvi") if File.exists?("#{ftp_server.tftp_path}\\dimtestvi")
+    puts "Deleting #{ftp_server.tftp_path}\\dimtestvi"
+      File.delete("#{ftp_server.tftp_path}\\dimtestvi") if File.exists?("#{ftp_server.tftp_path}\\dimtestvi")
       FileUtils.cp(app,"#{ftp_server.tftp_path}\\")
-	  sleep(2)
+      sleep(2)
       Dir.chdir("#{ftp_server.tftp_path}\\")
       File.rename("#{File.basename(app)}","dimtestvi")
     rescue SystemCallError
       $stderr.print "File IO failed" + $!
       raise
     end
-      dut.send_cmd("cd /APP",/.*/,2)
-	  dut.send_cmd("rm dimtestvi",/.*/,2)
+      dut.send_cmd("cd /root/app/",/.*/,2)
+      dut.send_cmd("rm -f dimtestvi",/.*/,2)
       dut.send_cmd("wget ftp\://#{ftp_server.telnet_login}\:#{ftp_server.telnet_passwd}@#{ftp_server.telnet_ip}/dimtestvi",/100%/,10)
-	  sleep(2)
+      sleep(2)
     if(dut.timeout?)
       raise "wget: dimtestvi failed"
     end 
