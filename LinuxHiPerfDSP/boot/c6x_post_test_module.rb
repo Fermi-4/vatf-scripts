307323d058ca91212466b2c078237da51a4c36fd
Elvita Lobo <elvita@ti.com>
2011-11-23 18:26:22 -0500

    LinuxHiPerfDSP: Fix POST test

--- LinuxHiPerfDSP/boot/c6x_post_test_module.rb
+++ LinuxHiPerfDSP/boot/c6x_post_test_module.rb
@@ -6,14 +6,14 @@
     def run
       @equipment['dut1'].set_api('linux-c6x')
       boot_times = @test_params.params_chan.instance_variable_get("@boot_times")[0].to_i
-      wait_for_string = Regexp.new(@test_params.params_chan.instance_variable_get("@wait_for_string")[0])
+      wait_for_string = Regexp.new("POST result:\s+(PASS|FAIL)")
       puts "Test looks for #{wait_for_string}"
       timeout = @test_params.params_chan.instance_variable_get("@timeout")[0].to_i
       power_port = @equipment['dut1'].power_port
       success_times = 0
       fail_times = 0
       boot_arr = [] 
-       
+      boot_failures = 0
       test_done_result = FrameworkConstants::Result[:fail]
       comment = "Test fail"    
      
@@ -27,26 +27,33 @@
       disconnect('dut1')
       @power_handler.switch_off(power_port)
       sleep(10)
-      connect_to_equipment('dut1')
+     # connect_to_equipment('dut1')
       @power_handler.switch_on(power_port)
-      @equipment['dut1'].wait_for(wait_for_string, timeout)
-      if (@equipment['dut1'].timeout?)
-        fail_times = fail_times+1
-        boot_arr << 'X'
-      else
-        success_times = success_times+1
-        boot_arr << 'B'
-      # rescue Exception => e
-        # puts e.to_s+"\n"+e.backtrace.to_s
-        # response =  nil
-      end
+	  response = connect_to_equipment('dut1',wait_for_string,timeout)
+	  if (response != nil)
+	    result = parse_response(response,wait_for_string)
+		if (result == true)
+		  success_times = success_times+1
+		  boot_arr << 'B'
+		else
+		  fail_times = fail_times+1
+		  boot_arr << 'X'
+		end
+	  else
+		fail_times = fail_times+1
+		boot_failures = boot_failures+1
+		boot_arr << 'X'
+	  end
       }
       if success_times == boot_times
         test_done_result = FrameworkConstants::Result[:pass]
         comment = "Test pass. POST test completed successfully #{boot_times} out of #{boot_times} times "
+	  else
+		test_done_result = FrameworkConstants::Result[:fail]
+        comment = "Test fail. POST test failed #{fail_times} out of #{boot_times} times "    
       end
-      if (fail_times > 0) 
-        comment += "POST failed to boot #{fail_times} out of #{boot_times} times. Boot log - #{boot_arr.to_s}"
+      if (boot_failures > 0) 
+        comment += "Board failed to boot #{boot_failures} out of #{boot_times} times"
       end
       set_result(test_done_result,comment)
     end
@@ -57,15 +64,36 @@
 
     end
 
-    def connect_to_equipment(equipment)
+    def connect_to_equipment(equipment,wait_for_string,timeout)
       this_equipment = @equipment["#{equipment}"]
       if ((this_equipment.respond_to?(:serial_port) && this_equipment.serial_port != nil ) || (this_equipment.respond_to?(:serial_server_port) && this_equipment.serial_server_port != nil)) && !this_equipment.target.serial
         this_equipment.connect({'type'=>'serial'})     
       elsif !this_equipment.target.serial
         raise "You need Serial port connectivity to #{equipment}. Please check your bench file" 
       end
+      begin
+        this_equipment.wait_for(wait_for_string, timeout)
+        if (this_equipment.timeout?)
+          return nil
+        else
+          return this_equipment.response
+        end
+      rescue Exception => e
+        puts e.to_s+"\n"+e.backtrace.to_s
+        return nil
+      end
     end
-    
+    def parse_response(response,wait_for_string)
+	  result = response.match(/POST result:\s+(PASS|FAIL)/).captures[0].to_s
+	  puts " +++++++++ Result: #{result} ++++++++++++++"
+	  if (result.match(/PASS/))
+	    return true
+	  elsif (result.match(/FAIL/))
+		return false
+	  else 
+	    return nil
+      end
+	end
     def disconnect(equipment)
       this_equipment = @equipment["#{equipment}"]
       if this_equipment.target.telnet || this_equipment.target.serial
