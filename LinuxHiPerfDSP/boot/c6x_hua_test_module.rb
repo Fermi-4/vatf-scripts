ab38d98d06dc71056b4f9550329b8703e6ff4562
Elvita Lobo <elvita@ti.com>
2011-12-01 18:29:25 -0500

    LinuxHiPerfDSP: Add HUA test

---
+++ LinuxHiPerfDSP/boot/c6x_hua_test_module.rb
@@ -0,0 +1,110 @@
+# -*- coding: ISO-8859-1 -*-
+
+    def setup
+    
+    end
+    def run
+      @equipment['dut1'].set_api('linux-c6x')
+      boot_times = @test_params.params_chan.instance_variable_get("@boot_times")[0].to_i
+      #wait_for_string = Regexp.new("Network\sAdded:\s+If-1:((?:\d{1,3}\.){3}\d{1,3})")
+      wait_for_string = /Network\sAdded:\s+If-1:((?:\d{1,3}\.){3}\d{1,3})/
+      puts "Test looks for #{wait_for_string}"
+      timeout = @test_params.params_chan.instance_variable_get("@timeout")[0].to_i
+      power_port = @equipment['dut1'].power_port
+      success_times = 0
+      fail_times = 0
+      boot_arr = [] 
+      boot_failures = 0
+      test_done_result = FrameworkConstants::Result[:fail]
+      comment = "Test fail"    
+     
+      #Switch off power
+
+      debug_puts 'Switching off @using power switch'
+      @power_handler.switch_off(power_port)
+
+      boot_times.times { |i|
+      puts "Switching power for # #{i}th iteration"
+      disconnect('dut1')
+      @power_handler.switch_off(power_port)
+      sleep(10)
+     # connect_to_equipment('dut1')
+      @power_handler.switch_on(power_port)
+      response = connect_to_equipment('dut1',wait_for_string,timeout)
+      if (response != nil)
+        result = parse_response(response,wait_for_string)
+      if (result == true)
+        success_times = success_times+1
+        boot_arr << 'B'
+      else
+        fail_times = fail_times+1
+        boot_arr << 'X'
+      end
+      else
+      fail_times = fail_times+1
+      boot_failures = boot_failures+1
+      boot_arr << 'X'
+      end
+      }
+      if success_times == boot_times
+        test_done_result = FrameworkConstants::Result[:pass]
+        comment = "Test pass. HUA test completed successfully #{boot_times} out of #{boot_times} times "
+      else
+      test_done_result = FrameworkConstants::Result[:fail]
+          comment = "Test fail. HUA test passed #{success_times} out of #{boot_times} times. Boot log - #{boot_arr.to_s} "    
+      end
+      if (boot_failures > 0) 
+        comment += "Board failed to boot #{boot_failures} out of #{boot_times} times"
+      end
+      set_result(test_done_result,comment)
+    end
+
+    
+    def clean
+      debug_puts "default.clean"
+
+    end
+
+    def connect_to_equipment(equipment,wait_for_string,timeout)
+      this_equipment = @equipment["#{equipment}"]
+      if ((this_equipment.respond_to?(:serial_port) && this_equipment.serial_port != nil ) || (this_equipment.respond_to?(:serial_server_port) && this_equipment.serial_server_port != nil)) && !this_equipment.target.serial
+        this_equipment.connect({'type'=>'serial'})     
+      elsif !this_equipment.target.serial
+        raise "You need Serial port connectivity to #{equipment}. Please check your bench file" 
+      end
+      begin
+        this_equipment.wait_for(wait_for_string, timeout)
+        if (this_equipment.timeout?)
+          return nil
+        else
+          return this_equipment.response
+        end
+      rescue Exception => e
+        puts e.to_s+"\n"+e.backtrace.to_s
+        return nil
+      end
+    end
+    def parse_response(response,wait_for_string)
+      result = response.match(/Network\sAdded:\s+If-1:((?:\d{1,3}\.){3}\d{1,3})/).captures[0].to_s
+      puts " +++++++++ Result: #{result} ++++++++++++++"
+      if (result)
+        return true
+      elsif (result == nil)
+        return false
+      else 
+        return nil
+      end
+    end
+    def disconnect(equipment)
+      this_equipment = @equipment["#{equipment}"]
+      if this_equipment.target.telnet || this_equipment.target.serial
+        this_equipment.disconnect
+      end
+    end
+
+    def debug_puts(message)
+      if @show_debug_messages == true
+        puts(message)
+      end
+    end
+
