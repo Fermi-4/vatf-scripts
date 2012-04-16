0ff3a0057bd011f721b557474ec118e7d0a9ef7c
Carlos Hernandez <ceh@ti.com>
2012-04-16 12:27:31 -0400

    LSP: POWER: make power_s2ram_func less strict
    DUT sometimes take long time (up to 30 seconds) to suspend.
    This patch increases the wait time and also adds debug data.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- LSP/A-Power/power_s2ram_func.rb
+++ LSP/A-Power/power_s2ram_func.rb
@@ -18,11 +18,17 @@ def run
   end
   sleep 5                           # Give tests thread a 5 secs head start
   suspend_thr = suspend_resume_loop
+  while status = test_thr.status
+    puts "test_thr status=#{status}"
+    puts "suspend_thr status=#{suspend_thr.status.to_s}"
+    sleep 1
+  end
   result = test_thr.value           # This will block (join) until test_thr completes
   set_result(result[0], result[1]) 
   @stop_test = true
   suspend_thr.join
   query_pm_stats
+  show_execution_logs
 end
 
 def get_ip
@@ -68,12 +74,14 @@ def start_target_tests
     @equipment['dut1'].target.telnet.send_cmd("chmod +x test.sh",@equipment['dut1'].prompt)
     @equipment['dut1'].log_info("Telnet Data: \n #{@equipment['dut1'].target.telnet.response}")
     cmd_timeout = @test_params.params_control.instance_variable_defined?(:@timeout) ? @test_params.params_control.timeout[0].to_i : 600
-    cmd_timeout *= ((suspend_time + resume_time + 5) / resume_time)       #5 is max wait time to suspend
+    cmd_timeout *= ((suspend_time + resume_time + 15) / resume_time)       #15 is approx max observed wait time to suspend
     @start_suspend_loop = true
     
-    while ((Time.now - time) < @test_params.params_control.test_duration[0].to_f && !failure && !@stop_test )
+    #while ((elapsed = Time.now - time) < @test_params.params_control.test_duration[0].to_f && !failure && !@stop_test )
+    while ((elapsed = Time.now - time) < 10800 && !failure && !@stop_test )
       begin
-        
+        #puts "At least #{@test_params.params_control.test_duration[0].to_f - elapsed} seconds to go"
+        puts "At least #{10800 - elapsed} seconds to go"
         @equipment['dut1'].target.telnet.send_cmd("./test.sh 2> stderr.log > stdout.log 3> result.log",@equipment['dut1'].prompt, cmd_timeout)
       rescue Timeout::Error => e
         @equipment['dut1'].log_info("Telnet TIMEOUT ERROR. Data START:\n #{@equipment['dut1'].target.telnet.response}\nTelnet Data END")
@@ -101,13 +109,23 @@ def suspend_resume_loop
   thr = Thread.new {
     while (!@stop_test) 
       #Suspend
-      @equipment['dut1'].send_cmd("sync; echo mem > /sys/power/state", /Freezing remaining freezable tasks/, 5)
-      raise "DUT took more than 5 seconds to suspend" if @equipment['dut1'].timeout?
+      puts "GOING TO SUSPEND DUT"
+      @equipment['dut1'].send_cmd("sync; echo mem > /sys/power/state", /Freezing remaining freezable tasks/, 60)
+      if @equipment['dut1'].timeout?
+        puts "Timeout while waiting to suspend"
+        raise "DUT took more than 60 seconds to suspend" 
+      end
       sleep suspend_time    
       # Resume from console
-      @equipment['dut1'].send_cmd("", @equipment['dut1'].prompt, 5)  # Try to resume 2
-      @equipment['dut1'].send_cmd("", @equipment['dut1'].prompt, 5)  # times, just in case
-      raise "DUT took more than 5 seconds to resume" if @equipment['dut1'].timeout?
+      puts "GOING TO RESUME DUT"
+      @equipment['dut1'].send_cmd("\n\n\n", @equipment['dut1'].prompt, 1)  # Try to resume
+      @equipment['dut1'].send_cmd("\n\n\n", @equipment['dut1'].prompt, 1)  # Try to resume 
+      @equipment['dut1'].send_cmd("\n\n\n", @equipment['dut1'].prompt, 1)  # Try to resume 
+      @equipment['dut1'].send_cmd("", @equipment['dut1'].prompt, 15)  # One last time
+      if @equipment['dut1'].timeout?
+        puts "Timeout while waiting to resume"
+        raise "DUT took more than 15 seconds to resume" 
+      end
       sleep resume_time 
     end
   }
@@ -119,5 +137,10 @@ def query_pm_stats
   @equipment['dut1'].send_cmd("cat /debug/pm_debug/count", @equipment['dut1'].prompt) 
   @equipment['dut1'].send_cmd("cat /sys/devices/system/cpu/cpu0/cpufreq/stats/time_in_state", @equipment['dut1'].prompt)
   @equipment['dut1'].send_cmd("find /sys/devices/system/cpu/cpu0/cpuidle/ -name \"state*\" -exec cat {}/time \\;", @equipment['dut1'].prompt)
-  
+end
+
+def show_execution_logs
+  @equipment['dut1'].send_cmd("cd #{@linux_dst_dir}",@equipment['dut1'].prompt)
+  @equipment['dut1'].send_cmd("cat stdout.log",@equipment['dut1'].prompt,120)
+  @equipment['dut1'].send_cmd("cat stderr.log",@equipment['dut1'].prompt,60)
 end
