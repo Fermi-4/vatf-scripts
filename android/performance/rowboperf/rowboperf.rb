73da7f6887733cb40a9c7260d3ab08270ba962e4
Alejandro Hernandez <ajhernandez@ti.com>
2012-09-17 16:37:34 -0400

    Added code to push needed binaries in rowboperf cpu tests
    Added code to the test script so that is pushes the binaries
    needed for cpu testing with rowboperf
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- android/performance/rowboperf/rowboperf.rb
+++ android/performance/rowboperf/rowboperf.rb
@@ -1,27 +1,34 @@
 require File.dirname(__FILE__)+'/../../android_test_module' 
 include AndroidTest
 
+def setup
+  super
+  bins_list = send_host_cmd("find #{File.join(@test_params.var_test_libs_root,"armeabi-v7a/RowboPERF_binaries/*")}").split(/[\n\r]+/)
+  bins_list.each do |curr_bin|
+    send_adb_cmd("push #{curr_bin} /system/bin/")
+  end
+end
+
 def run 
   regexp = Hash.new
-  cmd = "shell /system/bin/sh /data/data/com.ti.android.apps.launcher/files/" + @test_params.params_chan.app_name[0]
-  #send apps command 
-  data = send_adb_cmd cmd
-  regexp["runDhrystone"]= /Dhrystones\s*per\s*Second:\s*([0-9]+.[0-9])+/
-  regexp["runWhetstone"]= /Converted\s*Double\s*Precision\s*Whetstones:\s*([0-9]+.[0-9]+)\s*MIPS/
-  regexp["runLinpack"]= /Unrolled\s*Single\s*Precision\s*([0-9]+)\s*Kflops\s*;\s*[0-9]+\s*Reps/
+  data = send_adb_cmd("shell " + @test_params.params_chan.app_name[0])
+  regexp["dhrystone2 10000000"]= /Dhrystones\s*per\s*Second:\s*([0-9]+.[0-9])+/
+  regexp["whetstone 20000"]= /Converted\s*Double\s*Precision\s*Whetstones:\s*([0-9]+.[0-9]+)\s*MIPS/
+  regexp["linpack"]= /Unrolled\s*Single\s*Precision\s*([0-9]+)\s*Kflops\s*;\s*[0-9]+\s*Reps/
   reading = data.scan(regexp[@test_params.params_chan.app_name[0]])  
-  puts reading 
+  puts reading.to_s 
   result,comment,perfdata = save_results(reading[0])
   set_result(result,comment,perfdata)
 end
 
 def save_results(reading)
+   return [FrameworkConstants::Result[:fail], "Test case FAILED."] if !reading
    units = Hash.new 
-   units["runDhrystone"]= "sec"
-   units["runWhetstone"]= "MIPS"
-   units["runLinpack"]= "Kflops"
+   units["dhrystone2 10000000"]= "dhrystones/sec"
+   units["whetstone 20000"]= "MIPS"
+   units["linpack"]= "Kflops"
    perf_data = [];
    perf_data << {'name' => @test_params.params_chan.app_name[0], 'value' =>reading, 'units' => units[@test_params.params_chan.app_name[0]]}
   # I have to add pass-fail criteria once known
-  [FrameworkConstants::Result[:pass], "Test case PASS.",perf_data]
+  [FrameworkConstants::Result[:pass], "Test case PASSED.",perf_data]
 end
