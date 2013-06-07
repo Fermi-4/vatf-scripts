8b26341c0671dc3f6c8b59b6688e710a2a46149f
Alejandro Hernandez <ajhernandez@ti.com>
2013-06-07 12:44:19 -0400

    Modify the storage performance script to use refactor metrics lib
    This patch modifies the storageio_perf test script to use the refactored
    metrics library.
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- android/performance/StorageIO/storageio_perf.rb
+++ android/performance/StorageIO/storageio_perf.rb
@@ -36,7 +36,13 @@ def run
     if iter == 0
       test_data = run_test(@test_params.params_chan.test_option[0].sub(/-e\s+location\s+#{device}\s+-e/,"-e location #{dev_node} -e"))
     elsif @test_params.params_control.instance_variable_defined?(:@collect_stats)
-      start_collecting_stats(@test_params.params_control.collect_stats,2){|cmd| send_adb_cmd("shell #{cmd}")}
+      start_collecting_stats(@test_params.params_control.collect_stats,2){|cmd, stat| 
+        if stat == 'proc_mem'
+          send_adb_cmd("shell #{cmd} com.ti.android.apps.storage")
+        else
+          send_adb_cmd("shell #{cmd}")
+        end
+      }
       run_test(@test_params.params_chan.test_option[0].sub(/-e\s+location\s+#{device}\s+-e/,"-e location #{dev_node} -e"))
       sys_stats = stop_collecting_stats(@test_params.params_control.collect_stats)
     end
@@ -53,20 +59,19 @@ def run
       @results_html_file.add_row_to_table(res_table,[data[0][0], data[0][1], data[0][2]])
     end
   end
-  perfdata.concat(sys_stats)
   @results_html_file.add_paragraph("")
-  systat_names = []
-  systat_vals = []
-  sys_stats.each do |current_stat|
-    systat_vals << current_stat['value']
-    current_stat_plot = stat_plot(current_stat['value'], current_stat['name']+" plot", "sample", current_stat['units'], current_stat['name'], current_stat['name'], "system_stats")
-    plot_path, plot_url = upload_file(current_stat_plot)
-    systat_names << [current_stat['name']+' ('+current_stat['units']+')',nil,nil,plot_url]
+  if sys_stats
+    sys_stats.each do |current_stats|
+      perfdata.concat(current_stats)
+      current_stats.each do |current_stat|
+        current_stat_plot = stat_plot(current_stat['value'], current_stat['name']+" plot", "sample", current_stat['units'], current_stat['name'], current_stat['name'], "system_stats")
+        plot_path, plot_url = upload_file(current_stat_plot)
+        @results_html_file.add_paragraph("")
+        res_table2 = @results_html_file.add_table([[current_stat['name']+' ('+current_stat['units']+')',{:bgcolor => "33CC66", :colspan => "#{current_stat['name'].length}"},{:color => "blue"},plot_url]],{:border => "1",:width=>"20%"})
+        @results_html_file.add_rows_to_table(res_table2,[current_stat['value']].transpose)
+      end
+    end
   end
-  @results_html_file.add_paragraph("")
-  res_table2 = @results_html_file.add_table([["Sytem Stats",{:bgcolor => "336666", :colspan => "#{systat_names.length}"},{:color => "white"}]],{:border => "1",:width=>"20%"})
-  @results_html_file.add_row_to_table(res_table2, systat_names)
-  @results_html_file.add_rows_to_table(res_table2,systat_vals.transpose)
   @results_html_file.add_paragraph(test_data['response'],nil,nil,nil)
   ensure
     if test_data && test_data['perf_data']
