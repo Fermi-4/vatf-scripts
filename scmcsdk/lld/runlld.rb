c4e1e6b5d68b4fa1d32f09a64ce279901c599832
Jonathan Hsu <j-hsu4@ti.com>
2013-07-11 18:22:41 -0400

    scmcsdk update runlld scripts for nightly build
    Signed-off-by: Elvita Lobo <elvita@ti.com>

--- scmcsdk/lld/runlld.rb
+++ scmcsdk/lld/runlld.rb
@@ -1,3 +1,7 @@
+###########################
+## FOR THE NIGHTLY BUILD ##
+###########################
+
 require 'fileutils'
 
 require File.dirname(__FILE__)+'/../../LSP/default_test_module'
@@ -18,110 +22,41 @@ end
 
 #? how do errors get passed up the chain?
 
-#@	commands is a semi-colon separated array of commands (type string) to run. each command is composed of executable files
-	#Example commands = "/usr/bin/qmScfgTest_k2h.out;rmServer_k2h.out global-resource-list_k2h.dtb policy_dsp_arm_k2h.dtb"
+#@	commands is a semi-colon separated array of commands (type string) to run. the VERY FIRST command is the test file to run
+	#Example commands = "/usr/bin/qmScfgTest_k2h.out;cd /usr/bin/;rmServer_k2h.out global-resource-list_k2h.dtb policy_dsp_arm_k2h.dtb;cd"
 #@	constraints is a semi-colon separated array of constraints/criteria for run() to compare against
 	#Example constraints = "Static Memory region configuration tests Passed"
 #@	iterations is the number of times run() will run the commands
-def run
-	##get the files from ftp server
-	#ftp "mcsdk/test/314_armv7.tar.gz"
-	#get tarball. untar everything to /usr/bin/. run ___
-	testFolder = @test_params.test_out_files
-	@equipment['server1'].send_cmd("cp #{testFolder} #{@equipment['server1'].tftp_path}/armv7.tar.gz",@equipment['server1'].prompt, 20)
-	@equipment['dut1'].send_cmd("cd /usr/bin/",@equipment['dut1'].prompt, 20)
-	@equipment['dut1'].send_cmd("tftp -g -r armv7.tar.gz #{@equipment['server1'].telnet_ip}", @equipment['dut1'].prompt, 20)
-	@equipment['dut1'].send_cmd("tar -xvzf armv7.tar.gz",@equipment['dut1'].prompt, 20)
-	@equipment['dut1'].send_cmd("ls armv7",@equipment['dut1'].prompt, 20)
-	armFolders = @equipment['dut1'].response
-	puts "\n\n\nfolders #{armFolders} \n\n\n"
+#@	lld_test_archive is part of the build description. it holds a flattened (or structured) folder of .dtb and .out files
 	
+def run
 	test_done_result = FrameworkConstants::Result[:fail]
 	comment = "*Test failed.*\n" #initial assumptions
-	
-	##Input parameters
-	file_list = Array.new
-	commands = @test_params.params_control.commands
-	commands.each do |files|
-		files.split(" ").each {|file| file_list.push(file)}
-	end
+
 	#directory and name of file to test
-	test_cmd = @test_params.params_control.commands[0].to_s
-	server_cmd = @test_params.params_control.commands[1].to_s
+	commands = @test_params.params_control.commands
+	test_cmd = commands[0].to_s
 	#criteria[0] is a phrase signifying test completion. criteria[!0] signifies a passing test. 
 	criteria = @test_params.params_control.constraints
 	#number of times to run the test
 	iterations = @test_params.params_control.iterations[0].to_i
-
-#NOTE: possible to search for the file somewhere else, then run it from there. or even copy the file to the local folder
-	##get additional files from ftp server
-	inputfile= @test_params.lld_dtb
-	@equipment['server1'].send_cmd("cp #{inputfile} #{@equipment['server1'].tftp_path}/lld_dtb.tar.gz",@equipment['server1'].prompt, 20)
-	@equipment['dut1'].send_cmd("tftp -g -r lld_dtb.tar.gz #{@equipment['server1'].telnet_ip}", @equipment['dut1'].prompt, 20)
-	@equipment['dut1'].send_cmd("tar -xvzf lld_dtb.tar.gz",@equipment['dut1'].prompt, 20)
-	@equipment['dut1'].send_cmd("ls",@equipment['dut1'].prompt, 20)
-
-	##proceed if all specified files exist, else return <file not found>
-	file_version = ""
-	file_list.each do |tag|
-		##parse the folder location
-		folder = tag[/.*\//]
-		if folder
-			file = tag[folder.length,tag.length]
-		else 
-			folder = ""
-			file = tag
-		end
-		file_extension = tag[/\.{1}\w+/]
-		##ensure that file versions are the same
-		file_version += tag[/[a-zA-Z]\.{1}\w+/][0]
-		if file_version.length > 1
-			(file_version.length-1).times do |i|
-				if file_version[i] != file_version[i+1]
-					comment = "file versions are different: #{commands}. " 
-				end
-			end
-			file_version = file_version[0]
-		end
-		##check that the file exists, where specified
-		ls_cmd = "ls #{folder}*#{file_extension}"
-		@equipment['dut1'].send_cmd(ls_cmd, /#{file}/i, 10)
-		puts "Command Sent"
-		if @equipment['dut1'].timeout?
-			@equipment['dut1'].send_cmd(ls_cmd.insert(3, "/usr/bin/"), /#{file}/i, 10)
-			if @equipment['dut1'].timeout?
-				comment += "File #{file} not found. Check input #{@test_params.params_control.commands} for correctness"
-				set_result(test_done_result,comment)
-				return
-			else
-				puts "time out resolved. found in /usr/bin/"
-			end
-		end
+	test_folder_location = nil
+	#REMOVED FOR NIGHTLY#test_folder_location = @test_params.lld_test_archive
+	
+	##Prepare for running tests. Bundle up file building, server starting
+	prep_results= prepare_for_test(test_folder_location,commands)	
+	comment += prep_results[1]
+	if !prep_results[0]
+		set_result(test_done_result,comment)
+		return
 	end
-
-	##run the rmServer when requested
-	if server_cmd and file_list.length > 1
-#!! change to make output a testlink var
-		rm_result = rmServer_up?(server_cmd, output = "rmServerOutput.txt")	
-		#rm_result = [up?, comments, rmServer_PID]
-		if rm_result[0]
-			comment += rm_result[1] + "rmServer_k2#{file_version} up and running. "
-			puts "rmServer up and running. "
-			pid_info = rm_result[2]
-		else
-			comment += "rmServer did not run. "
-			set_result(test_done_result,comment)
-			return
-		end
-		#to kill the rmServer after this test
-		#@equipment['dut1'].send_cmd("kill #{pid_info}",@equipment['dut1'].prompt,10)
-	end
-
+	
 	##see if test passes and satisfies criteria
 	comment = "\nRunning Test.\n"
 	iterations.times do |count|
 		#send_cmd, store buffer into std_response
 		@equipment['dut1'].send_cmd(test_cmd, /#{criteria[0]}/i, 20)
+		puts "Command Sent"
 		std_response = @equipment['dut1'].response.to_s
 		if @equipment['dut1'].timeout?
 			puts comment += "**On iteration #{count +1}, FAILURE to meet end criteria**"
@@ -156,18 +91,14 @@ def run
 				return
 			end
 		end
-		puts "Done with iteration #{count +1}. "
+		@equipment['server1'].send_cmd("echo 'Done with iteration #{count +1}.", @equipment['dut1'].prompt, 20)
 	end
-	
-	# EL edits
-	#test = @test_params.params_control.test_tag[0].to_s
-	#comment += "Ran test #{test}"
-	# end EL edits
 
 	##close rmServer upon completion if it was up, after each test
-	if server_cmd and rm_result and file_list.length > 1
-		#find pid here?
-		#@equipment['dut1'].send_cmd("ps | grep rmserver",@equipment['dut1'].prompt,10) 
+	@equipment['dut1'].send_cmd("ps | grep rmServer", @equipment['dut1'].prompt, 20)
+	processes = @equipment['dut1'].response.to_s
+	if processes[/rmServer_k2/i]
+		pid_info = @equipment['dut1'].response.to_s[/[0-9]+/].to_i	#find pid here
 		begin  
 			@equipment['dut1'].send_cmd("kill #{pid_info}",@equipment['dut1'].prompt,10)
 		end while @equipment['dut1'].timeout?
@@ -175,46 +106,168 @@ def run
 	end
 	
 	##DONE
-	puts "\nDone with #{test_cmd}"
-	puts comment += "\n#{test_cmd} *TEST PASSED*"
+	@equipment['server1'].send_cmd("echo 'Done with #{test_cmd}\n#{test_cmd} *TEST PASSED*'", @equipment['dut1'].prompt, 20)
+	comment += "\n#{test_cmd} *TEST PASSED*"
 	test_done_result = FrameworkConstants::Result[:pass]
 	set_result(test_done_result,comment)
 end
 
-#looks to see if the server is running, and starts it if it is not up already
-# returns [running=true|not_running=false, comments, rmServer pid]
-#runCmd = "rmServer_k2#{file_version}.out /usr/bin/global-resource-list_k2#{file_version}.dtb /usr/bin/policy_dsp_arm_k2#{file_version}.dtb > #{output} &"
-def rmServer_up? (server_cmd = "", output = "rmServerOutput.txt", file_version = "hk")
+##Do most of the preparation for testing: archive building, file checking, and server starting
+def prepare_for_test(test_folder_location,commands)
+	comment = ""
+	##Set up files from archive
+	build_files(test_folder_location) 
+	file_list = Array.new
+	
+	##Parse commands for files needed
+	files_result = good_files_specified?(commands)
+	if files_result[0]
+		file_list = files_result[2]
+		#comment += files_result[1]
+	else
+		comment += files_result[1] + "Files not consistent. "
+		return [false, comment]
+	end	
+	other_cmds = commands[1...commands.length]
+	@equipment['server1'].send_cmd("echo 'Checked input. Preparing for test.\n'", @equipment['dut1'].prompt, 20)
+	##Run the rmServer when requested
+	if other_cmds[0] and file_list.length > 1 and other_cmds.to_s[/rmServer/]
+		rm_result = rmServer_up?(other_cmds)
+		#rm_result = [up?, comments]
+		if rm_result[0]
+			@equipment['server1'].send_cmd("echo '#{rm_result[1]} + rmServer up and running. \n'", @equipment['dut1'].prompt, 20)
+			puts "rmServer up and running. "
+		else
+			comment += rm_result[1] + "RmServer did not run. "
+			return [false, comment]
+		end
+	elsif other_cmds[0] 
+		other_cmds.each do |cmd| 
+			@equipment['dut1'].send_cmd(cmd, @equipment['dut1'].prompt, 10)
+			if @equipment['dut1'].timeout?
+				comment += "cmd #{cmd} not send correctly. "
+				return [false, comment]
+			end
+		end
+	end
+	return [true,comment]
+end
+
+##copy archive from ftp server location "@test_params.lld_test_archive" to linux_host
+##untar the archive in the /usr/bin/ directory of the EVM, flattening its file structure
+def build_files (test_folder_location)
+#NOTE: possible to search for the file somewhere else, then run it from there. or even copy the file to the local folder
+	# get tar.gz file. untar everything to /usr/bin/
+	if test_folder_location
+		@equipment['server1'].send_cmd("cp #{test_folder_location} #{@equipment['server1'].tftp_path}/test_archive.tar.gz",@equipment['server1'].prompt, 20)
+		@equipment['dut1'].send_cmd("cd /usr/bin/",@equipment['dut1'].prompt, 10)
+		@equipment['dut1'].send_cmd("tftp -g -r test_archive.tar.gz #{@equipment['server1'].telnet_ip}", @equipment['dut1'].prompt, 20)
+		@equipment['dut1'].send_cmd("tar -xvzf test_archive.tar.gz --recursive-unlink",/#{@equipment['dut1'].prompt}/, 20)
+		@equipment['dut1'].send_cmd("cd",/#{@equipment['dut1'].prompt}/, 10)
+	end
+end
+
+##Make a array of files needed to run the test, ensure they are version consistent, check these files to ensure they exist
+# returns [good_files_specified=true|bad_file_specification=false, comments, file_list]
+def good_files_specified?(commands)
+	file_list = Array.new
+	comment = ""
+	##Look through all commands and see if they require files
+	commands.each do |command|
+	if command
+			command.split(" ").uniq.each do |piece_of_command|
+				folder = piece_of_command[/.*\//]
+				if folder
+					file = piece_of_command[folder.length,piece_of_command.length]
+				else
+					file = piece_of_command
+				end
+				if file =~ /\.{1}\w+/
+					file_list.push(piece_of_command)
+					comment += "Added #{file}. "
+				else
+					#comment += "Ignored #{file}. "
+				end
+			end
+		end
+	end
+	
+	##Proceed if all specified files exist, else return <file not found>
+	file_version = ""
+	file_list.each do |tag|
+		##parse the folder location
+		folder = tag[/.*\//]
+		if folder
+			file = tag[folder.length,tag.length]
+		else 
+			folder = ""
+			file = tag
+		end
+#!! Version parsing may be different
+		##ensure that file versions are the same
+		file_version += tag[/[a-zA-Z]\.{1}\w+/][0]
+		if file_version.length > 1
+			(file_version.length-1).times do |i|
+				if file_version[i] != file_version[i+1]
+					comment += "File versions are different: '#{tag}' in '#{commands}'. " 
+				end
+			end
+			file_version = file_version[0]
+		end
+		file_extension = file[/\.{1}\w+/]
+		##check that the file exists, where specified, or in /usr/bin/
+		if file_extension != ".txt"
+			ls_cmd = "ls #{folder}*#{file_extension}"
+			@equipment['dut1'].send_cmd(ls_cmd, /#{file}/i, 10)
+			if @equipment['dut1'].timeout?
+				@equipment['server1'].send_cmd("echo 'dealing with timeout in usrbin'", @equipment['dut1'].prompt, 10)
+				@equipment['dut1'].send_cmd(ls_cmd.insert(3, "/usr/bin/"), /#{file}/i, 10)
+				if @equipment['dut1'].timeout?
+					comment += "File #{file} not found. Check input #{@test_params.params_control.commands} for correctness. "
+					return [false, comment, file_list]
+				else
+					@equipment['server1'].send_cmd("echo 'time out resolved. found in usrbin'", @equipment['dut1'].prompt, 10)
+				end
+			end
+		end
+	end
+	@equipment['server1'].send_cmd("echo 'Files all checked.\n'", @equipment['dut1'].prompt, 10)
+	return [true, comment, file_list]
+end
+
+#See if the server is running, and starts it if not up already
+# returns [running=true|not_running=false, comments]
+#["cd /usr/bin/","rmServer_k2h.out /usr/bin/global-resource-list_k2h.dtb /usr/bin/policy_dsp_arm_k2h.dtb > output.out &","cd"]
+def rmServer_up? (other_cmds = [], file_version = "hk")
+	comment = ""
 	running = false
 	##test to see that the rmServer is running. Start it if its not running
 	@equipment['dut1'].send_cmd("ps | grep rmServer", /rmServer_k2[#{file_version}]/i, 5)
 	if @equipment['dut1'].timeout?			#run a new rmServer in the background
-		comment = "rmServer not running initially."
-		if server_cmd.phrases < 3
-			return [false, "server_cmd has too few arguments - has #{server_cmd.phrases}; needs 3",nil]
-		end
-		runCmd = "#{server_cmd} > #{output} &"
-		@equipment['dut1'].send_cmd(runCmd, @equipment['dut1'].prompt, 10)
-		if @equipment['dut1'].timeout?
-			comment += "server did not start "
-			return [false, comment ,nil]
+		comment = "rmServer not running initially. "
+		other_cmds.each do |cmd| 
+			comment += "Running '#{cmd}.' "
+			@equipment['dut1'].send_cmd(cmd, @equipment['dut1'].prompt, 10)
+			if @equipment['dut1'].timeout?
+				comment += "cmd #{cmd} not send correctly. "
+				return [false, comment]
+			end
+			comment += "'#{cmd}' run. "
 		end
 		#test to see that the rmServer is now running.
 		@equipment['dut1'].send_cmd("ps | grep rmServer", /rmServer_k2[#{file_version}]/i, 10)
 		if @equipment['dut1'].timeout?
-			comment += "Prompt not received. "
-			return [false, comment ,nil]
+			comment += "Server did not start. "
+			return [false, comment]
 		end
 		#may need to wait longer for the rmServer to be able to take input
-		
 		comment += "Successfully started rmServer in background. "
 	else
 		comment = "rmServer running initially. "
 	end
+	puts "**SERVER UP**\n"
 	running = true
-	pid_info = @equipment['dut1'].response.to_s[/[0-9]+/].to_i
-	puts "\nPID info #{pid_info}\n"
-	return [running, comment, pid_info]
+	return [running, comment]
 end
 
 #parse output to check consistency of output
@@ -262,6 +315,14 @@ def internally_consistent? (output, type_of_check = "", criterion = "default")
 			return [false, internal_comment]
 		end
 		#make sure that if the test outputs the String "Failed", it was because nothing Failed
+		if criterion.downcase[/failed/i]
+			line = output[/failed \s* : .* \d+ \S*/ix]
+			#line = line[line[/.*:/].length,line.length]
+			if line != nil and line[/\d+/] and (line[/\d+/].to_i != 0) #if there is a number at the end.
+				comment += ". Failed more than 0 times. #{line}"
+				return [false, comment]
+			end
+		end
 	end
 
 	#std_response 
