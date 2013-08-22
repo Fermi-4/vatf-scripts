f032f4136a98909d9a76d2b48930797b547b73e9
Yebio <yebio@yebio-System-Product-Name.(none)>
2013-08-22 17:56:37 -0230

    VATF-SCRIPT: ADC and TSC automation.
    adc_test.rb and tsc_adc_test.rb scripts are created
    to automate ADC testing. tsc_adc_test.rb tests ADC
    by generating touch event using a tool that
    touches on the screen, then data generated is verified
    systematically. adc_test.rb tests data by sending
    analong data on the ADC channels expoxed to the outside
    and make sure the digitized data is valid.
    
    Signed-off-by: Yebio Mesfin <ymesfin@ti.com>
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

---
+++ LSP/A-ADC/adc_test.rb
@@ -0,0 +1,135 @@
+require File.dirname(__FILE__)+'/../default_test_module' 
+
+
+def setup
+  # HP wave generator is connected to host through serial connection. 
+  # HP wave generator analog autput is connected to jumpers for ADC input.
+  add_equipment('wavegen1') do |log_path|
+    HpWaveGenDriver.new(@equipment['dut1'].params['wavegen1'],log_path)
+  end
+  # Connect to multimeter and bench entry should look like the following. 
+  #winfo = EquipmentInfo.new("wavegen")
+  #winfo.driver_class_name = "HpWaveGenDriver"
+  #winfo.serial_port = '/dev/ttyUSB1'
+  #winfo.serial_params = {"baud" => 9600, "data_bits" => 8, "stop_bits" => 1,   "parity"=>SerialPort::NONE}
+  @equipment['wavegen1'].connect({'type'=>'serial'})
+  @equipment['dut1'].connect({'type'=>'serial'})
+  super
+end
+
+def run
+  perf = []
+  #vin = 1 # in volt 
+  #vref = 1.8 # reference voltage 
+  vin = @equipment['dut1'].params['vin'] # in volt 
+  vref = @equipment['dut1'].params['vref'] # reference voltage 
+  channels = @equipment['dut1'].params['channels']
+  #D = Vin * (2^n - 1) / Vref # theoretical digital represention of the input voltage 
+  # Configure multimeter 
+  @equipment['wavegen1'].configure_wave_gen()
+  #This mode configuration is not yet decided on the main kernel
+  mode = 'oneshot' # continuous
+  #Configure ADC  
+  #config_adc(mode) this function call is for future use. The developers are debating as of now. 
+  test_name = @test_params.params_chan.test_name[0]
+  puts "test name is #{test_name}"
+  case test_name 
+  when "cont"
+    test_status = cont_test(channels,vin, vref)
+  when "oneshot"
+    test_status = oneshot_test(channels,vin,vref)
+  else 
+    puts "undefined test case!"
+    test_status = 0
+  end 
+  
+  if test_status > 0
+    set_result(FrameworkConstants::Result[:pass], "ADC Test Pass","")
+  else
+    set_result(FrameworkConstants::Result[:fail], "ADC Test Pass","")   
+  end  
+end 
+
+#Function configures platform's ADC.
+# Input parameters: mode is to set it to continoues or oneshot 
+# Return Parameter: none 
+
+def config_adc(mode)
+  @equipment['dut1'].send_cmd("echo #{mode} > /sys/bus/iio/devices/iio\:device0/mode", @equipment['dut1'].prompt)
+end
+
+#Function configures does ADC continoues test.
+# Input parameters: number of channels supported by platform ADC, voltage input and voltage refrence 
+# Return Parameter: test status  
+def cont_test(channels, vin, vref)
+  dReadings =  Hash.new()
+  channels.each do |chan|
+    dReadings[chan.to_s] = Array.new()
+  end
+  #dtheo = vin * (2**12 - 1) / vref
+  wave_gen_cmd = 'OUTPut:LOAD 50'
+  @equipment['wavegen1'].gen_wave_cmd(wave_gen_cmd)
+  sleep 1
+  vin_shift =  vin/2 
+  wave_gen_cmd =  "APPL:SIN #{@test_params.params_chan.freq[0]}, #{vin}, #{vin_shift}"
+  @equipment['wavegen1'].gen_wave_cmd(wave_gen_cmd)
+  sleep 1
+  fail = 0
+  20.times do
+      channels.each do |chan|
+          @equipment['dut1'].send_cmd("cat /sys/bus/iio/devices/iio\:device0/in_voltage#{chan}_raw", @equipment['dut1'].prompt)
+          dread =  @equipment['dut1'].response.to_s.match(/raw\s+([0-9]+)/).captures[0]
+          vd = ((dread.to_f * vref).to_f/ (2**12 - 1).to_f).round(1)
+          dReadings[chan.to_s] << vd
+      end 
+  end 
+  dReadings.each do |key,values|
+    dreadingMax = dReadings[key].max
+    dreadingMin = dReadings[key].min
+    if (dreadingMax).round(2) > vin || dreadingMin < 0 
+       puts "ADC FAILED on channel #{key} #################." 
+       fail = 1 
+    else 
+       puts puts "ADC SUCCESS ON CHANNEL #{key} #################."
+    end  
+  end      
+  return fail 
+end  
+
+#Function configures does ADC oneshot test.
+# Input parameters: number of channels supported by platform ADC, voltage input and voltage refrence 
+# Return Parameter: test status  
+
+def oneshot_test(channels, vin, vref)
+  #dtheo = vin * (2**12 - 1) / vref
+  wave_gen_cmd = 'APPLy:DC #{vin}'
+  @equipment['wavegen1'].gen_wave_cmd(wave_gen_cmd)
+  sleep 1
+  wave_gen_cmd = 'OUTPut:LOAD INFinity'
+  @equipment['wavegen1'].gen_wave_cmd(wave_gen_cmd)
+  sleep 1
+  wave_gen_cmd =  "VOLTage:OFFSet #{vin}"
+  @equipment['wavegen1'].gen_wave_cmd(wave_gen_cmd)
+  sleep 1
+  fail = 0
+  channels.each do |chan|
+      @equipment['dut1'].send_cmd("cat /sys/bus/iio/devices/iio\:device0/in_voltage#{chan}_raw", @equipment['dut1'].prompt)
+      dread =  @equipment['dut1'].response.to_s.match(/raw\s+([0-9]+)/).captures[0]
+      #ddelta =  (dtheo - dread.to_i).abs
+      vd = ((dread.to_f * vref).to_f/ (2**12 - 1).to_f).round(1)
+      vdelta = (vin - vd.to_i).abs 
+      if vdelta > 0.2 
+         puts "ADC FAILED ON CHANNEL #{chan}" 
+         puts "Channel #{chan} : Voltage in #{vin} : ADC Value #{vd}"
+         fail = 1 
+      else 
+        puts puts "ADC SUCCESS ON CHANNEL #{chan}"
+        puts "Channel #{chan} : Voltage in #{vin} : ADC Value #{vd}"
+      end  
+  end 
+  return fail
+end  
+
+def clean
+
+end 
---
+++ LSP/A-ADC/tsc_adc_test.rb
@@ -0,0 +1,130 @@
+require File.dirname(__FILE__)+'/../default_test_module' 
+
+
+def setup
+  @equipment['dut1'].connect({'type'=>'serial'})
+  # Bench entry is documented in the sample bench.rb 
+  add_equipment('relay') do |log_path|
+    DevantechRelayController.new(@equipment['dut1'].params['relay'].keys[0],log_path)
+  end
+  super
+end
+
+def run
+  test_status = 0
+  tsc_filtered_data = Hash.new()
+  tsc_filtered_data['x_value'] = Array.new()
+  tsc_filtered_data['y_value'] = Array.new() 
+  flag = 0 
+  # making sure valid ports are defined on the bench.
+  # Bench dut.params = {'relay' => {pwr => [6,3, 5,4]}}
+  # In the sample bench entry above, four relay portes are used
+  # control selonoids. The power to the selonoid could be turned on one at 
+  # a time. 
+  @equipment['dut1'].params['relay'].values[0].each do |power_port|
+      if power_port ==nil
+        raise "You need Ethernet Relay connectivity to run this test"
+      end
+  end 
+  @equipment['dut1'].send_cmd("lsinput", @equipment['dut1'].prompt, 3)
+  str = @equipment['dut1'].response
+  tsc_event = get_event(str)
+  if tsc_event == "" 
+    puts "No event detected for ti-tsc\n"
+  end  
+  for i in 0..@test_params.params_chan.num_iter[0].to_i
+      @equipment['dut1'].params['relay'].values[0].each do |power_port|
+        @equipment['dut1'].send_cmd("input-events #{tsc_event.match(/([0-9]+)/).captures[0]}", @equipment['dut1'].prompt, 3)
+        do_touch_screen(power_port)
+        @equipment['dut1'].send_cmd("", @equipment['dut1'].prompt, 3)
+        tsc_data = @equipment['dut1'].response
+        tsc_filtered_data['x_value'] << get_tsc_data(tsc_data)['x_value'].to_s
+        tsc_filtered_data['y_value'] << get_tsc_data(tsc_data)['y_value'].to_s
+      end
+   puts "TSC DATA ##########################################"
+   puts "x value #{tsc_filtered_data['x_value']}"
+   puts "y value #{tsc_filtered_data['y_value']}"
+   puts "END DATA ###########################################"
+  end  
+  #Verify Result 
+  counter = 0
+  result = ""
+  for outer_index in 0..1
+    inner_counter = 0
+    for innet_index in 0..(@equipment['dut1'].params['relay'].values[0].length - 1)
+      delta_x = tsc_filtered_data['x_value'][counter + innet_index] - tsc_filtered_data['x_value'][innet_index]
+      delta_y = tsc_filtered_data['y_value'][counter + innet_index] - tsc_filtered_data['y_value'][innet_index]
+      if delta_x.to_i > 5 or delta_y.to_i > 5
+        puts "TEst failed: x reference value #{tsc_filtered_data['x_value'][innet_index]}, x read #{tsc_filtered_data['x_value'][counter + innet_index]}"
+        puts "TEst failed: y reference value #{tsc_filtered_data['y_value'][innet_index]}, y read #{tsc_filtered_data['y_value'][counter + innet_index]}"
+        result = "x reference value #{tsc_filtered_data['x_value'][innet_index]}, x read #{tsc_filtered_data['x_value'][counter + innet_index]} + " "+\
+                  y reference value #{tsc_filtered_data['y_value'][innet_index]}, y read #{tsc_filtered_data['y_value'][counter + innet_index]}"
+        inner_counter += inner_counter
+      else 
+        test_status =  1
+      end     
+    end  
+      counter += inner_counter
+  end  
+  if test_status > 0
+    set_result(FrameworkConstants::Result[:pass], "Touch Screen Test Pass",result)
+  else
+    set_result(FrameworkConstants::Result[:fail], "Touch Screen Test Pass",result)   
+  end 
+ 
+end 
+
+#Function does the touch using solenoids.
+# Input parameters: power_port: is the relay port to which the solenoid is connected. 
+# Return Parameter: none  
+
+def do_touch_screen(power_port)
+  @equipment['relay'].switch_on(power_port)
+  puts "turn ON USB Swtich !!!!!!!!!"
+  puts "sleep half second !!!" 
+  sleep 0.5
+  @equipment['relay'].switch_off(power_port)
+end 
+
+#Function filters the touch data report by the platform(input-utils).
+# Input parameters: str: is string data collected from platform. 
+# Return Parameter: filterd data   
+
+def get_tsc_data(str)
+    tsc_data = Hash.new() 
+    x_values = str.to_s.scan(/EV_ABS\s+ABS_X\s+([0-9]+)/)
+    tsc_data['x_value'] = get_average(x_values)
+    y_values = str.to_s.scan(/EV_ABS\s+ABS_Y\s+([0-9]+)/)
+    tsc_data['y_value'] = get_average(y_values)
+    return tsc_data
+end 
+
+#Function calculates the avarage of the x and y values reported by each touch.
+# Input parameters: array_values: are array of x or y values for each touch. 
+# Return Parameter: average  
+
+def get_average(array_values)
+    total = 0 
+    array_values.each do |x|
+      total += x[0].to_i
+    end  
+    return  total / array_values.length      
+end 
+
+#Function gets the touch event number. 
+# Input parameters: str: string data returned by lsinput command of input-utils 
+# Return Parameter: touch event number   
+
+def get_event(str)
+    str.split(/\/dev\/input/).each do |str|
+      if str.include?('ti-tsc')
+        tsc_event = str.to_s.match(/(event[0-9]+)/).captures[0] 
+        return tsc_event
+      end 
+    end 
+    return ""
+end 
+
+def clean
+
+end 
