2fa3f86d42b68a96e1b6128fcc336dd844fe008b
Aparna Balasubramanian <aparnab@ti.com>
2013-08-29 23:29:49 -0500

    Test application to perform usb device tests in parallel with usbhost tests
    This patch modifies an existing test application for usb gadget to run
    usb host tests in parallel. A test case parameter will determine whether
    such a parallel test is to be run as well as indicate what type of host
    test to be run. There are also some fixes to make the original application
    more robust by checking the usb interface on host PC dynamically
    rather than assume it to be usb0.
    
    Signed-off-by: Aparna Balasubramanian <aparnab@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- LSP/A-USB/usb_dev_msc_cdc.rb
+++ LSP/A-USB/usb_dev_msc_cdc.rb
@@ -17,26 +17,62 @@ end
 
 
 def run
+  simultaneous_host_device_test=false
+  test_cmd = Hash.new
+  test_cmd = {'msc'=>"./runltp -P #{@equipment['dut1'].name} -f ddt/usbhost_perf_vfat -s USBHOST_S_PERF_VFAT_0001 ",
+              'audio' =>"./runltp -P #{@equipment['dut1'].name} -f  ddt/usbhost_audio -s USBHOST_S_FUNC_AUDIO_LOOPBACK_ACCESSTYPE_NONINTER_01 ",
+              'video' => "./runltp -P #{@equipment['dut1'].name} -f  ddt/usbhost_video -s USBHOST_M_FUNC_VIDEO_640_480 ",
+              'mscxhci'=>"./runltp -P #{@equipment['dut1'].name} -f ddt/usbhost_perf_vfat -s USBXHCIHOST_S_PERF_VFAT_0001 ",
+              'audioxhci' =>"./runltp -P #{@equipment['dut1'].name} -f  ddt/usbhost_audio -s USBXHCIHOST_S_FUNC_AUDIO_LOOPBACK_ACCESSTYPE_NONINTER_01 ",
+              'videoxhci' => "./runltp -P #{@equipment['dut1'].name} -f  ddt/usbhost_video -s USBXHCIHOST_M_FUNC_VIDEO_640_480 ",
+              }
+  test_command = ''
 
   $result = 0
   command = "modprobe -r g_ether"
+  #@equipment['dut1'].send_cmd(command, @equipment['dut1'].prompt,1)
+  #command = "modprobe -r g_file_storage"
   @equipment['dut1'].send_cmd(command, @equipment['dut1'].prompt,1)
   command = "modprobe -r g_mass_storage"
   @equipment['dut1'].send_cmd(command, @equipment['dut1'].prompt,1)
 
   cmds = @test_params.params_chan.instance_variable_get("@#{'cmd'}").to_s
   $cmd = cmds
+  
+  if (@test_params.params_control.instance_variable_defined?(:@simultaneous_host))
+    puts "This test uses device and host in parallel and params is #{@test_params.params_control.simultaneous_host[0]}\n"
+    simultaneous_host_device_test = true
+    puts "This test uses device and host in parallel and params is #{@test_params.params_control.simultaneous_host[0]}\n"
+    host_type=@test_params.params_control.simultaneous_host[0]
+    case host_type
+      when 'msc', 'video', 'audio', 'mscxhci', 'audioxhci', 'videoxhci'
+      test_command = test_cmd[host_type]
+      else
+         puts "Unsupported Host Device type in test parameter - #{host_type}\n"
+         set_result(FrameworkConstants::Result[:fail], "Testcase has an unsupported device type as parameter.")
+         return
+    end
+    start_usbhost_test(test_command)
+    @stop_test = false
+  end
 
+ @stop_test=false
+ while (!@stop_test)
   case  
     when cmds.match(/usb_dev_msc/) 
       usb_dev_msc()
+      @stop_test=true
     when cmds.match(/usb_dev_cdc/) 
       usb_dev_cdc()
+      @stop_test=true
     else
       $result = 1
       puts "#{cmds} does not match any case\n"
+     if (!simultaneous_host_device_test)
+       @stop_test=true
+     end
   end
-
+ end
   if $result == 0
     set_result(FrameworkConstants::Result[:pass], "Testcase Result is PASS.")  
     else
@@ -45,8 +81,13 @@ def run
     @equipment['dut1'].send_cmd(command, @equipment['dut1'].prompt,1)
     command = "modprobe -r g_mass_storage"
     @equipment['dut1'].send_cmd(command, @equipment['dut1'].prompt,1)
+     if (!simultaneous_host_device_test)
+     @stop_test=true
+    end
+
   end  
 
+ stop_usbhost_test
 end
 
 
@@ -64,6 +105,9 @@ def create_share_memory(command, response_pattern, next_command, timeout)
   else
     $result = 1
     set_result(FrameworkConstants::Result[:fail], "#{command} command could not execute")
+    if (@test_params.params_control.instance_variable_defined?(:@simultaneous_host))
+     @stop_test=true
+    end
     return
   end
 end
@@ -92,6 +136,10 @@ def usb_dev_msc()
   else
     $result = 1
     set_result(FrameworkConstants::Result[:fail], "g_mass_storage.ko module is not available.")  
+     if (@test_params.params_control.instance_variable_defined?(:@simultaneous_host))
+     @stop_test=true
+    end
+
     return     
   end
 
@@ -136,6 +184,11 @@ def usb_dev_msc()
   if response.include?('Error')
     $result = 1
     set_result(FrameworkConstants::Result[:fail], "g_mass_storage.ko insertion fialed.")  
+     #if (simultaneous_host_device_test)
+    if (@test_params.params_control.instance_variable_defined?(:@simultaneous_host))
+     @stop_test=true
+    end
+
     return     
   end
 
@@ -153,6 +206,11 @@ def usb_dev_msc()
     puts "Host does not detect any USB device"
     $result = 1
     set_result(FrameworkConstants::Result[:fail], "Testcase Result is FAIL.")
+    # if (simultaneous_host_device_test)
+     if (@test_params.params_control.instance_variable_defined?(:@simultaneous_host))
+     @stop_test=true
+    end
+
     return
   
   end
@@ -225,11 +283,17 @@ def usb_dev_cdc()
   else
     $result = 1
     set_result(FrameworkConstants::Result[:fail], "g_ether.ko module is not available.")  
+    #if (simultaneous_host_device_test)
+    if (@test_params.params_control.instance_variable_defined?(:@simultaneous_host))
+     @stop_test=true
+    end
+
     return     
   end
 
   command = "dmesg -c"
-  @equipment['dut1'].send_cmd(command, @equipment['dut1'].prompt,10)
+  @equipment['dut1'].send_cmd(command, @equipment['dut1'].prompt,2)
+  @equipment['server2'].send_sudo_cmd(command, @equipment['server2'].prompt,10)
 
   system ("sleep 2")
 
@@ -239,17 +303,50 @@ def usb_dev_cdc()
   if response.include?('not found')
     $result = 1
     set_result(FrameworkConstants::Result[:fail], "Module insertion is failed.")  
+    #if (simultaneous_host_device_test)
+    if (@test_params.params_control.instance_variable_defined?(:@simultaneous_host))
+     @stop_test=true
+    end
+
     return     
   end
 
   command = "dmesg"
-  @equipment['dut1'].send_cmd(command, @equipment['dut1'].prompt,10)
+  @equipment['dut1'].send_cmd(command, @equipment['dut1'].prompt,4)
+  @equipment['server2'].send_cmd(command, @equipment['server2'].prompt,4)
   response = @equipment['dut1'].response
+  response_server = @equipment['server2'].response
   if response.include?('usb0')
-    puts "g_ether moduel inserted succesfully"
+    puts "g_ether module inserted succesfully"
   else
     $result = 1
     set_result(FrameworkConstants::Result[:fail], "g_ether module insertion fialed.")  
+    if (@test_params.params_control.instance_variable_defined?(:@simultaneous_host))
+     @stop_test=true
+  end
+  return
+
+  end
+  puts "SERVER RESPONSE is #{response_server}\n"
+  if (response_server.include?('cdc_ether') || response.server.include?('cdc_eem'))
+    puts "SUCCESS"
+    puts "g_ether registered on host succesfully"
+    if (response_server.include?('cdc_ether'))
+      usb_num=/usb(\d+)\s?:\s?register.*cdc_ether.*/.match(response_server).captures
+    else
+      usb_num=/usb(\d+)\s?:\s?register.*cdc_eem.*/.match(response_server).captures
+    end
+    usb_num=usb_num[0].to_i
+    server_usb_interface='usb'+usb_num.to_s
+    puts "USB_INTERFACE is #{server_usb_interface}\n"
+  else
+    puts "FAILURE"
+    $result = 1
+    set_result(FrameworkConstants::Result[:fail], "Ethernet gadget did not register on host side. Please check connections")  
+    if (@test_params.params_control.instance_variable_defined?(:@simultaneous_host))
+     @stop_test=true
+    end
+
     return     
   end
   
@@ -264,9 +361,9 @@ def usb_dev_cdc()
     return 
   end
 
-  system ("sleep  60")
+  #system ("sleep  60")
 
-  command ="bash -c 'ifconfig usb0 #{@equipment['server2'].usb_ip} up'"
+  command ="bash -c 'ifconfig #{server_usb_interface} #{@equipment['server2'].usb_ip} up'"
   @equipment['server2'].send_sudo_cmd(command, @equipment['server2'].prompt , 5)
   response = @equipment['server2'].response
   if response.include?('No such device')
@@ -283,17 +380,17 @@ def usb_dev_cdc()
     when $cmd.match(/_cdc_ping/)
 
       #Ping test
-      pingtest_cdc()
+      pingtest_cdc(server_usb_interface)
 
     when $cmd.match(/_cdc_floodping/)
 
       #Flood ping test
-      floodpingtest_cdc()
+      floodpingtest_cdc(server_usb_interface)
 
     when $cmd.match(/_cdc_iperf/)
 
       #iperf test
-      iperftest_cdc()
+      iperftest_cdc(server_usb_interface)
 
     else
       $result = 1
@@ -310,25 +407,25 @@ def usb_dev_cdc()
 end
 
 
-def assign_server_ip()
-  command ="bash -c 'ifconfig usb0 #{@equipment['server2'].usb_ip} up'"
+def assign_server_ip(server_usb_interface)
+  command ="bash -c 'ifconfig #{server_usb_interface} #{@equipment['server2'].usb_ip} up'"
   @equipment['server2'].send_sudo_cmd(command, @equipment['server2'].prompt , 5)
   response = @equipment['server2'].response
   if response.include?('No such device')
     $result = 1
-    set_result(FrameworkConstants::Result[:fail], "Linux system ip address is not assigned properlly.")
+    set_result(FrameworkConstants::Result[:fail], "Linux system ip address is not assigned properly.")
     return
   end
-  command ="ping -I usb0 #{@equipment['dut1'].usb_ip}"
+  command ="ping -I #{server_usb_interface} #{@equipment['dut1'].usb_ip}"
   @equipment['server2'].send_sudo_cmd(command, @equipment['server2'].prompt , 5)
   system ("sleep 1")
 end
 
 
 #ping test
-def pingtest_cdc()
+def pingtest_cdc(server_usb_interface)
   packetsize = [64,4096,65500]
-  assign_server_ip() 
+  assign_server_ip(server_usb_interface) 
   packetsize.each { |psize| 
   
   #Ping from DUT to host
@@ -362,9 +459,9 @@ end
 
 
 # Flood ping test
-def floodpingtest_cdc()
+def floodpingtest_cdc(server_usb_interface)
   packetsize = [64,4096,65500]
-  assign_server_ip()
+  assign_server_ip(server_usb_interface)
   packetsize.each { |psize|
 
   #Flood ping from host to DUT
@@ -385,7 +482,7 @@ end
 
 
 #iperf test
-def iperftest_cdc()
+def iperftest_cdc(server_usb_interface)
 
 #  iperf test from host to DUT
   windowsize = [8,16,32,64,128]
@@ -429,7 +526,7 @@ def iperftest_cdc()
 
   puts "iperf test from dut to host is going to start"
   windowsize = [8,16,32,64,128]
-  assign_server_ip()
+  assign_server_ip(server_usb_interface)
   windowsize.each { |wsize|
 
   system ("kill -9 $(pidof iperf)")
@@ -559,4 +656,41 @@ def MSC_Raw_Read(mscmount, mbsize)
   @equipment['server2'].send_sudo_cmd('bash -c "echo 3 > /proc/sys/vm/drop_caches"', @equipment['server2'].prompt , 30)
   @equipment['server2'].send_sudo_cmd("umount #{mscmount}", @equipment['server2'].prompt , 30)
   @equipment['server2'].send_sudo_cmd("sleep 5", @equipment['server2'].prompt , 30)
+end
+
+def start_usbhost_test(test_cmd)
+  @usbhost_thread = Thread.new(){
+  i=0
+  Thread.pass
+  Thread.current['stop']=false
+  time = Time.now
+  @eth_ip_addr = get_ip_addr()   # get_ip_addr() is defined at default_target_test.rb
+  raise "Can't run the test because DUT does not seem to have an IP address configured" if !@eth_ip_addr
+  @equipment['dut1'].target.platform_info.telnet_ip = @eth_ip_addr
+  @equipment['dut1'].target.platform_info.telnet_port = 23
+  @equipment['dut1'].connect({'type'=>'telnet'})
+  @equipment['dut1'].target.telnet.send_cmd("cd /opt/ltp", @equipment['dut1'].prompt, 5)
+  @equipment['dut1'].log_info("Telnet Data: \n #{@equipment['dut1'].target.telnet.response}")
+  @equipment['dut1'].target.telnet.send_cmd('cd /opt/ltp')
+  @equipment['dut1'].target.telnet.send_cmd(test_cmd, @equipment['dut1'].prompt,600)
+  @equipment['dut1'].log_info("Telnet Data: \n #{@equipment['dut1'].target.telnet.response}")
+  if ( @equipment['dut1'].target.telnet.timeout? )
+     set_result(FrameworkConstants::Result[:fail], "DUT is either not responding or took more that #{cmd_timeout} seconds to complete the usbhost test")
+     @stop_test = true
+  end
+  result_count=/Total Failures\s?:\s?(\d+)/.match(@equipment['dut1'].target.telnet.response).captures
+  result_count=result_count[0].to_i
+  if (result_count > 0)
+    set_result(FrameworkConstants::Result[:fail], "LTP Test has failed. Please check logs regarding failure.")
+    @stop_test = true
+  end
+
+    @stop_test=true
+}
+end
+
+def stop_usbhost_test
+  if @usbhost_thread
+    @usbhost_thread["stop"]=true
+  end
 end
