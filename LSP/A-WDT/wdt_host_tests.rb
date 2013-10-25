bdf3f479948f8352c2d98ba1ec74ed9022a396a6
Aparna Balasubramanian <aparnab@ti.com>
2013-10-25 13:53:34 -0500

    Modifying wdt_host_tests to look for U-Boot prompt
    WDT-tests will now look for U-Boot string and then U-Boot prompt
    to consider the success of the test unlike before when login to kernel
    was a requirement to pass the test. This will cover overnight tests
    where the rootfs information need not saved in the uboot environment.
    
    Signed-off-by: Aparna Balasubramanian <aparnab@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- LSP/A-WDT/wdt_host_tests.rb
+++ LSP/A-WDT/wdt_host_tests.rb
@@ -25,26 +25,21 @@ def watchdog_timer_test
 	dut_timeout = @test_params.params_control.instance_variable_defined?(:@dut_timeout) ? @test_params.params_control.dut_timeout[0].to_i : 600
 	runtest_cmd = @test_params.params_control.script.join(";")
 	cmd = eval(('"'+runtest_cmd.gsub("\\","\\\\\\\\").gsub('"','\\"')+'"')+"\n")
-	@equipment['dut1'].send_cmd(cmd, @equipment['dut1'].login_prompt, dut_timeout)
-  if @equipment['dut1'].timeout?
-    # For cases where the board is not rebooted after the test
-	  @equipment['dut1'].send_cmd("echo $?",/^0[\0\n\r]+/m, 2)
-	  if @equipment['dut1'].timeout?
-	    runltp_fail = true
-	  end
-  else
-    # board did reboot
-    @equipment['dut1'].send_cmd("#{@equipment['dut1'].login}", @equipment['dut1'].prompt,1)
-  end
+	@equipment['dut1'].send_cmd(cmd, "U-Boot", dut_timeout)
+        if @equipment['dut1'].timeout?
+           runltp_fail = true
+        else
+	   @equipment['dut1'].send_cmd(' ', @equipment['dut1'].boot_prompt, dut_timeout)
+           if @equipment['dut1'].timeout?
+              runltp_fail = true
+           end
+        end
 
-  @equipment['dut1'].send_cmd('uname -a', @equipment['dut1'].prompt,1)
-  response = @equipment['dut1'].response
-
-	if response.include?("Linux") and !runltp_fail
-		puts "Test PASS"
-		set_result(FrameworkConstants::Result[:pass], "Test Passed.")
+        if !runltp_fail
+           puts "Test PASS"
+           set_result(FrameworkConstants::Result[:pass], "Test Passed.")
 	else
-		puts "Test Fail"
-		set_result(FrameworkConstants::Result[:fail], "Test Failed.")
+           puts "Test Fail"
+           set_result(FrameworkConstants::Result[:fail], "Test Failed.")
 	end
 end
