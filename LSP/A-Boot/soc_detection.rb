dedfa0a76e7ede5290b9d29d71a33782fe90b12c
Carlos Hernandez <ceh@ti.com>
2013-12-02 23:07:42 -0500

    Linux: New SoC detection and Max OPP test script
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

---
+++ LSP/A-Boot/soc_detection.rb
@@ -0,0 +1,32 @@
+require File.dirname(__FILE__)+'/../default_test_module' 
+include LspTestScript
+
+def run
+  begin
+    params = {'platform' => @equipment['dut1'].name}
+    @equipment['dut1'].send_cmd('uname -r', @equipment['dut1'].prompt)
+    params['version'] = @equipment['dut1'].response.match(/^([\d\.]+)/i).captures[0]
+    platform_string = get_platform_string(params)
+        
+    # Check platform string
+    @equipment['dut1'].send_cmd('dmesg', /#{platform_string}.+#{@equipment['dut1'].prompt}/mi, 20)
+    if @equipment['dut1'].timeout?
+      set_result(FrameworkConstants::Result[:fail], "SoC was not properly detected. Could not find #{platform_string} in boot logs")
+      return
+    end
+    
+    # Check max OPP is available
+    @equipment['dut1'].send_cmd('cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq', @equipment['dut1'].prompt)
+    max_opp_string = get_max_opp_string(params)
+    if !@equipment['dut1'].response.match(/#{max_opp_string}/i)
+      set_result(FrameworkConstants::Result[:fail], "Max OPP is not available. Expected #{max_opp_string}")
+      return
+    end
+
+    set_result(FrameworkConstants::Result[:pass], "SoC was properly detected and Max OPP is #{max_opp_string}")
+  rescue Exception => e  
+    puts e.message 
+    puts e.backtrace
+    set_result(FrameworkConstants::Result[:fail], "Linux version not found")
+  end
+end
