2497c59bd39c1506dcec875283e998d189f81549
Carlos Hernandez <ceh@ti.com>
2013-12-06 12:25:34 -0500

    Linux: dev_test_perf_gov.rb: Restore previous governor
    Raise an error if performance governor is not available and also
    restore previous governor after running the performance test.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- LSP/TARGET/dev_test_perf_gov.rb
+++ LSP/TARGET/dev_test_perf_gov.rb
@@ -1,11 +1,24 @@
 require File.dirname(__FILE__)+'/dev_test2'
 
 def run
-  @equipment['dut1'].send_cmd("cpus=$(ls /sys/devices/system/cpu | grep \"cpu[0-9].*\"); for cpu in $cpus; do echo performance > /sys/devices/system/cpu/$cpu/cpufreq/scaling_governor; done",
-                              @equipment['dut1'].prompt)
+  # Preserve current governor
   @equipment['dut1'].send_cmd("cpus=$(ls /sys/devices/system/cpu | grep \"cpu[0-9].*\"); for cpu in $cpus; do cat /sys/devices/system/cpu/$cpu/cpufreq/scaling_governor; done",
                               @equipment['dut1'].prompt)
-
+  previous_govs = @equipment['dut1'].response.scan(/^\w+\s*$/)
+  #Change to performance governor
+  @equipment['dut1'].send_cmd("cpus=$(ls /sys/devices/system/cpu | grep \"cpu[0-9].*\"); for cpu in $cpus; do echo -n performance > /sys/devices/system/cpu/$cpu/cpufreq/scaling_governor; done",
+                              @equipment['dut1'].prompt)
+  @equipment['dut1'].send_cmd("echo $?",/^0[\0\n\r]+/m, 2)
+  raise "performance governor is not available" if @equipment['dut1'].timeout?
+  @equipment['dut1'].send_cmd("cpus=$(ls /sys/devices/system/cpu | grep \"cpu[0-9].*\"); for cpu in $cpus; do cat /sys/devices/system/cpu/$cpu/cpufreq/scaling_governor; done",
+                              @equipment['dut1'].prompt)
+  # Run the test
   self.as(LspTargetTestScript).run
+
+  # Restore previous governor
+  previous_govs.each_with_index{|v,i| 
+    v.gsub!(/\s*/,'')
+    @equipment['dut1'].send_cmd("echo -n #{v} > /sys/devices/system/cpu/cpu#{i}/cpufreq/scaling_governor", @equipment['dut1'].prompt)
+  }
 end
 
