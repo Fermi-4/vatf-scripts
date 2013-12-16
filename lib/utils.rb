f18f402c8c2fc3c7378a2d777e473f0ce0b3c02e
Alejandro Hernandez <ajhernandez@ti.com>
2013-12-15 22:28:27 -0500

    Add scp and stat function to lib/utils
    This patch add scp functions to the lib/utils based on gems
    net-ssh and net-scp. It also adds function to compute mean,
    variance, standard deviation and cross_correlation to the lib
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,3 +1,6 @@
+require 'net/ssh'
+require 'net/scp'
+
 #Function to parse string containing <name>sep<value> pairs, takes
 #  field_string the string to be parsed
 #  sep, string or regex used as the <name>, <value> separator, 
@@ -9,4 +12,75 @@ def parse_sep_field(field_string, sep='=')
   vals_hash = {}
   vals.each { |c_val| vals_hash[c_val[0].strip()] = c_val[1] }
   vals_hash
+end
+
+#Fucntion to obtain the mean (avg), of an array of values, takes
+#  arr, an array of values
+def mean(arr)
+  arr.inject(:+).to_f / arr.length
+end
+
+#Function to compute the variance of the values in an array, takes
+#  arr, an array of values
+def variance(arr)
+  mean = mean(arr)
+  sum = arr.inject(0){|sum,ele| sum+=(ele-mean)**2}
+  sum/(arr.length-1) 
+end
+
+#Function to compute the standard deviation of the values in an array, takes
+#  arr, an array of value
+def std_dev(arr)
+  variance(arr)**0.5    
+end
+
+#Function to compute the cross correlation between two arrays, takes
+#  arr1, an array of samples
+#  arr2, an array of samples
+def cross_correlation(arr1, arr2)
+  raise "Arrays must have same length" if arr1.length != arr2.length
+  dev1 = std_dev(arr1)
+  dev2 = std_dev(arr2)
+  m1 = mean(arr1)
+  m2 = mean(arr2)
+  sum = (0...arr2.length).inject(0) {|r, i| r + (arr1[i]-m1)*(arr2[i]-m2)}
+  sum/((arr1.length-1)*dev1*dev2)
+end
+
+# Function to obtain the ip address assigned to the ethernet interface specified, takes
+#  iface, the interface name to be for which the address will be parsed
+#  eq, driver object used to communicate with the system, used to send commands
+#Returns a string containing the ip address of the interface specified
+def get_ip_addr(iface='eth0', eq=@equipment['dut1'])
+  eq.send_cmd("ifconfig #{iface}", eq.prompt)
+  dut_ip = (/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(?=\s+(Bcast))/.match(eq.response)).captures[0] 
+  dut_ip
+end
+
+#Function to perform a scp from a ssh server (require gems net-ssh and net-scp)
+#to the host, takes
+#  ip_addr, string containing the ip address of the ssh server
+#  rem_path, string containing the path of the file, that will be copied, from ip_addr
+#  local_path, string containing destination path of the file
+#  username, (Optiona) string containing the ssh username, defaults to root
+#  password, (Optional) string containing the password associated with user 
+#            username, defaults to ''
+def scp_pull_file(ip_addr, rem_path, local_path, username='root', password='') 
+  Net::SSH.start(ip_addr, username, :password => password) do |ssh|
+    ssh.scp.download!(rem_path, local_path)
+  end
+end
+
+#Function to perform a scp to a ssh server (require gems net-ssh and net-scp)
+#to the host, takes
+#  ip_addr, string containing the ip address of the ssh server
+#  local_path, string containing the path of the file to be copied
+#  rem_path, string containing destination path of the file in ip_addr
+#  username, (Optiona) string containing the ssh username, defaults to root
+#  password, (Optional) string containing the password associated with user 
+#            username, defaults to ''
+def scp_push_file(ip_addr, local_path, rem_path, username='root', password='') 
+  Net::SSH.start(ip_addr, username, :password => password) do |ssh|
+    ssh.scp.upload!(local_path, rem_path)
+  end
 end
