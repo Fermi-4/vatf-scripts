d355f03fa22367c55dd86260af58d7b3ff08e8f1
Carlos Hernandez <ceh@ti.com>
2014-03-17 11:20:52 -0400

    Deleted unused *atp.rb files in LSP/Common
    RtpWizard tool which uses atp.rb files is not longer used, hence
    there is no need to keep atp.rb files.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- LSP/Common/atp_helper.rb
+++
@@ -1,8 +0,0 @@
-  # concatenate 'cmd' for 'rtp_times'.
-  def get_repeat_cmd(rpt_times, cmd)
-    rtn = ''
-    rpt_times.to_i.times {|x|
-      rtn = rtn + ";#{cmd}"
-    }
-    rtn = rtn.sub(/^;/, '')
-  end
