1d279e1e78ea6d20c8df4bc4ca391d3eb7abf3a4
Aparna Balasubramanian <aparnab@ti.com>
2014-06-20 15:43:48 -0500

    Functions to save, set and restore cpufreq governors.
    Functions added so that test applications can call them to set governor to
    performance or any desired policy.
    
    Signed-off-by: Aparna Balasubramanian <aparnab@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- LSP/default_test_module.rb
+++ LSP/default_test_module.rb
@@ -656,5 +656,32 @@ module LspTestScript
   def kill_process(this_equipment=@equipment['dut1'],process)
     this_equipment.send_cmd("kill $(ps aux | grep `echo #{process} | sed -r 's/^/[/g' | sed 's/^\\(.\\{2\\}\\)/\\1]/'` | awk '{print $2}')", @equipment['dut1'].prompt, 10)
   end
+
+  # Preserve current governor
+  def create_save_cpufreq_governors
+    @equipment['dut1'].send_cmd("cpus=$(ls /sys/devices/system/cpu | grep \"cpu[0-9].*\"); for cpu in $cpus; do cat /sys/devices/system/cpu/$cpu/cpufreq/scaling_governor; done",
+                              @equipment['dut1'].prompt)
+    previous_govs = @equipment['dut1'].response.scan(/^\w+\s*$/)
+  end
+
+
+ # Change to specified governor
+  def enable_cpufreq_governor(type='performance')
+    @equipment['dut1'].send_cmd("cpus=$(ls /sys/devices/system/cpu | grep \"cpu[0-9].*\"); for cpu in $cpus; do echo -n #{type} > /sys/devices/system/cpu/$cpu/cpufreq/scaling_governor; done",
+                              @equipment['dut1'].prompt)
+    @equipment['dut1'].send_cmd("echo $?",/^0[\0\n\r]+/m, 2)
+    raise "#{type} governor is not available" if @equipment['dut1'].timeout?
+    @equipment['dut1'].send_cmd("cpus=$(ls /sys/devices/system/cpu | grep \"cpu[0-9].*\"); for cpu in $cpus; do cat /sys/devices/system/cpu/$cpu/cpufreq/scaling_governor; done",
+                              @equipment['dut1'].prompt)
+ end
+
+ # Restore previous governor
+  def restore_cpufreq_governors(previous_govs)
+    previous_govs.each_with_index{|v,i|
+       v.gsub!(/\s*/,'')
+       @equipment['dut1'].send_cmd("echo -n #{v} > /sys/devices/system/cpu/cpu#{i}/cpufreq/scaling_governor", @equipment['dut1'].prompt)
+    }
+  end
+
 end
 
