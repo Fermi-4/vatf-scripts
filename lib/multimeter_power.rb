04c4fbaac8668cbd21a1f83559621b15ec39c26d
Carlos Hernandez <ceh@ti.com>
2014-07-02 17:17:47 -0400

    lib/multimeter_power.rb: Minor cleanup
    Do minor cleanup and skip power calculations if they are already
    provided in data passed from multimeter.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- lib/multimeter_power.rb
+++ lib/multimeter_power.rb
@@ -4,21 +4,28 @@ module MultimeterModule
 # Return Parameter: Hash table Power consumptions for all domains   
 def calculate_power_consumption(volt_reading, multimeter=@equipment['multimeter'])
   power_consumption = Hash.new
-  for i in (0..multimeter.number_of_channels/2 -1 ) 
-    power_consumption["domain_" + multimeter.dut_power_domains[i]+ "_power_readings"] = Array.new()
+  readings_included_power = volt_reading.key?('domain_' + multimeter.dut_power_domains[0] + '_power_readings')
+  if readings_included_power
+    volt_reading.each {|k,v|
+      power_consumption[k] = v if k.match(/domain_.+_power_readings/)
+    }
+  end
+  num_channels = (volt_reading.select {|k,v| k.match(/domain_.+_current_readings/)} ).size
+  for i in (0..num_channels -1 ) 
+    power_consumption["domain_" + multimeter.dut_power_domains[i]+ "_power_readings"] = Array.new() if !readings_included_power
   end 
   power_consumption["all_domains"] = Array.new
   volt_reading['domain_' + multimeter.dut_power_domains[0] + '_volt_readings'].each_index{|i|
-    for k in (1..multimeter.number_of_channels/2) 
-     power_consumption["domain_" + multimeter.dut_power_domains[k - 1]+ "_power_readings"] << ((volt_reading["domain_" + multimeter.dut_power_domains[k - 1]+ "_volt_readings"][i] * volt_reading["domain_"+ multimeter.dut_power_domains[k - 1] + "_current_readings"][i])) * 1000
+    for k in (1..num_channels) 
+     power_consumption["domain_" + multimeter.dut_power_domains[k - 1]+ "_power_readings"] << ((volt_reading["domain_" + multimeter.dut_power_domains[k - 1]+ "_volt_readings"][i] * volt_reading["domain_"+ multimeter.dut_power_domains[k - 1] + "_current_readings"][i])) * 1000 if !readings_included_power
     end
     total_power = 0
-    for k in (1..multimeter.number_of_channels/2) 
+    for k in (1..num_channels) 
       total_power = total_power + power_consumption["domain_" + multimeter.dut_power_domains[k - 1]+ "_power_readings"][i]
     end
     power_consumption["all_domains"] << total_power
   }
-  for i in (1..multimeter.number_of_channels/2)
+  for i in (1..num_channels)
   power_consumption["mean_" + multimeter.dut_power_domains[i - 1]] = mean(power_consumption["domain_" + multimeter.dut_power_domains[i - 1]+ "_power_readings"])
   end
   power_consumption['mean_all_domains'] = mean(power_consumption["all_domains"])
@@ -26,12 +33,12 @@ def calculate_power_consumption(volt_reading, multimeter=@equipment['multimeter'
 end 
 
 def mean(a)
- array_sum = 0.0
- a.each{|elem|
- array_sum = array_sum + elem.to_f
-}
-mean = array_sum/a.size
-return mean
+  array_sum = 0.0
+  a.each{|elem|
+    array_sum = array_sum + elem.to_f
+  }
+  mean = array_sum/a.size
+  return mean
 end
 
 
