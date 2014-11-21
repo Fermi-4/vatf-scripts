ae39ded90a410d2e7cc5d372649c7f8249f0242c
Carlos Hernandez <ceh@ti.com>
2014-11-21 08:05:58 -0500

    kernel_boot_test: Detects errors during reboot
    It is possible that some drivers will trigger errors during the
    shutdown process during board reboot, so try to catch such errors
    and fail the test if they are seen.
    Poking into /var/log/messages or /dev/kmsg is tricky during shutdown
    and the error messages that we are trying to catch are not written
    there because filesystem is already unmounted by the time they
    happen. So our best choice is to rely on console messages.
    Unfortunately prinkt console messages does not include the severity
    level and a patch to included it was NAK [1]. So this patch looks for
    timestamp signature followed by undesired keywords, such as fail and
    error.
    
    [1]: http://dflvsu0004.dal.design.ti.com/patch/11673/
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- LSP/A-Boot/kernel_boot_test.rb
+++ LSP/A-Boot/kernel_boot_test.rb
@@ -9,6 +9,7 @@ def run
   translated_boot_params = setup_host_side()
   counter = 0
   result = 0
+  cleanup_errors = 0
   loop_count = @test_params.params_control.loop_count[0].to_i
   while counter < loop_count
     puts("Inside the loop counter = #{counter}" );
@@ -17,13 +18,20 @@ def run
       if is_soft_boot == 'yes'
         puts "soft-reboot....\n\n"
         @equipment['dut1'].send_cmd('reboot', translated_boot_params['dut'].login_prompt, 150)
+        boot_log = @equipment['dut1'].response
         @equipment['dut1'].send_cmd(translated_boot_params['dut'].login, translated_boot_params['dut'].prompt, 10) # login to the unit
         raise 'Could not soft-reboot' if @equipment['dut1'].timeout?
+        errors = boot_log.split(/^u-boot\s+/i)[0].scan(/\[[\s\d\.]+\]\s+.*(?=timed out|error|fail).*/i)
+        raise SignalException, "Errors detected while rebooting" if errors.size > 0
       else
         @equipment['dut1'].disconnect
         self.as(LspTestScript).setup
         @equipment['dut1'].disconnect
       end
+    rescue SignalException => e
+      puts "Error message seen during reboot on iteration #{counter}: " + e.to_s + ": " + e.backtrace.to_s
+      @equipment['dut1'].log_info("Error message seen during reboot on iteration #{counter}")
+      cleanup_errors += 1
     rescue Exception => e 
       puts "Failed to boot on iteration #{counter}: " + e.to_s + ": " + e.backtrace.to_s
       @equipment['dut1'].log_info("Failed to boot on Iteration #{counter}")
@@ -34,10 +42,14 @@ def run
     end
   end
     
-  if result == 0 
+  if result == 0 && cleanup_errors == 0
     set_result(FrameworkConstants::Result[:pass], "Stress Kernel Boot test passed.")
+  elsif result > 0 && cleanup_errors > 0
+    set_result(FrameworkConstants::Result[:fail], "Kernel failed to boot #{result} times out of #{loop_count}, and there were cleanup errors #{cleanup_errors} times")
+  elsif cleanup_errors > 0
+    set_result(FrameworkConstants::Result[:fail], "There were cleanup errors #{cleanup_errors} times out of #{loop_count}")
   else
-    set_result(FrameworkConstants::Result[:fail], "Kernel failed to boot #{result} times out of #{loop_count}, stress kernel boot test failed!")
+    set_result(FrameworkConstants::Result[:fail], "Kernel failed to boot #{result} times out of #{loop_count}")
   end
 end
 
