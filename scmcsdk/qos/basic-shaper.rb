f14a4f251d9252e46a69d6ae0ce7dda111be9dcb
Elvita Lobo <elvita@ti.com>
2014-12-10 11:52:44 -0500

    scmcsdk: Add QoS testscript for Keystone 2 devices
    This patch adds support for QoS testing on Keystone 2 devices
    
    The test code reads the QoS tree dynamically from the /sys/devices entries, configures / reads traffic output rates, wrr weights, overhead bytes used in QoS counting.
    
    It uses tc to set up a mapping of dscp values to various traffic queues
    
    It sends the configured amount of traffic through each queue using iperf
    
    It checks the iperf reported bandwidth (adjusted to what QoS is counting) and reports % error, if any on each queue

---
+++ scmcsdk/qos/basic-shaper.rb
@@ -0,0 +1,268 @@
+# Test script to test basic QoS shaper
+
+# The test code reads the QoS tree dynamically from the /sys/devices entries, configures / reads 
+# traffic output rates, wrr weights, overhead bytes used in QoS counting.
+#
+# It uses tc to set up a mapping of dscp values to various traffic queues 
+# It sends the configured amount of traffic through each queue using iperf
+# It checks the iperf reported bandwidth (adjusted to what QoS is counting), compares with expected bw 
+# and reports % error, if any, on each queue
+# It prints stats for packets sent and packets discarded per queue
+
+require File.dirname(__FILE__)+'/../../LSP/default_test_module' 
+include LspTestScript
+require File.dirname(__FILE__)+'/utils' 
+include QosModule
+
+def setup
+  self.as(LspTestScript).setup
+  @interface = @test_params.params_control.instance_variable_defined?(:@interface) ? @test_params.params_control.interface[0].to_s : 'eth0'
+  @queue_type = @test_params.params_control.instance_variable_defined?(:@queue_type) ? @test_params.params_control.queue_type[0].to_s : 'linux'
+  @tree_output_rate = @test_params.params_control.instance_variable_defined?(:@tree_output_rate) ? @test_params.params_control.tree_output_rate[0].to_s : nil
+  # max output rate limit per queue
+  @output_rates = @test_params.params_control.instance_variable_defined?(:@output_rates) ? @test_params.params_control.output_rates[0].split(":").collect {|x| x == "nil" ? x = nil :  x.to_s } : []
+  # weights for each wrr queue
+  @wrr_weights = @test_params.params_control.instance_variable_defined?(:@wrr_weights) ? @test_params.params_control.wrr_weights[0].split(":").collect {|x| x == "nil" ? x = nil :  x.to_i } : []
+  @overhead_bytes = @test_params.params_control.instance_variable_defined?(:@oh_bytes) ? @test_params.params_control.oh_bytes[0].split(":").collect {|x| x == "nil" ? x = nil : x.to_i } : [-42,-42,-42,-42,-42,-42]
+  #@overhead_bytes = @test_params.params_control.instance_variable_defined?(:@oh_bytes) ? @test_params.params_control.oh_bytes[0].split(":").collect {|x| x.to_i } : [24,24,24,24,24,24]
+  # traffic to be sent per queue
+  @traffic_rates = @test_params.params_control.instance_variable_defined?(:@traffic_rates) ? @test_params.params_control.traffic_rates[0].split(":").collect {|x| x == "nil" ? x = nil : x.to_s } : ["20M", "20M", "40M", "60M", "80M", "30M"]
+  @allowed_error_percent = @test_params.params_control.instance_variable_defined?(:@allowed_error_percent) ? @test_params.params_control.allowed_error_percent[0].to_f : 2
+
+
+  @equipment['dut1'].send_cmd("cd ",@equipment['dut1'].prompt,10)
+  @interface_shapers = get_shapers_per_interface(@interface)
+  @shaper_name = get_interface_shaper_name(@interface)
+  @output_rate_arr = [] #filepaths
+  @output_weight_arr = [] #filepaths
+  @packets_fwd_arr = [] #filepaths
+  @packets_drop_arr = [] #filepaths
+  @overhead_bytes_arr = [] #filepaths
+
+  @wrr_weights_sum = 0
+  @expected_op_traffic_rate = []
+  @reported_throughput = []
+  @expected_data_throughput = []
+  @qos_active = []
+  @test_result_comment = ""
+  @test_outcome = FrameworkConstants::Result[:pass]
+end
+
+
+def run()
+  # Find qos tree root
+  @equipment['dut1'].send_cmd("cd ",@equipment['dut1'].prompt,10)
+  @qos_tree_root = get_qos_trees(@interface_shapers,@shaper_name)
+  
+  # Find qos tree branches we are interested in: wrr weights, stats, overhead bytes
+  data = learn_qos_tree(@qos_tree_root,@queue_type)
+  wrr_data = learn_node(@qos_tree_root,"weight")
+  stats_data_packets_fwd = learn_stats_branches(@qos_tree_root,@queue_type,"packets_forwarded")
+  stats_data_packets_drop = learn_stats_branches(@qos_tree_root,@queue_type,"packets_discarded")
+  oh_bytes = learn_node(@qos_tree_root,"overhead_bytes")
+
+  
+  # Read branch information
+  @tree = to_tree(data)
+  @wrr_tree = to_tree(wrr_data)
+  @stats_tree_pf = to_tree(stats_data_packets_fwd)
+  @stats_tree_pd = to_tree(stats_data_packets_drop)
+  @oh_bytes_tree = to_tree(oh_bytes)
+  parse_tree(@tree[0],@output_rate_arr)
+  parse_tree(@oh_bytes_tree[0],@overhead_bytes_arr)
+  parse_tree(@wrr_tree[0],@output_weight_arr)
+  parse_tree(@stats_tree_pf[0],@packets_fwd_arr)
+  parse_tree(@stats_tree_pd[0],@packets_drop_arr)
+
+  configure_qos()
+  
+  # Write input configuration to logs
+  write_to_log "=============  QoS PARAMS  =============="
+  write_to_log "QoS tree root: #{@qos_tree_root}"
+  write_to_log "tree_output_rate: #{@tree_output_rate}"
+  write_to_log "weights: #{@wrr_weights}"
+  write_to_log "output_rates: #{@output_rates}"
+  write_to_log "traffic_rates: #{@traffic_rates}"
+  write_to_log "output_rate_arr: #{@output_rate_arr}"
+  write_to_log "output_weight_arr: #{@output_weight_arr}"
+  write_to_log "packets_fwd: #{@packets_fwd_arr}"
+  write_to_log "packets dropped: #{@packets_drop_arr}"
+  write_to_log "overhead_bytes: #{@overhead_bytes}"
+  write_to_log "=========================================" 
+
+  
+  # Now, start figuring expected output rates on each queue
+  @leftover_bw = @tree_output_rate
+  
+  @total_input_traffic_in_bps = 0
+  @traffic_rates.each {|x|
+  if x!= nil
+    puts "x: #{x}"
+    @total_input_traffic_in_bps = @total_input_traffic_in_bps + convert_to_bits_per_sec(x)  
+  end
+  }
+
+  @wrr_weights.each {|x|
+    @wrr_weights_sum = @wrr_weights_sum + x 
+  }
+  
+  @wrr_queues = @wrr_weights.length
+  @wrr_weights_sum = @wrr_weights_sum.to_f
+  puts "WRR weights sum is #{@wrr_weights_sum}"
+  @total_input_traffic_in_mbps = @total_input_traffic_in_bps/1000000
+  total_queues = @output_rate_arr.length 
+  @available_bw = Array.new(total_queues, nil)
+  
+
+  @total_input_traffic_in_mbps = @total_input_traffic_in_mbps.to_s + "M"
+
+  @wrr_index =  @wrr_queues - 1
+  @output_rate_arr.reverse.each_with_index {|val, i|
+      n = total_queues-i-1
+      @leftover_bw = convert_to_bits_per_sec(@leftover_bw)
+      if @leftover_bw == 0
+        @available_bw[n] = 0
+        puts "leftover bw is 0"
+      elsif @traffic_rates[n] == nil
+        @available_bw[n] = 0
+        puts "no traffic on this queue"
+      else
+        if is_wrr?(val)
+          puts "is wrr"
+          cir_ratio = convert_to_bits_per_sec(@output_rates[n])/@wrr_weights_sum
+          @available_bw[n] = cir_ratio.to_f*@wrr_weights[@wrr_index]
+          puts @available_bw[n]
+          @wrr_index = @wrr_index - 1
+        else
+          @available_bw[n] = [(convert_to_bits_per_sec(@traffic_rates[n])).to_f,@leftover_bw].min
+        end
+        @leftover_bw = @leftover_bw - @available_bw[n]
+        if @leftover_bw < 0
+          @leftover_bw = 0
+        end 
+      end
+      @leftover_bw = @leftover_bw.to_f/1000000
+      @leftover_bw = @leftover_bw.to_s + "M"
+      @available_bw[n] = @available_bw[n]/1000000
+      @available_bw[n] = @available_bw[n].to_s + "M"     
+      n = n-1
+    }
+  puts "Available bw is #{@available_bw}"
+  puts "leftover bw is #{@leftover_bw}"
+
+  # Check if all are over subscribed. If yes, allow only available_bw on each queue
+  @all_oversubscribed = true
+  @traffic_rates.each_index { |i|
+  if (@traffic_rates[i] == nil)
+    @expected_data_throughput[i] = nil
+  else
+    x,y,unit_x,unit_y = get_values(@traffic_rates[i],@available_bw[i])
+    if x <= y
+      @all_oversubscribed = false
+      break
+    end
+  end
+  }
+  puts "all_oversubscribed #{@all_oversubscribed}"
+  puts "@total_input_traffic_in_mbps: #{@total_input_traffic_in_mbps} tree_output_rate:#{@tree_output_rate}"
+
+  query_qos_stats(@queue_type)
+  run_dsmark()
+  @equipment['dut1'].send_cmd("killall iperf",@equipment['dut1'].prompt,10) 
+  @equipment['server1'].send_cmd("killall iperf",@equipment['server1'].prompt,10) 
+  @equipment['dut1'].send_cmd("cd ",@equipment['dut1'].prompt,10) 
+  @traffic_rates.each_index { |i|
+  dport = 5000 + i + 1   
+    if (@traffic_rates[i] == nil)
+      # do nothing
+    else
+      @equipment['server1'].send_cmd_nonblock(get_iperf_cmd("server", nil, "#{dport}", "udp", "4", nil, nil, 300, nil, nil), /.*/,120)
+    end
+  }
+  
+     
+  @traffic_rates.each_index { |i|
+  dport = 5000 + i + 1
+    if (@traffic_rates[i] == nil)
+      # do nothing
+    else
+      cmd = get_iperf_cmd("client", @equipment['server1'].telnet_ip, "#{dport}", "udp", "4", "1470", "#{@traffic_rates[i]}", 300, nil, nil)
+      puts "cmd is #{cmd}"
+      @equipment['dut1'].send_cmd(cmd, @equipment['dut1'].prompt,10)
+    end
+
+  }
+  sleep 330
+  
+  puts "@total_input_traffic_in_mbps: #{@total_input_traffic_in_mbps} tree_output_rate:#{@tree_output_rate}"
+  @traffic_rates.each_index { |i|
+    dport = 5000 + i + 1
+    @expected_op_traffic_rate[i]  = nil
+    if (@traffic_rates[i] == nil)
+      @reported_throughput[i] = nil
+      @qos_active[i] = false
+      # Nothing was sent throught this queue, do nothing
+    else
+      @equipment['dut1'].send_cmd("cat #{dport}.txt", @equipment['dut1'].prompt,10)
+      reported_throughput = get_iperf_reported_bw(@equipment['dut1'].response)
+      @reported_throughput[i] = convert_to_mbps(convert_to_bytes_per_sec(reported_throughput)).to_s + "M"
+      x,y,unit_x,unit_y = get_values(@total_input_traffic_in_mbps,@tree_output_rate)
+      if x < y 
+        #Total input traffic is < max rate allowed
+        write_to_log "Total input traffic is < max rate allowed"
+        @expected_op_traffic_rate[i] = @traffic_rates[i]
+        @expected_data_throughput[i] = @expected_op_traffic_rate[i]
+      elsif x == y 
+        #Total input traffic is = max rate allowed
+        write_to_log "Total input traffic is = max rate allowed"
+        @expected_op_traffic_rate[i] = @traffic_rates[i]
+        @qos_active[i] = true
+      elsif @all_oversubscribed
+        # Each queue is over subscribed, allow only available_bw
+        write_to_log "Each queue is over subscribed, allow only available_bw"
+        @expected_op_traffic_rate[i] = @available_bw[i]
+        @qos_active[i] = true
+      else
+        x,y,unit_x,unit_y = get_values(@traffic_rates[i],@available_bw[i])
+        if (x<=y)
+          @expected_op_traffic_rate[i] = @traffic_rates[i]
+          @qos_active[i] = true
+        else
+          puts "TO DO: calculate expected_op_traffic_rate"
+          @expected_op_traffic_rate[i] = @traffic_rates[i] # this is incorrect, fix 
+          write_to_log "WARNING: expected throughput calculation is approximate"
+          @qos_active[i] = true
+        end
+      end
+      if @expected_data_throughput[i] == nil
+        @expected_data_throughput[i] = get_expected_data_throughput(@expected_op_traffic_rate[i], @overhead_bytes[i].to_i, "udp", 1470)
+      end
+      puts "expected_data_throughput is #{@expected_data_throughput[i]} reported_throughput is #{@reported_throughput[i]}"
+      error = get_percent_error(@reported_throughput[i], @expected_data_throughput[i])
+      puts "Error is #{error}%"
+      if check_error(error) == 1
+        # fail test 
+        puts "Error is greater than allowed - FAIL TEST"
+        @test_result_comment = @test_result_comment + "#{error} % error found on queue #{i} \n"
+        @test_outcome = FrameworkConstants::Result[:fail]
+      else
+        # do nothing
+      end
+      if (@test_outcome == FrameworkConstants::Result[:pass])
+        @test_result_comment = "Test Pass"
+      end
+    end
+    }
+  write_to_log "reported bw is #{@reported_throughput}"
+  @equipment['dut1'].send_cmd("cd #{@qos_tree_root}",@equipment['dut1'].prompt,10) 
+  query_qos_stats(@queue_type)
+  set_result(@test_outcome,@test_result_comment)
+end
+
+
+
+def clean
+  @equipment['dut1'].send_cmd("killall iperf",@equipment['dut1'].prompt,10) 
+  @equipment['server1'].send_cmd("killall iperf",@equipment['server1'].prompt,10) 
+end
+
---
+++ scmcsdk/qos/utils.rb
@@ -0,0 +1,309 @@
+# tree parsing code adapted from http://stackoverflow.com/questions/16408563/efficiently-building-a-file-system-tree-structure-with-nested-hashes
+module QosModule
+
+def learn_qos_tree(root,type)
+  @equipment['dut1'].send_cmd("cd #{root}",@equipment['dut1'].prompt,10)
+  @equipment['dut1'].send_cmd("find . -name output_rate | grep #{type}",@equipment['dut1'].prompt,10)
+  @equipment['dut1'].response.match(/(\.\/.+)\n/m)[0].strip
+end
+
+def learn_stats_branches(root,type,stat)
+  @equipment['dut1'].send_cmd("cd #{root}",@equipment['dut1'].prompt,10)
+  @equipment['dut1'].send_cmd("find .. -name #{stat} | grep #{type}",@equipment['dut1'].prompt,10)
+  @equipment['dut1'].response.match(/(\.\.\/.+)\n/m)[0].strip
+end
+
+def learn_node(root,name)
+  @equipment['dut1'].send_cmd("cd #{root}",@equipment['dut1'].prompt,10)
+  @equipment['dut1'].send_cmd("find . -name #{name} | grep -v linux | grep -v wifi | grep -v 4g | grep -v 3g",@equipment['dut1'].prompt,10)
+  @equipment['dut1'].response.match(/(\.\/.+)\n/m)[0].strip
+end
+
+def get_qos_trees(root,name)
+  @equipment['dut1'].send_cmd("find #{root} -maxdepth 1 -name \"qos*\"",@equipment['dut1'].prompt,10)
+  return @equipment['dut1'].response.match(/.*\/*#{name}\/.*/)
+end
+
+def get_shapers_per_interface(interface)
+  case interface
+  when "eth0"
+    @equipment['dut1'].send_cmd("find /sys/devices/ -name \"qos-inputs-1\"",@equipment['dut1'].prompt,10)
+    return @equipment['dut1'].response.match(/.*\/qos-inputs-1/)
+  when "eth1"
+    @equipment['dut1'].send_cmd("find /sys/devices/ -name \"qos-inputs-2\"",@equipment['dut1'].prompt,10)
+    return @equipment['dut1'].response.match(/.*\/qos-inputs-2/)
+  end
+end
+
+def parse_paths(list, depth, out)
+  to = 1
+  base = list.first[:name][depth]
+  while list[to] and list[to][:name][depth] == base do
+    to += 1
+  end
+
+  if list.first[:name][depth+1]
+    out << {name: base, children: []}
+
+    # Common directory found for the first N records; recurse deeper.
+    parse_paths(list[0..to-1], depth + 1, out.last[:children])
+
+  else
+    # It's a file, we can't go any deeper.
+    out << {name: list.first[:name].last }
+  end
+
+  if list[to]
+    # Recurse in to try find common directories for the deeper records.
+    parse_paths(list[to..-1], depth, out)
+  end
+
+  nil
+end
+
+def traverse_with_path tree, output_tree, path = [], &block
+  path += [tree[:name]]
+  yield path
+  if tree[:children]
+    tree[:children].each{|c| traverse_with_path c, output_tree, path, &block} 
+  else
+    # reached the end, child node you were looking for
+    output_tree << path.join("/").sub(/(\/)?/,"")
+  end
+end
+
+def parse_tree(input_tree,output_tree)
+  traverse_with_path input_tree, output_tree do |path|
+end
+end
+
+def to_tree(txt)
+  items = []
+  txt.split("\n").each do |line|
+    m = line.strip.match(/(.+)/).to_a
+    if m[0]
+      items << {name: m[0]}
+    end 
+  end
+  puts items.size
+  items.each do |item|
+    puts item
+    item[:name] = item[:name].split("/")
+  end
+
+  out = []
+  parse_paths(items, 0, out)
+  out
+end
+
+def get_interface_shaper_name(interface)
+  case interface
+  when "eth0"
+    return "qos-inputs-1"
+  when "eth1"
+    return "qos-inputs-2"
+  end
+end
+
+def read_overhead_bytes
+  @overhead_bytes_arr.each_index { |i|
+    oh_bytes = get_rate(@overhead_bytes_arr[i])
+    @overhead_bytes[i] = oh_bytes
+  }
+end
+
+def configure_qos()
+  if (@tree_output_rate != nil)
+    puts "Configuring tree output rate"
+    configure_tree_output_rate()
+  else
+    read_tree_output_rate()
+  end
+  if !(@output_rates.empty?)
+    configure_branch_output_rate()
+  else
+    read_branch_output_rate()
+  end 
+  if !(@wrr_weights.empty?)
+    configure_wrr_weights()
+  else
+    read_wrr_weights()
+  end
+  if !(@overhead_bytes.empty?)
+    configure_overhead_bytes()
+  else
+    read_overhead_bytes()
+  end
+end
+
+def configure_tree_output_rate()
+  rate = convert_to_bytes_per_sec(@tree_output_rate)
+  set_rate(rate, "output_rate")
+end
+
+def configure_branch_output_rate()
+  #assumes user will set branch output rate < tree output rate (no check on that)
+  @output_rate_arr.each_index { |i|
+    if @output_rates[i] != nil 
+      set_rate(convert_to_bytes_per_sec(@output_rates[i]), @output_rate_arr[i])
+    end
+    }
+end
+
+def read_branch_output_rate()
+  @output_rates = []
+  @output_rate_arr.each_index { |i|
+    rate = get_rate(@output_rate_arr[i])
+    if (rate > convert_to_bytes_per_sec(@tree_output_rate))
+      puts "branch output rate > tree output rate, resetting branch output rate to tree output rate"
+      @output_rates[i] = @tree_output_rate
+    else
+      @output_rates[i] = convert_to_mbps(rate)
+      @output_rates[i] = @output_rates[i].to_s + "M"
+    end
+    puts ""
+    puts @output_rates[i]
+  }
+end
+
+def read_tree_output_rate()
+    @tree_output_rate = convert_to_mbps(get_rate("output_rate"))
+    @tree_output_rate = @tree_output_rate.to_s + "M"
+    puts ""
+    puts @tree_output_rate
+  
+end
+
+def read_wrr_weights()
+  @output_weight_arr.each_index { |i|
+    weight = get_rate(@output_weight_arr[i])
+    @wrr_weights[i] = weight
+  }
+end
+
+def read_packets_forwarded(type)
+  @results_html_file.add_paragraph "== Packets forwarded. Queue type: #{type} =="
+  @packets_fwd_arr.each_index { |i|
+    packets = get_rate(@packets_fwd_arr[i])
+    @results_html_file.add_paragraph "#{@packets_fwd_arr[i].match(/#{type}-\w+\d?/)} : #{packets}"
+  }
+  @results_html_file.add_paragraph "============================================"
+end
+
+def read_packets_discarded(type)
+  @results_html_file.add_paragraph "== Packets discarded. Queue type: #{type} =="
+  @packets_drop_arr.each_index { |i|
+    packets = get_rate(@packets_drop_arr[i])
+    @results_html_file.add_paragraph "#{@packets_drop_arr[i].match(/#{type}-\w+\d?/)} : #{packets}"
+  }
+  @results_html_file.add_paragraph "============================================"
+end
+
+def configure_wrr_weights()
+  @wrr_weights.each_index { |i|
+    if @wrr_weights[i] != nil 
+      set_rate(@wrr_weights[i], @output_weight_arr[i])
+    end
+    }
+end
+
+def configure_overhead_bytes()
+  @overhead_bytes.each_index { |i|
+    if @overhead_bytes[i] != nil 
+      set_rate(@overhead_bytes[i], @overhead_bytes_arr[i])
+    end
+    }
+end
+
+def set_rate(rate,node)
+  get_rate(node)
+  @equipment['dut1'].send_cmd("echo #{rate.to_i} > #{node}",@equipment['dut1'].prompt,10) 
+  get_rate(node)
+end
+
+def get_rate(node)
+  @equipment['dut1'].send_cmd("cat #{node}",@equipment['dut1'].prompt,10)
+  return @equipment['dut1'].response.match(/^([+|-]?\d+)/).captures[0].to_i
+end 
+
+def run_dsmark()
+  @equipment['dut1'].send_cmd("tc qdisc del dev #{@interface} root",@equipment['dut1'].prompt,10)
+  @equipment['dut1'].send_cmd("tc qdisc add dev #{@interface} root handle 1 dsmark indices 32 default_index 0", @equipment['dut1'].prompt,10)
+  @output_rate_arr.each_index { |i|
+    dport = 5000 + i + 1
+    queue = i + 1
+    @equipment['dut1'].send_cmd("tc filter add dev eth0 parent 1:0 \
+                    protocol ip prio 1 u32 match ip dport #{dport} 0xffff action skbedit queue_mapping #{queue}", @equipment['dut1'].prompt,10)
+   }
+end
+
+def is_wrr?(filepath)
+  return filepath.include?("wrr")
+end
+
+def check_error(error)
+  puts "error: #{error} allowed_error_percent: #{@allowed_error_percent}"
+  if error > @allowed_error_percent
+    puts "Error is greater than allowed"
+    return 1
+  else
+    return 0
+  end
+end
+
+def query_qos_stats(queue_type)
+  read_packets_forwarded(queue_type)
+  read_packets_discarded(queue_type)
+end
+
+def write_to_log(string)
+  puts string
+  @results_html_file.add_paragraph(string)
+end
+
+def get_expected_data_throughput(rate, overhead_bytes, protocol="udp", data_len=1470)
+  if protocol == "udp"
+    headers_length = 42 #no vlan
+  end
+    x,unit_x = split_units(rate)
+	((data_len/(data_len+headers_length+overhead_bytes).to_f)*x).to_s + "#{unit_x}"
+  end
+    
+  def common_units?(a,b)
+    unit_x = a.match(/\d+(M|G|K)/).captures[0].to_s
+    unit_y = b.match(/\d+(M|G|K)/).captures[0].to_s
+    if unit_x == unit_y
+     true
+    else
+      false
+    end
+  end
+  
+  def split_units(rate)
+    x = rate.match(/(\d+.*\d*)[M|G|K]/).captures[0].to_f
+    unit_x = rate.match(/\d+(M|G|K)/).captures[0].to_s
+	return x,unit_x
+  end
+  
+  def get_values(a,b)
+    # returns values in common units, to make comparisons easier. 
+    # For example if a=3M,b=5K, will return 3000000,"",5000,""
+    # For example if a=3K,b=5K, will return 3,K,5,K
+    x, unit_x = split_units(a)
+    y, unit_y = split_units(b)
+    if common_units?(a,b)
+     return x,y,unit_x,unit_y
+    else
+      return convert_to_bits_per_sec(a),convert_to_bits_per_sec(b),"",""
+    end
+  end
+ 
+  def get_percent_error(measured_bw,expected_bw)
+    x,y,unit_x,unit_y = get_values(measured_bw,expected_bw)
+    x = x.abs
+    y = y.abs
+    puts "x: #{x}"  
+    puts "y: #{y}"
+    return (((y-x)/y)*100).abs.to_f
+  end
+end
