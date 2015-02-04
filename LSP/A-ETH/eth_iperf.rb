60a81df01b83809f26b1a167f71c1978b762a390
Aparna Balasubramanian <aparnab@ti.com>
2015-02-04 14:36:08 -0600

    Changes for simultaneous multi- ethernet interface throughput measurement
    Adds changes required to support running simultaneous throughput measurement
    on multiple ethernet interfaces. Sum of throughput on all interfaces specified
    by test case parameter will be written to performance database.
    
    Requirements to use this test application:
    
    1) If running throughput on n interfaces, n ethernet interfaces are
    required on host PC and each interface needs to be on a different subnet.
    
    2) Test cases need to be added to ltp test suite similar to
    eth_iperf_dual_tcp and eth_iperf_dual_udp. Checks for number of
    interfaces and additional run_iperf processes need to be added to the
    run_processes.sh call similar to those tests.
    
    3) Testlink tests need to be added for the n number of interfaces
    similar to tests existing under eth/host/tcp_bidir_dual_mac, etc.
    
    Signed-off-by: Aparna Balasubramanian <aparnab@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- LSP/A-ETH/eth_iperf.rb
+++ LSP/A-ETH/eth_iperf.rb
@@ -1,8 +1,12 @@
 require File.dirname(__FILE__)+'/../TARGET/dev_test_perf_gov'
+require File.dirname(__FILE__)+'/../network_utils'
 
 def setup
   super
   test_type = @test_params.params_control.type[0]
+  interface_num = @test_params.params_control.instance_variable_defined?(:@interface_num) ? @test_params.params_control.interface_num[0] : 1
+  array_of_interfaces = Array.new 
+  array_of_server_ips = Array.new
 
   if (test_type.match(/udp/i))
     @equipment['dut1'].send_cmd("sysctl -w net.core.rmem_max=33554432", @equipment['dut1'].prompt, 3)
@@ -12,6 +16,24 @@ def setup
     @equipment['dut1'].send_cmd("sysctl -w net.ipv4.udp_mem='4096 87380 33554432'", @equipment['dut1'].prompt, 3)
     @equipment['dut1'].send_cmd("sysctl -w net.ipv4.route.flush=1", @equipment['dut1'].prompt, 3)
   end
+
+# this part of test application triggers throughput measurement on the specified number of interfaces simultaneously and reports sum of throughput on all interfaces
+  if (interface_num.to_i > 1)
+    @equipment['server1'].send_cmd_nonblock("killall iperf", @equipment['server1'].prompt, 10)
+    array_of_interfaces = get_eth_interfaces
+    index=0
+    array_of_interfaces.each{|dut_eth|
+         @equipment['dut1'].send_cmd("ifdown #{dut_eth}", @equipment['dut1'].prompt, 3)
+         @equipment['dut1'].send_cmd("ifup #{dut_eth}", @equipment['dut1'].prompt, 10)
+         array_of_server_ips<<get_eth_server(dut_eth)
+         test_cmd = test_type.match(/udp/i) ? "iperf -s -B #{array_of_server_ips[index]} -u -w 128k &": "iperf -s -B #{array_of_server_ips[index]} &"
+         @equipment['server1'].send_cmd(test_cmd, /Server\s+listening.*?#{test_type}\sport.*?/i, 10)
+         index=index+1
+        }
+   return
+  end
+# end of multi-interface throughput logic
+         
   test_cmd = test_type.match(/udp/i) ? "iperf -s -u -w 128k &" : "iperf -s &"
   if !is_iperf_running?(test_type)
     @equipment['server1'].send_cmd_nonblock(test_cmd, /Server\s+listening.*?#{test_type}\sport.*?/i, 10)
@@ -31,19 +53,40 @@ def is_iperf_running?(type)
   end
 end
 
+def get_eth_interfaces()
+  interface_arr = Array.new
+  @equipment['dut1'].send_cmd("ls /sys/class/net|grep eth")
+  eth_interface_list = @equipment['dut1'].response
+  eth_interface_arr = eth_interface_list.split(/[\n\r]+/)
+  eth_interface_arr.each{|eth|
+      if (eth.match(/eth\d/))
+         interface_arr << eth
+      end
+    }
+  return interface_arr
+end
+
+def get_eth_server(interface_name)
+    ip_addr = ''
+    ip_addr = get_remote_ip(interface_name, 'dut1', 'server1')
+    if (ip_addr == '')
+       return [FrameworkConstants::Result[:fail], "Server does not have ethernet interface corresponding to dut's #{interface_name}. Please emsure host machine has an interface on subnet of each dut interface.\n"]
+   end
+    @equipment['dut1'].send_cmd("export IPERF#{interface_name}HOST=#{ip_addr}", @equipment['dut1'].prompt)
+    ip_addr
+end
+
 # Determine test result outcome and save performance data
 def run_determine_test_outcome(return_non_zero)
   @equipment['dut1'].send_cmd("cat result.log",/^1[\0\n\r]+/m, 2)
   failtest_check = !@equipment['dut1'].timeout?
   perf_data = get_performance_data(File.join(@linux_temp_folder,'test.log'), get_perf_metrics)
   test_type = @test_params.params_control.type[0]
-  if test_type.match(/tcp/i)
     perf_data.each{|d|
       sum = 0.0
       d['value'].each {|v| sum += v}
       d['value'] = sum
     }  
-  end
   
   if return_non_zero
     return [FrameworkConstants::Result[:fail], 
