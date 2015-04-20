aed15e3839d006e92e2395c45c828ca5bb127a2e
Yan Liu <yan-liu@ti.com>
2015-04-20 11:14:48 -0400

    am43xx_gpevm_boot_order: Add option to not to boot to kernel for boot order test
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- LSP/A-Boot/am43xx_gpevm_boot_order_test.rb
+++ LSP/A-Boot/am43xx_gpevm_boot_order_test.rb
@@ -31,7 +31,7 @@ def nand_boot()
   # validate nandboot
   begin 
     # now the board should be in uboot prompt
-    puts "##### NAND BOOT START #####" 
+    report_msg "===== NAND BOOT START =====" 
     flash_nand()
 
     # invalidate other boot media
@@ -52,11 +52,14 @@ def nand_boot()
     raise e
   end
 
-  # check if the board can boot kernel from nand
-  flash_or_boot_kernel_fromto_media('flash', 'nand')
-  flash_or_boot_kernel_fromto_media('boot', 'nand')
-  
-  puts "##### NAND BOOT END #####" 
+  boot_to_kernel = @test_params.params_chan.instance_variable_defined?(:@boot_to_kernel) ? @test_params.params_chan.boot_to_kernel[0] : 'no'
+  if boot_to_kernel == 'yes'
+    # check if the board can boot kernel from nand
+    flash_or_boot_kernel_fromto_media('flash', 'nand')
+    flash_or_boot_kernel_fromto_media('boot', 'nand')
+  end
+ 
+  report_msg "===== NAND BOOT END =====" 
   return status 
 end 
 
@@ -69,7 +72,7 @@ def usbhost_boot()
   begin
 
     # now the board should be in uboot prompt
-    puts "##### USB-MSC BOOT START #####" 
+    report_msg "===== USB-MSC BOOT START =====" 
     @usb_switch_handler.select_input(@equipment['dut1'].params['usbhost_port'])
     flash_usbhost()
 
@@ -91,11 +94,14 @@ def usbhost_boot()
     raise e
   end
 
-  # check if board can boot kernel from usbhost
-  flash_or_boot_kernel_fromto_media('flash', 'usbmsc')
-  flash_or_boot_kernel_fromto_media('boot', 'usbmsc')
+  boot_to_kernel = @test_params.params_chan.instance_variable_defined?(:@boot_to_kernel) ? @test_params.params_chan.boot_to_kernel[0] : 'no'
+  if boot_to_kernel == 'yes'
+    # check if board can boot kernel from usbhost
+    flash_or_boot_kernel_fromto_media('flash', 'usbmsc')
+    flash_or_boot_kernel_fromto_media('boot', 'usbmsc')
+  end
 
-  puts "##### USB-MSC BOOT END #####" 
+  report_msg "===== USB-MSC BOOT END =====" 
   return status 
 end 
 
@@ -104,7 +110,7 @@ end
 # Return Parameter: pass or fail.  
 
 def usbrndis_boot()
-  puts "##### USB-ETH BOOT START #####" 
+  report_msg "===== USB-ETH BOOT START =====" 
   init_dhcp()
 
   if ! @equipment['dut1'].at_prompt?({'prompt'=>@equipment['dut1'].boot_prompt})
@@ -136,10 +142,13 @@ def usbrndis_boot()
     raise e
   end
  
-  # check if the dut can boot to kernel under usbrndis booting mode
-  flash_or_boot_kernel_fromto_media('boot', 'eth')
-  
-  puts "##### USB-ETH BOOT END #####" 
+  boot_to_kernel = @test_params.params_chan.instance_variable_defined?(:@boot_to_kernel) ? @test_params.params_chan.boot_to_kernel[0] : 'no'
+  if boot_to_kernel == 'yes'
+    # check if the dut can boot to kernel under usbrndis booting mode
+    flash_or_boot_kernel_fromto_media('boot', 'eth')
+  end
+ 
+  report_msg "===== USB-ETH BOOT END =====" 
   return status 
 end 
 
