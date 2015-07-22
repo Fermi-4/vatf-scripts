d8634394150921ecf0d4ea7339e4e3a1fd679ccf
Aparna Balasubramanian <aparnab@ti.com>
2015-07-22 12:07:27 -0500

    Simultaneous ethernet udp receive tests on multiple ports and cleanup
    Implements simultaneous iperf tests for udp rx
    functionality on multiple ethernet ports.
    Also, makes some changes in eth_iperf.rb to use refactored
    functions from network_utils.rb and default_test_module.rb.
    
    This patch has a dependency on the
    "Refactoring/enhancements to network functions and kill_process
    function" patch.
    
    Signed-off-by: Aparna Balasubramanian <aparnab@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- LSP/A-ETH/eth_iperf-server.rb
+++ LSP/A-ETH/eth_iperf-server.rb
@@ -1,44 +1,52 @@
 require 'net/ftp'
 require File.dirname(__FILE__)+'/../default_test_module'
 require File.dirname(__FILE__)+'/../../lib/parse_perf_data'
+require File.dirname(__FILE__)+'/../network_utils'
 
 include LspTestScript
 include ParsePerfomance
 
-initial_interval=0
 def setup
   super
   test_type = @test_params.params_control.type[0]
+  interface_num = @test_params.params_control.instance_variable_defined?(:@interface_num) ? @test_params.params_control.interface_num[0] : 1
+  array_of_interfaces = Array.new
+
   if (test_type.match(/udp/i))
-    @equipment['dut1'].send_cmd("sysctl -w net.core.rmem_max=33554432", @equipment['dut1'].prompt, 3)
-    @equipment['dut1'].send_cmd("sysctl -w net.core.wmem_max=33554432", @equipment['dut1'].prompt, 3)
-    @equipment['dut1'].send_cmd("sysctl -w net.core.rmem_default=33554432", @equipment['dut1'].prompt, 3)
-    @equipment['dut1'].send_cmd("sysctl -w net.core.wmem_default=33554432", @equipment['dut1'].prompt, 3)
-    @equipment['dut1'].send_cmd("sysctl -w net.ipv4.udp_mem='4096 87380 33554432'", @equipment['dut1'].prompt, 3)
-    @equipment['dut1'].send_cmd("sysctl -w net.ipv4.route.flush=1", @equipment['dut1'].prompt, 3)
+    set_eth_sys_control_optimize('dut1')
   end
-  if (test_type.match(/tcp/i) && @test_params.params_control.instance_variable_defined?(:@interrupt_pacing_interval)
-)
-   response = @equipment['dut1'].send_cmd("ethtool -c #{@test_params.params_control.iface[0]}",@equipment['dut1'].prompt, 3)
-   # to get the initial setting of rx-usecs if required
-   initial_interval = response.scan(/rx-usecs:*\s\d*/m)[0].split(/:/)[1].strip
-   puts "Initial setting is #{initial_interval}\n"
-   @equipment['dut1'].send_cmd("set_ethtool_coalesce_options.sh -d #{@test_params.params_control.iface[0]} -p 'rx-usecs' -n #{@test_params.params_control.interrupt_pacing_interval[0]}", @equipment['dut1'].prompt, 3)
-  end
-  test_cmd = test_type.match(/udp/i) ? "iperf -s -u -w 128k &" : "iperf -s &"
-  # If iperf is already running on the DUT, it could possibly interfere with our results, kill it.
+
+  kill_process('iperf', :this_equipment => @equipment['server1'],:use_sudo => true)
   kill_process('iperf')
-  puts "Send iperf command"
-  @equipment['dut1'].send_cmd(test_cmd, /Server\s+listening.*?#{test_type}\sport/i, 10)
-  # iperf server process needs some time
-  @equipment['dut1'].send_cmd("", @equipment['dut1'].prompt, 10)
-  if !is_iperf_running?(test_type)
-    raise "iperf can not be started. Please make sure iperf is installed in the DUT"    
+
+  if (interface_num.to_i > 1)
+    array_of_interfaces = get_eth_interfaces
+  else
+    array_of_interfaces = [@test_params.params_control.iface[0]] 
   end
+
+  array_of_interfaces.each{|dut_eth|
+         run_down_up_udhcpc('dut1', dut_eth)
+         ip_addr=get_ip_addr('dut1', dut_eth)
+         test_cmd = test_type.match(/udp/i) ? "iperf -s -B #{ip_addr} -u -w 128k &": "iperf -s -B #{ip_addr} &"
+         @equipment['dut1'].send_cmd(test_cmd, /Server\s+listening.*?#{test_type}\sport.*?/i, 10)
+         @equipment['dut1'].send_cmd("", @equipment['dut1'].prompt, 10)
+         if !is_iperf_running?(test_type)
+             raise "iperf can not be started. Please make sure iperf is installed in the DUT"    
+         end
+        }
+
 end
 
-def run
-  dut_ip_addr = get_ip_addr('dut1',@test_params.params_control.iface[0])
+def run  
+  test_type = @test_params.params_control.type[0]
+  interface_num = @test_params.params_control.instance_variable_defined?(:@interface_num) ? @test_params.params_control.interface_num[0] : 1
+  array_of_interfaces = Array.new
+  if (interface_num.to_i > 1)
+    array_of_interfaces = get_eth_interfaces
+  else
+    array_of_interfaces = [@test_params.params_control.iface[0]] 
+  end
   if @test_params.params_control.type.length > 1
     test_vars = @test_params.params_control.type[1]
   else
@@ -47,25 +55,28 @@ def run
   if test_vars != nil && test_vars.match(/stress/i)
     start_genload()
   end
+
   run_start_stats
+
   test_type = @test_params.params_control.type[0]
-  if (test_type.match(/udp/i))
-  @equipment['server1'].send_cmd("iperf -c #{dut_ip_addr} -l #{@test_params.params_control.packet_size[0]} -f M -u -t #{@test_params.params_control.time[0]} -b #{@test_params.params_control.bandwidth[0]} -w 128k", 
+  array_of_interfaces.each{|dut_eth|
+      dut_ip=get_ip_addr('dut1', dut_eth)
+      if (test_type.match(/udp/i))
+          @equipment['server1'].send_cmd("iperf -c #{dut_ip} -l #{@test_params.params_control.packet_size[0]} -f M -u -t #{@test_params.params_control.time[0]} -b #{@test_params.params_control.bandwidth[0]} -w 128k &", 
                                  @equipment['server1'].prompt,
                                  @test_params.params_control.timeout[0].to_i)
-  elsif (test_type.match(/tcp/i))
-   direction = @test_params.params_control.direction[0].match(/bi/i)? '-d':''
-   @equipment['server1'].send_cmd("iperf -c #{dut_ip_addr} -m -M  #{@test_params.params_control.packet_size[0]} -f M #{direction} -t #{@test_params.params_control.time[0]} -w  #{@test_params.params_control.window[0]}",@equipment['server1'].prompt,@test_params.params_control.timeout[0].to_i)
-  end
+      elsif (test_type.match(/tcp/i))
+          direction = @test_params.params_control.direction[0].match(/bi/i)? '-d':''
+          @equipment['server1'].send_cmd("iperf -c #{dut_ip} -m -M  #{@test_params.params_control.packet_size[0]} -f M #{direction} -t #{@test_params.params_control.time[0]} -w  #{@test_params.params_control.window[0]} &",@equipment['server1'].prompt,@test_params.params_control.timeout[0].to_i)
+      end
+     
+     }
+  sleep @test_params.params_control.time[0].to_i
   run_stop_stats 
   if test_vars != nil && test_vars.match(/stress/i)
     stop_genload  
   end
 
-  if (test_type.match(/tcp/i) && @test_params.params_control.instance_variable_defined?(:@interrupt_pacing_interval)
-)
-   @equipment['dut1'].send_cmd("set_ethtool_coalesce_options.sh -d #{@test_params.params_control.iface[0]} -p 'rx-usecs' -n 16", @equipment['dut1'].prompt, 3)
-end
   run_data = @equipment['server1'].response                                 
   @equipment['server1'].send_cmd("echo $?",/^0/m, 2)
   return_non_zero = @equipment['server1'].timeout?
--- LSP/A-ETH/eth_iperf.rb
+++ LSP/A-ETH/eth_iperf.rb
@@ -6,38 +6,37 @@ def setup
   test_type = @test_params.params_control.type[0]
   interface_num = @test_params.params_control.instance_variable_defined?(:@interface_num) ? @test_params.params_control.interface_num[0] : 1
   array_of_interfaces = Array.new 
-  array_of_server_ips = Array.new
 
   if (test_type.match(/udp/i))
-    @equipment['dut1'].send_cmd("sysctl -w net.core.rmem_max=33554432", @equipment['dut1'].prompt, 3)
-    @equipment['dut1'].send_cmd("sysctl -w net.core.wmem_max=33554432", @equipment['dut1'].prompt, 3)
-    @equipment['dut1'].send_cmd("sysctl -w net.core.rmem_default=33554432", @equipment['dut1'].prompt, 3)
-    @equipment['dut1'].send_cmd("sysctl -w net.core.wmem_default=33554432", @equipment['dut1'].prompt, 3)
-    @equipment['dut1'].send_cmd("sysctl -w net.ipv4.udp_mem='4096 87380 33554432'", @equipment['dut1'].prompt, 3)
-    @equipment['dut1'].send_cmd("sysctl -w net.ipv4.route.flush=1", @equipment['dut1'].prompt, 3)
+    set_eth_sys_control_optimize('dut1')
   end
+
+  kill_process('iperf', :this_equipment => @equipment['server1'], :use_sudo => true)
+  kill_process('iperf')
 
 # this part of test application triggers throughput measurement on the specified number of interfaces simultaneously and reports sum of throughput on all interfaces
   if (interface_num.to_i > 1)
-    @equipment['server1'].send_cmd_nonblock("killall iperf", @equipment['server1'].prompt, 10)
     array_of_interfaces = get_eth_interfaces
-    index=0
+
     array_of_interfaces.each{|dut_eth|
-         @equipment['dut1'].send_cmd("ifdown #{dut_eth}", @equipment['dut1'].prompt, 3)
-         @equipment['dut1'].send_cmd("ifup #{dut_eth}", @equipment['dut1'].prompt, 10)
-         array_of_server_ips<<get_eth_server(dut_eth)
-         test_cmd = test_type.match(/udp/i) ? "iperf -s -B #{array_of_server_ips[index]} -u -w 128k &": "iperf -s -B #{array_of_server_ips[index]} &"
+    
+         run_down_up_udhcpc('dut1', dut_eth)
+         serverip=get_eth_server(dut_eth, 'dut1', 'server1')
+         @equipment['dut1'].send_cmd("export IPERF#{dut_eth}HOST=#{serverip}", @equipment['dut1'].prompt)
+
+         test_cmd = test_type.match(/udp/i) ? "iperf -s -B #{serverip} -u -w 128k &": "iperf -s -B #{serverip} &"
          @equipment['server1'].send_cmd(test_cmd, /Server\s+listening.*?#{test_type}\sport.*?/i, 10)
-         index=index+1
+
         }
    return
-  end
 # end of multi-interface throughput logic
-         
-  test_cmd = test_type.match(/udp/i) ? "iperf -s -u -w 128k &" : "iperf -s &"
-  if !is_iperf_running?(test_type)
-    @equipment['server1'].send_cmd_nonblock(test_cmd, /Server\s+listening.*?#{test_type}\sport.*?/i, 10)
+  else
+    test_cmd = test_type.match(/udp/i) ? "iperf -s -u -w 128k &" : "iperf -s &"
+    if !is_iperf_running?(test_type)
+      @equipment['server1'].send_cmd_nonblock(test_cmd, /Server\s+listening.*?#{test_type}\sport.*?/i, 10)
+    end
   end
+         
   if !is_iperf_running?(test_type)
     raise "iperf can not be started. Please make sure iperf is installed at the #{@equipment['server1'].telnet_ip} server"    
   end
@@ -51,29 +50,6 @@ def is_iperf_running?(type)
   else
     return true
   end
-end
-
-def get_eth_interfaces()
-  interface_arr = Array.new
-  @equipment['dut1'].send_cmd("ls /sys/class/net|grep eth")
-  eth_interface_list = @equipment['dut1'].response
-  eth_interface_arr = eth_interface_list.split(/[\n\r]+/)
-  eth_interface_arr.each{|eth|
-      if (eth.match(/eth\d/))
-         interface_arr << eth
-      end
-    }
-  return interface_arr
-end
-
-def get_eth_server(interface_name)
-    ip_addr = ''
-    ip_addr = get_remote_ip(interface_name, 'dut1', 'server1')
-    if (ip_addr == '')
-       return [FrameworkConstants::Result[:fail], "Server does not have ethernet interface corresponding to dut's #{interface_name}. Please emsure host machine has an interface on subnet of each dut interface.\n"]
-   end
-    @equipment['dut1'].send_cmd("export IPERF#{interface_name}HOST=#{ip_addr}", @equipment['dut1'].prompt)
-    ip_addr
 end
 
 # Determine test result outcome and save performance data
