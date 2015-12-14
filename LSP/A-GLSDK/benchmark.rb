4eb47f35bbafa191dd715df392e5ba397c9f28a3
Alejandro Hernandez <ajhernandez@ti.com>
2015-12-14 17:57:36 -0500

    A-GLSDK: Add opp stats to benchmark test
    This patch adds code to collect the opp information
    during the benchmark test
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>

--- LSP/A-GLSDK/benchmark.rb
+++ LSP/A-GLSDK/benchmark.rb
@@ -25,15 +25,30 @@ def run
   tests_table = Hash[*tests_table]
   perf_data = []
   tests = @test_params.params_chan.tests.join().downcase.strip() == 'all' ? tests_table.keys() : @test_params.params_chan.tests
+  @equipment['dut1'].send_cmd("omapconf show opp", @equipment['dut1'].prompt)
+  opp_info = @equipment['dut1'].response.scan(/^\|.*/i)
+  m_name = opp_info[1].scan(/[^\|]+/).collect { |m| m.strip() }
+  opp_info[3..-1].each do |c_opp|
+    current_opp = c_opp.scan(/[^\|]+/).collect { |m| m.strip() }
+    next if current_opp.length() < 5
+    name = current_opp[0].strip().gsub(/\s+/,'')
+    idx = current_opp[m_name.index('Voltage')] == '' ? m_name.index('Frequency') : m_name.index('Voltage')
+    m_info = current_opp[idx].strip().split(/\s+/)
+    if m_info.length() == 2
+       perf_data << {'name' => "#{name}-#{m_name[idx]}",
+                     'units' => m_info[1],
+                     'values' => m_info[0]}
+    end
+  end
   tests.each do |test|
     @equipment['dut1'].send_cmd("#{execfile} -t #{tests_table[test]} -d ./data ", @equipment['dut1'].prompt, 600)
     non_zero = false
     fps = @equipment['dut1'].response.scan(/(?<=FPS = )[\d\.]+/i).select{ |f| non_zero |= f.to_f > 0 }
     metrics = @equipment['dut1'].response.match(/^Log:\s*Application::FinishCurrentTest\((.*?):\s*([\d\.]+)\s*([\w\/]+)\s*(.*?)FPS/i).captures
-    perf_data << {'name' => "#{test.sub(/^.*?_/,'')}-fps",
+    perf_data << {'name' => "#{tests_table[test]}-#{test.sub(/^.*?_/,'')}-fps",
                   'units' => 'fps',
                   'values' => fps.length() > 0 ? fps : metrics[3]}
-    perf_data << {'name' => "#{test.sub(/^.*?_/,'')}-#{metrics[0].gsub(/\s+/,'-')}",
+    perf_data << {'name' => "#{tests_table[test]}-#{test.sub(/^.*?_/,'')}-#{metrics[0].gsub(/\s+/,'-')}",
                   'units' => metrics[2].gsub(/\s+/,'-'),
                   'values' => metrics[1]}
   end
