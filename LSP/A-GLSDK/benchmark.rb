f837ab0ac5285a5f58ec7304fcf2d7469d3f4b31
Alejandro Hernandez <ajhernandez@ti.com>
2015-12-18 12:37:48 -0500

    A-GLSDK: Make capturing OPP settings conditional
    This patch make capturing the OPP settings metrics
    conditional. The patch checks for the exitence of
    show_opp as a test parameter before capturing the
    OPP settings in the test
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>

--- LSP/A-GLSDK/benchmark.rb
+++ LSP/A-GLSDK/benchmark.rb
@@ -25,20 +25,22 @@ def run
   tests_table = Hash[*tests_table]
   perf_data = []
   tests = @test_params.params_chan.tests.join().downcase.strip() == 'all' ? tests_table.keys() : @test_params.params_chan.tests
-  @equipment['dut1'].send_cmd("omapconf show opp", @equipment['dut1'].prompt)
-  opp_info = @equipment['dut1'].response.scan(/^\|.*/i)
-  if opp_info.length() > 3
-    m_name = opp_info[1].scan(/[^\|]+/).collect { |m| m.strip() }
-    opp_info[3..-1].each do |c_opp|
-      current_opp = c_opp.scan(/[^\|]+/).collect { |m| m.strip() }
-      next if current_opp.length() < 5
-      name = current_opp[0].strip().gsub(/\s+/,'')
-      idx = current_opp[m_name.index('Voltage')] == '' ? m_name.index('Frequency') : m_name.index('Voltage')
-      m_info = current_opp[idx].strip().split(/\s+/)
-      if m_info.length() == 2
-         perf_data << {'name' => "#{name}-#{m_name[idx]}",
-                       'units' => m_info[1],
-                       'values' => m_info[0]}
+  if @test_params.params_chan.instance_variable_defined?(:@show_opp)
+    @equipment['dut1'].send_cmd("omapconf show opp", @equipment['dut1'].prompt)
+    opp_info = @equipment['dut1'].response.scan(/^\|.*/i)
+    if opp_info.length() > 3
+      m_name = opp_info[1].scan(/[^\|]+/).collect { |m| m.strip() }
+      opp_info[3..-1].each do |c_opp|
+        current_opp = c_opp.scan(/[^\|]+/).collect { |m| m.strip() }
+        next if current_opp.length() < 5
+        name = current_opp[0].strip().gsub(/\s+/,'')
+        idx = current_opp[m_name.index('Voltage')] == '' ? m_name.index('Frequency') : m_name.index('Voltage')
+        m_info = current_opp[idx].strip().split(/\s+/)
+        if m_info.length() == 2
+           perf_data << {'name' => "#{name}-#{m_name[idx]}",
+                         'units' => m_info[1],
+                         'values' => m_info[0]}
+        end
       end
     end
   end
@@ -47,10 +49,13 @@ def run
     non_zero = false
     fps = @equipment['dut1'].response.scan(/(?<=FPS = )[\d\.]+/i).select{ |f| non_zero |= f.to_f > 0 }
     metrics = @equipment['dut1'].response.match(/^Log:\s*Application::FinishCurrentTest\((.*?):\s*([\d\.]+)\s*([\w\/]+)\s*(.*?)FPS/i).captures
-    perf_data << {'name' => "#{tests_table[test]}-fps-#{test.sub(/^.*?_/,'')}",
+    perf_data << {'name' => 'test-number',
+                  'units' => 'none',
+                  'values' => tests_table[test]}
+    perf_data << {'name' => "fps-#{test.sub(/^.*?_/,'')}",
                   'units' => 'fps',
                   'values' => fps.length() > 0 ? fps : metrics[3]}
-    perf_data << {'name' => "#{tests_table[test]}-#{metrics[0].gsub(/\s+/,'-')}-#{test.sub(/^.*?_/,'')}",
+    perf_data << {'name' => "#{metrics[0].gsub(/\s+/,'-')}-#{test.sub(/^.*?_/,'')}",
                   'units' => metrics[2].gsub(/\s+/,'-'),
                   'values' => metrics[1]}
   end
