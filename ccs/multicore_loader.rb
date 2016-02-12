de3dab988e35c572b95142c9fdb9d6a22c274d81
John Godbey <j-godbey@ti.com>
2016-02-12 17:19:47 -0500

    ccs: multicore_loader support for ROV and all platforms
    This patch reworks multicore_loader to support running the coredump
    utility to read ROV data.  The loader now accepts any generic binary
    and uses a lookup table in multicore_data to determine which cores to
    load.  Some of the changes needed in the loader to facilitate the
    coredump require changes in the bench file, so a sample bench has been
    provided.
    
    Signed-off-by: John Godbey <j-godbey@ti.com>
    Signed-off-by: Elvita Lobo <elvita@ti.com>

--- ccs/multicore_loader.rb
+++ ccs/multicore_loader.rb
@@ -1,66 +1,152 @@
 # Multicore Loader - takes cores and binaries and loads and runs them
 require File.dirname(__FILE__)+'/default_ccs'
 require File.dirname(__FILE__)+'/../LSP/lsp_helpers.rb'
+require File.dirname(__FILE__)+'/../ipc/multicore_data.rb'
 include CcsTestScript
 include LspHelpers
+include MulticoreData
+
+@base_dir = ""
+@rtos_bins_dir = ""
+@test_name = ""
+
 def setup
+  #set the addresses for the coredump tar.gz and the ipc binaries
+  coredump_tar = SiteInfo::COREDUMP_UTIL
   rtos_bins = @test_params.instance_variable_defined?(:@rtos_bins) ? @test_params.rtos_bins : nil
-  if (rtos_bins == nil) 
-    raise "Please specify test bins tarball"  
+  if (rtos_bins == nil)
+    raise "Please specify ipc bins tarball"
   end
+
+  #set the binary prefix--can change based on the build
+  @base_dir = @test_params.params_chan.instance_variable_defined?(:@path_prefix) ?@test_params.params_chan.path_prefix.to_s : nil
+  if (@base_dir == nil)
+    raise "Please specify the prefix path i.e. '/mytestdir/bios_66AK2E_norebuild/ipc_3_42_00_00_eng'"
+  end
+  @base_dir = @base_dir[3...-4]
+
+  #create the temp directory and untar the binaires
   @linux_temp_folder = File.join(SiteInfo::LINUX_TEMP_FOLDER,@test_params.staf_service_name.to_s)
-  @RTOS_BINS_DIR = "#{@linux_temp_folder}/rtos_bins"
-  @equipment['server1'].send_cmd("mkdir -p #{@RTOS_BINS_DIR}")
-  untar(rtos_bins, @RTOS_BINS_DIR)
+  @rtos_bins_dir = "#{@linux_temp_folder}/rtos_bins"
+  @equipment['server1'].send_cmd("mkdir -p #{@rtos_bins_dir}")
+  untar(rtos_bins, @rtos_bins_dir)
 
-  cores =  @test_params.params_chan.instance_variable_defined?(:@core_list) ?@test_params.params_chan.core_list[0].tr('"','').split(" ") : "*"
+  #wget the coredump utilities and untar into the temp dir
+  @equipment['server1'].send_cmd("wget --directory-prefix=#{@rtos_bins_dir} #{coredump_tar}",Regexp.new('.*'),0)
+  coredump = "#{@rtos_bins_dir}/coredump.tar.gz"
+  @equipment['server1'].send_cmd("mkdir -p #{@rtos_bins_dir}/xdc/rov")
+  untar(coredump, @rtos_bins_dir+"/xdc/rov")
 
-  bins = @test_params.params_chan.instance_variable_defined?(:@bin_list) ?@test_params.params_chan.bin_list[0].split(" ") : nil
-
-  if (bins == nil)
-    raise "must specify at least one binary to load"
+  #check for specified test
+  @test_name = @test_params.params_chan.instance_variable_defined?(:@ipc_test) ?@test_params.params_chan.ipc_test.to_s : ""
+  if (@test_name == "")
+    raise "please specify a test to run"
   end
+  @test_name.gsub!(/\W+/, '')
 
-  bins.each { |word| word[0,0] = ("#{@RTOS_BINS_DIR}/") }
+  #get the cores and binaries out of MulticoreIPCDATA
+  cores = get_data(@equipment['dut1'].name,@test_name,"cores")
+  bins = get_data(@equipment['dut1'].name,@test_name,"binaries")
+
+  #prepend temp dir path to the relative bin path
+  bins.each { |word| word[0,0] = ("#{@rtos_bins_dir}#{@base_dir}") }
   bins.each { |word| word.tr!('"','') }
 
-  if (bins.length != cores.length && bins.length != 1)
-    raise "Number of cores and binaries must match or there must be a single binary"
-  end
- 
+  #set the values that get picked up by auto_main.js
   @equipment['dut1'].params['outFile'] = bins
   @equipment['dut1'].params['ccsCpu'] = cores
-  @equipment['dut1'].params['ccsPlatform'] = "*"  
+  @equipment['dut1'].params['ccsPlatform'] = "*"
   super
 end
 
 def run
-
-	thr0 = Thread.new() {
-               @equipment['dut1'].run "",
-                          1000,
-                          {'reset' => 'yes',
-                           'no_profile' => 'yes',
-                           'timeout' => @test_params.params_chan.instance_variable_defined?(:@timeout) ?@test_params.params_chan.timeout[0] : '9000'}
+  #run auto_main.js
+  thr0 = Thread.new() {
+             @equipment['dut1'].run "",
+                        1000,
+                        {'reset' => 'yes',
+                         'no_profile' => 'yes',
+                         'timeout' => @test_params.params_chan.instance_variable_defined?(:@timeout) ?@test_params.params_chan.timeout[0] : '9000'}
        }
-	
-    thr0.join()
-    puts @equipment['dut1'].response
-    criteria_regex = @test_params.params_chan.instance_variable_defined?(:@pass_fail) ? Regexp.new(@test_params.params_chan.pass_fail[0].to_s.tr!('"',''), Regexp::MULTILINE) : nil
-  
-  if @equipment['dut1'].response.match(criteria_regex)
+
+  #kill the thread and print the result
+  thr0.join()
+  puts @equipment['dut1'].response
+
+  #set regular expression for the pass/fail criteria
+  criteria_regex = Regexp.new("#{get_data(@equipment['dut1'].name,@test_name,"output")}", Regexp::MULTILINE)
+  passed = false
+
+  #if the test does not use ROV, match the print statements to pass/fail
+  rov = get_data(@equipment['dut1'].name,@test_name,"ROV")
+  if (!rov)
+    if @equipment['dut1'].response.match(criteria_regex)
+      passed = true
+    end
+
+  #if the test does use ROV, dump the logs and pass/fail on those
+  else
+    passed = check_ROV(criteria_regex, rov)
+  end
+
+  #set the test result
+  if passed
     set_result(FrameworkConstants::Result[:pass], "Test Passed")
   else
     set_result(FrameworkConstants::Result[:fail], "Test Failed")
   end
 end
+
 def clean
-  #@equipment['server1'].send_cmd("rm -r #{@RTOS_BINS_DIR}")
+  #remove the coredump utility
+  @equipment['server1'].send_cmd("rm -r #{@rtos_bins_dir}/xdc",Regexp.new('.*'),0)
+
+  #remove the ROV text dumps, if they exist
+  bins = @equipment['dut1'].params['outFile']
+  bins.each { |word| @equipment['server1'].send_cmd("rm -r #{word}.txt",Regexp.new('.*'),0)}
+
+  #remove the remainder of the rtos binaries
+  @equipment['server1'].send_cmd("rm -r #{@rtos_bins_dir}",Regexp.new('.*'),0)
 end
 
-def untar(tar,dir) 
+def untar(tar,dir)
   params = {}
   params['server'] = @equipment['server1']
   tar_options = get_tar_options(tar,params)
-  @equipment['server1'].send_cmd("tar #{tar_options} #{tar} -C #{dir}")
+  @equipment['server1'].send_cmd("tar #{tar_options} #{tar} -C #{dir}",Regexp.new('.*'),0)
+end
+
+def check_ROV(criteria_regex, rov_paths)
+  #setting the paths used by the coredump utility
+  status = true
+  bins = @equipment['dut1'].params['outFile']
+  ccs_install_dir = @equipment['dut1'].params['ccs_install_dir']
+  jre = ccs_install_dir+"/eclipse/jre"
+  xdc = @equipment['dut1'].params['xdctools']
+  rovtools = "#{@rtos_bins_dir}"
+  bios = @equipment['dut1'].params['bios']
+  dss = ccs_install_dir+"/ccs_base/DebugServer/packages"
+
+  #prepend the temp dir path to the relative rov.xs path
+  rov_paths.each { |word| word[0,0] = ("#{@rtos_bins_dir}#{@base_dir}") }
+  rov_paths.each { |word| word.tr!('"','') }
+
+  #allow for specialized ROV fields, default to LoggerBuf
+  rov_field = get_data(@equipment['dut1'].name,@test_name,"field")
+  if (!rov_field)
+    rov_field = "xdc.runtime.LoggerBuf Records"
+  end
+
+  #move the rov.xs to the appropriate dir then run the coredump on each core
+  passed = true
+  (0..bins.length - 1).each do |i|
+    @equipment['server1'].send_cmd("cp #{rov_paths[i]} `dirname #{bins[i]}`")
+    @equipment['server1'].send_cmd("export XDCTOOLS_JAVA_HOME=#{jre};export XDCPATH=#{rovtools}\\;#{bios}\\;#{dss}\\;$XDCPATH;echo \"m #{rov_field}\\nq\"| #{xdc}/xs xdc.rov.coredump -e #{bins[i]} -d #{bins[i]}.raw 2>&1 | tee #{bins[i]}.txt")
+
+    #if any core fails, the whole test fails
+    if !(File.open("#{bins[i]}.txt").read() =~ criteria_regex)
+      status = false
+    end
+  end
+  return status
 end
---
+++ ipc/multicore_data.rb
@@ -0,0 +1,79 @@
+module MulticoreData
+
+  # Data list for each test on each platform
+  def get_data(platform, test, data)
+    test = test.upcase
+    data = data.upcase
+
+    #initial table structure
+    data_list = {}
+    data_list['k2hk-evm'] = { 'PING'     => {'CORES' => %w[C66xx_0 C66xx_1 C66xx_2 C66xx_3 C66xx_4 C66xx_5 C66xx_6 C66xx_7 arm_A15_0],
+                                             'BINARIES' => [],
+                                             'ROV' => [],
+                                             'OUTPUT' => ""},
+                              'MESSAGEQ' => {'CORES' => %w[C66xx_0 arm_A15_0],
+                                             'BINARIES' => [],
+                                             'ROV' => [],
+                                             'OUTPUT' => ""}
+                            }
+    data_list['k2e-evm'] = { 'PING'     => {'CORES' => %w[C66xx_0 arm_A15_0],
+                                             'BINARIES' => [],
+                                             'ROV' => [],
+                                             'OUTPUT' => ""},
+                             'MESSAGEQ' => {'CORES' => %w[C66xx_0 arm_A15_0],
+                                             'BINARIES' => [],
+                                             'ROV' => [],
+                                             'OUTPUT' => ""}
+                           }
+    data_list['k2l-evm'] = { 'PING'     => {'CORES' => %w[C66xx_0 C66xx_1 C66xx_2 C66xx_3 arm_A15_0],
+                                             'BINARIES' => [],
+                                             'ROV' => [],
+                                             'OUTPUT' => ""},
+                             'MESSAGEQ' => {'CORES' => %w[C66xx_0 arm_A15_0],
+                                             'BINARIES' => [],
+                                             'ROV' => [],
+                                             'OUTPUT' => ""}
+                           }
+    data_list['k2g-evm'] = { 'PING'     => {'CORES' => %w[C66xx_0 arm_A15_0],
+                                             'BINARIES' => [],
+                                             'ROV' => [],
+                                             'OUTPUT' => ""},
+                             'MESSAGEQ' => {'CORES' => %w[C66xx_0 arm_A15_0],
+                                             'BINARIES' => [],
+                                             'ROV' => [],
+                                             'OUTPUT' => ""}
+                           }
+
+    platform_token = {}
+    platform_token['k2hk-evm'] = "TCI6636_bios_elf"
+    platform_token['k2e-evm'] = "66AK2E_bios_elf"
+    platform_token['k2l-evm'] = "TCI6630_bios_elf"
+    platform_token['k2g-evm'] = "66AK2G_bios_elf"
+
+    key_platforms = ['k2hk-evm', 'k2e-evm', 'k2l-evm', 'k2g-evm']
+
+    #populate the lists
+    key_platforms.each { |evm|
+    (0..data_list[evm]['PING']['CORES'].length - 2).each do |i|
+      data_list[evm]['PING']['BINARIES'][i] = "/examples/#{platform_token[evm]}/ex11_ping/core#{i}/bin/debug/server_core#{i}.xe66"
+      data_list[evm]['PING']['ROV'][i] = "/examples/#{platform_token[evm]}/ex11_ping/core#{i}/bin/debug/configuro/package/cfg/Core#{i}_pe66.rov.xs"
+    end
+    data_list[evm]['PING']['BINARIES'][data_list[evm]['PING']['CORES'].length - 1] = "/examples/#{platform_token[evm]}/ex11_ping/host/bin/debug/server_host.xa15fg"
+    data_list[evm]['PING']['ROV'][data_list[evm]['PING']['CORES'].length - 1] = "/examples/#{platform_token[evm]}/ex11_ping/host/bin/debug/configuro/package/cfg/Host_pa15fg.rov.xs"
+    data_list[evm]['PING']['OUTPUT'] = "Main(Core[0-9]|Host)_done"
+    }
+
+    key_platforms.each { |evm|
+    (0..data_list[evm]['MESSAGEQ']['CORES'].length - 2).each do |i|
+      data_list[evm]['MESSAGEQ']['BINARIES'][i] = "/examples/#{platform_token[evm]}/ex02_messageq/core#{i}/bin/debug/server_core#{i}.xe66"
+      data_list[evm]['MESSAGEQ']['ROV'][i] = "/examples/#{platform_token[evm]}/ex02_messageq/core#{i}/bin/debug/configuro/package/cfg/Core#{i}_pe66.rov.xs"
+    end
+    data_list[evm]['MESSAGEQ']['BINARIES'][data_list[evm]['MESSAGEQ']['CORES'].length - 1] = "/examples/#{platform_token[evm]}/ex02_messageq/host/bin/debug/app_host.xa15fg"
+    data_list[evm]['MESSAGEQ']['ROV'][data_list[evm]['MESSAGEQ']['CORES'].length - 1] = "/examples/#{platform_token[evm]}/ex02_messageq/host/bin/debug/configuro/package/cfg/Host_pa15fg.rov.xs"
+    data_list[evm]['MESSAGEQ']['OUTPUT'] = "(<-- Server_delete|App_delete: <--)"
+    }
+
+    raise "The multicore data table does not have the entry for #{platform}; Please add the entry in 'ipc/multicore_ipc_data.rb'!" if !data_list.has_key?(platform)
+    return data_list[platform][test][data]
+  end
+end
