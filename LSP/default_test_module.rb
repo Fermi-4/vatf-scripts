4383933a0a39439b7d631eec75c70da7030fad62
Carlos Hernandez <ceh@ti.com>
2016-02-18 11:58:28 -0500

    LspTestScript: Add autologin option
    Some filesystems do not require users to login and instead system
    boots to system prompt directly, so support such use case.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- LSP/default_test_module.rb
+++ LSP/default_test_module.rb
@@ -253,6 +253,7 @@ module LspTestScript
     boot_params['bootargs_append'] = @test_params.params_control.bootargs_append[0] if @test_params.params_control.instance_variable_defined?(:@bootargs_append)
     boot_params['bootargs'] = @test_params.var_bootargs if @test_params.instance_variable_defined?(:@var_bootargs)
     boot_params['var_boot_timeout']  = @test_params.var_boot_timeout  if @test_params.instance_variable_defined?(:@var_boot_timeout)
+    boot_params['autologin'] = @test_params.var_autologin if @test_params.instance_variable_defined?(:@var_autologin)
     boot_params
   end
   
