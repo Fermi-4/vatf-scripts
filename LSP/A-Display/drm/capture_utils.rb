86ba776122a87cbe0a56f9e40c544f01a6014b5f
Alejandro Hernandez <ajhernandez@ti.com>
2016-05-09 16:10:23 -0400

    A-Display: Add HDMI display drm auto test
    This patch adds a new capture-utils lib and DRM test script
    that may be used to validate HDMI display functionality.
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>

---
+++ LSP/A-Display/drm/capture_utils.rb
@@ -0,0 +1,187 @@
+module CaptureUtils
+  CAP_UTILS_FOLDER = File.join(SiteInfo::UTILS_FOLDER, 'capture-utils')
+  HOST_UTILS_URL = 'http://gtopentest-server.gt.design.ti.com/anonymous/common/Multimedia/host-utils'
+  
+  def get_ref_files_name(mode, fmt, plane)
+    fields = ['p']
+    fields = ['f1', 'f2'] if mode[-1] == 'i'
+    ref_name = "ref_#{mode}"
+    p_info = ''
+    p_info = ["_P",
+              plane['xyoffset'][0],
+              plane['xyoffset'][1],
+              plane['scale'],
+              plane['format']].join('_') if plane
+    fields.collect{ |f| ref_name + f + "-#{fmt}" + p_info + ".rgb"}
+  end
+  
+  def install_utils(cap_sys=@equipment['server1'])
+      capture_bin = File.join(CAP_UTILS_FOLDER, 'Capture')
+      psnr_bin = File.join(CAP_UTILS_FOLDER, 'argb-psnr-ssim')
+      if !File.exists?(capture_bin) || !File.exists?(psnr_bin)
+        cap_sys.send_cmd("wget --no-proxy #{HOST_UTILS_URL}/capture-utils.tar.gz -P #{SiteInfo::UTILS_FOLDER} || " \
+                         "wget #{HOST_UTILS_URL}/capture-utils.tar.gz -P #{SiteInfo::UTILS_FOLDER}",
+                         cap_sys.prompt,
+                         180)
+        cap_sys.send_cmd("tar -C #{SiteInfo::UTILS_FOLDER} -zxvf #{File.join(SiteInfo::UTILS_FOLDER, 'capture-utils.tar.gz')}",
+                      cap_sys.prompt,
+                      180)
+        raise "Unable to fetch capture utility" if !File.exists?(capture_bin) || !File.exists?(psnr_bin)
+      end
+  end
+  
+  def change_uyvy_to_rgb(src_file, width, height, cap_sys=@equipment['server1'])
+    res_file = src_file+'.rgb24'
+    cap_sys.send_cmd("avconv -pix_fmt uyvy422 -s #{width}x#{height} -f rawvideo -i #{src_file} " \
+                     "-pix_fmt rgb24 -s #{width}x#{height} -f rawvideo #{res_file}" ,
+                      cap_sys.prompt,
+                      180)
+    res_file
+  end
+  
+  def get_ref_file(mode, format, plane, cap_sys=@equipment['server1'])
+    fmt = format ? format : 'XR24'
+    cap_sys.send_cmd("mkdir #{@linux_temp_folder}", cap_sys.prompt) if !File.exists?(@linux_temp_folder)
+    cap_sys.send_cmd("rm #{@linux_temp_folder}/ref_*.rgb*", cap_sys.prompt)
+    result = []
+    get_ref_files_name(mode, fmt, plane).each do |f_name|
+      f_base_name = f_name + '.tar.xz'
+      remote_url = "#{HOST_UTILS_URL}/ref-media/#{f_base_name}"
+      local_file = File.join(@linux_temp_folder, f_base_name)
+      cap_sys.send_cmd("wget --no-proxy #{remote_url} -O #{local_file} || " \
+                      "wget #{remote_url} -O #{local_file} || rm #{local_file}",
+                      cap_sys.prompt,
+                      600)
+
+      cap_sys.send_cmd("tar -C #{@linux_temp_folder} -Jxvf #{local_file} || rm #{local_file}",
+                      cap_sys.prompt,
+                      600)
+      result << File.join(@linux_temp_folder, f_name) if !cap_sys.response.match(/Error/i)
+    end
+    result
+  end
+
+  def get_psnr_ssim_argb(ref, test, width, height, num_comp, sys=@equipment['server1'])
+    result = []
+    install_utils(sys)
+    ssim = File.join(CAP_UTILS_FOLDER, 'argb-psnr-ssim')
+    sys.send_cmd("LD_LIBRARY_PATH=#{CAP_UTILS_FOLDER} #{ssim} #{ref} #{test} #{width} #{height} #{num_comp} 0", sys.prompt, 1000)
+    a_psnr=/\s*A=([-\d\.]+)dB,/
+    a_ssim=/\s*A=([-\d\.]+)%,/
+    if num_comp != 4
+      a_psnr=//
+      a_ssim=//
+    end
+    sys.response.scan(/^Frame\s*#\d+:\s+PSNR:#{a_psnr}\s*R=([-\d\.]+)dB,\s*G=([-\d\.]+)dB,\s*B=([-\d\.]+)dB\s*\|\|\s*SSIM:#{a_ssim}\s*R=([-\d\.]+)%,\s*G=([-\d\.]+)%,\s*B=([-\d\.]+)%/i) do |v1, v2, v3, v4, v5, v6, v7, v8| 
+      result << {'psnr' => {'a' => v1.to_f, 'r' => v2.to_f, 'g' => v3.to_f, 'b' => v4.to_f},
+                 'ssim' => {'a' => v5.to_f, 'r' => v6.to_f, 'g' => v7.to_f, 'b' => v8.to_f}}
+    end
+    result
+  end
+
+  #Class to control a blackmagicdesign card based on the Capture example 
+  #provided with their SDK 
+  class MediaCapture
+
+    EDIDS = {
+      'Intensity Pro 4K' => '00ffffffffffff0009a4000001000000' \
+                            '341601038047289612daffa3584aa229' \
+                            '17494b20000001010101010101010101' \
+                            '010101010101011d8018711c1620582c' \
+                            '2500c48e2100009e011d007251d01e20' \
+                            '6e285500c48e2100001e000000fc0042' \
+                            '4d442048444d490a20202020000000fd' \
+                            '00323c0f2d08000a20202020202001fc' \
+                            '02032a764e858486949395a0a1a29f90' \
+                            '021101230f0704831f00006e030c0000' \
+                            '00383c200080010203048c0aa01451f0' \
+                            '1600267c4300138e21000098011d80d0' \
+                            '721c1620102c2580c48e2100009e011d' \
+                            '00bc52d01e20b8285540c48e2100001e' \
+                            '8c0aa02051201810187e2300138e2100' \
+                            '009800000000000000000000000000dc'
+    }
+
+    def initialize(cap_sys)
+      @capture_modes = {}
+      @capture_bin = File.join(CAP_UTILS_FOLDER, 'Capture')
+      @sys = cap_sys
+      install_utils(@sys)
+      @sys.send_cmd("#{@capture_bin} -d 0")
+      @pixel_fmts = {}
+      @model = @sys.response.match(/(?<=device id.:).*?\d+:([^\(]+)\(selected\)/im)[1].strip()
+      @sys.response.scan(/(\d+):\s*(.*?)(\d+\s*x\s*\d+)\s*([\d\.]+)\s*FPS/i) do |val, name, resolution, fr|
+        res = resolution.gsub(/\s+/,'')
+        next if name.match(/ntsc|pal/i) #disabling ntsc and pal since card has problems with this modes
+        @capture_modes[res] = {} if !@capture_modes.has_key?(res)
+        @capture_modes[res][fr] = {} if !@capture_modes[res].has_key?(fr)
+        @capture_modes[res][fr][name.include?('p') ? 'p' : 'i'] = 
+                            {'name' => name.strip(),
+                            'val' => val}
+      end
+      @sys.response.scan(/(\d+):\s*(\d+\s*bit\s*[^\d\s]+)/i) do |val, name|
+        @pixel_fmts[name.gsub(/\s+/,'-').downcase()] = val
+      end
+    end
+    
+    def capture_media(v_name, a_name, width, height, framerate, interlace, cap_secs=10, a_chans=2, a_bits=16)
+      fr = framerate.to_i
+      n_h = height == 480 ? 486 : height.to_i
+      n_w = width.to_i
+      pixel = @pixel_fmts['8-bit-argb']
+      n_c = 4
+      inter = interlace.downcase()
+      if [720, 480].include?(n_w) && [486, 576].include?(n_h) && [50, 60].include?(fr)
+        pixel = @pixel_fmts['8-bit-yuv']
+        n_c = 3
+      end
+      num_frames = (cap_secs*fr).to_i
+      if inter == 'p' && @capture_modes[n_w.to_s+'x'+n_h.to_s][fr.to_s] &&
+         @capture_modes[n_w.to_s+'x'+n_h.to_s][fr.to_s][inter]
+        mode =  @capture_modes[n_w.to_s+'x'+n_h.to_s][fr.to_s][inter]['val']
+      elsif inter == 'i' && @capture_modes[n_w.to_s+'x'+n_h.to_s][(fr/2).to_s] &&
+            @capture_modes[n_w.to_s+'x'+n_h.to_s][(fr/2).to_s][inter]
+        mode =  @capture_modes[n_w.to_s+'x'+n_h.to_s][(fr/2).to_s][inter]['val']
+        num_frames = (cap_secs*fr/2).to_i
+      else
+        return -1
+      end
+      @sample_size = a_bits.to_i/8
+      @audio_channels = a_chans.to_i
+      @sys.send_cmd("#{@capture_bin} -v #{v_name} -d 0 -m #{mode} -p #{pixel} -a #{a_name} -c #{a_chans} -n #{num_frames} -s #{a_bits} -x 1", @sys.prompt, 10 + (num_frames*n_w*n_h).to_f/15552000) 
+      raise "Frame drop detected" if @sys.response.match(/Frame\s*received\s*\(#\d+\)\s*-\s*No input signal detected/)
+      n_c
+    end
+
+    def is_capture_mode_supported(width, height, framerate, interlace)
+      
+      fr = framerate.to_i
+      n_h = height.to_i == 480 ? 486 : height.to_i
+      n_w = width.to_i
+      inter = interlace.downcase()
+      
+      @capture_modes[n_w.to_s+'x'+n_h.to_s] &&
+      ((inter == 'p' && @capture_modes[n_w.to_s+'x'+n_h.to_s][fr.to_s] &&
+         @capture_modes[n_w.to_s+'x'+n_h.to_s][fr.to_s][inter]) ||
+      (inter == 'i' && @capture_modes[n_w.to_s+'x'+n_h.to_s][(fr/2).to_s] &&
+            @capture_modes[n_w.to_s+'x'+n_h.to_s][(fr/2).to_s][inter]))
+    end
+
+    def get_recorded_sample_size()
+      @sample_size
+    end
+    
+    def get_audio_sampling_rate()
+      48000
+    end
+    
+    def get_recorded_audio_channels()
+      @audio_channels
+    end
+    
+    def get_edid()
+      EDIDS[@model]
+    end
+    
+  end
+end
---
+++ LSP/A-Display/drm/drm_auto.rb
@@ -0,0 +1,232 @@
+# -*- coding: ISO-8859-1 -*-
+require File.dirname(__FILE__)+'/drm_utils'
+require File.dirname(__FILE__)+'/capture_utils'
+require File.dirname(__FILE__)+'/../../A-Audio/audio_utils'
+require File.dirname(__FILE__)+'/../../default_target_test'
+
+include LspTargetTestScript
+include CaptureUtils
+
+=begin
+ Test to validate HDMI displays, requires:
+   - HDMI frame grabber, for example
+     https://www.blackmagicdesign.com/products/intensitypro4k
+   - HDMI switch, for example
+     http://www.kramerelectronics.com/products/model.asp?pid=534
+   - Network connectivity on the host and dut.
+   - Adding "hdmiqual" capability the board's bench and 
+     staf registration command.
+=end
+
+def setup
+  super
+  @equipment['dut1'].send_cmd('',@equipment['dut1'].prompt) #making sure that the board is ok
+  @equipment['dut1'].send_cmd('ps -ef | grep -i weston | grep -v grep && /etc/init.d/weston stop && sleep 3',@equipment['dut1'].prompt,10)
+  @equipment['server1'].send_cmd("mkdir #{@linux_temp_folder}") if !File.exists?(@linux_temp_folder) #make sure the data folder exists 
+  @equipment['server1'].send_cmd("mkdir #{SiteInfo::UTILS_FOLDER}") if !File.exists?(SiteInfo::UTILS_FOLDER) #make sure the bins folder exists
+  @equipment['dut1'].send_cmd("ls #{@linux_dst_dir} || mkdir #{@linux_dst_dir}",@equipment['dut1'].prompt) 
+  @equipment['dut1'].send_cmd("rm #{@linux_dst_dir}/*",@equipment['dut1'].prompt) 
+end
+
+def run
+  num_passed = 0
+  total = 0
+  dut_audio = File.join(@linux_dst_dir, 'audio_src_file.wav')
+  ref_file = @test_params.params_chan.audio_file_url[0]
+  audio_server = ref_file.match(/tp:\/\/([^\/]+)/)[1].sub(/.*?@/,'')
+  @equipment['dut1'].send_cmd("wget #{ref_file} -O #{dut_audio}", @equipment['dut1'].prompt, 200)
+  hdmi_adev_info = get_audio_play_dev(nil, 'hdmi')
+  aic_adev_info = get_audio_play_dev(nil, 'aic')
+  hdmi_out_info = @equipment['dut1'].video_io_info.hdmi_outputs.keys[0]
+  hdmi_in_info = @equipment['server1'].video_io_info.hdmi_inputs.keys[0]
+  raise "Trying to connect io from different swithes" if hdmi_out_info != hdmi_in_info
+  begin
+    staf_handle = STAFHandle.new("#{@staf_service_name.to_s}_audio_handle")
+  rescue Exception => e
+    staf_handle = nil
+  end
+
+  @results_html_file.add_paragraph("")
+  res_table = @results_html_file.add_table([["Connector",{:bgcolor => "4863A0"}], 
+                                            ["Encoder", {:bgcolor => "4863A0"}],
+                                            ["CRTC", {:bgcolor => "4863A0"}],
+                                            ["Mode", {:bgcolor => "4863A0"}],
+                                            ["Plane", {:bgcolor => "4863A0"}],
+                                            ["Audio Card", {:bgcolor => "4863A0"}],
+                                            ["Audio Dev", {:bgcolor => "4863A0"}],
+                                            ["Result", {:bgcolor => "4863A0"}],
+                                            ["Comment", {:bgcolor => "4863A0"}]])
+
+  drm_info = ''
+  formats = Hash.new(['default'])
+  staf_mutex('video_capture', 36000000) do
+    add_equipment('hdmi_sw', hdmi_out_info, true) do |e_class, log_path|
+      e_class.new(hdmi_out_info, log_path)
+    end
+    @equipment['hdmi_sw'].disconnect_video_audio()
+    sleep 1
+    @equipment['hdmi_sw'].connect_video_audio(@equipment['dut1'].video_io_info.hdmi_outputs.values[0], @equipment['server1'].video_io_info.hdmi_inputs.values[0])
+    sleep 5
+    drm_info = get_properties()
+    
+    if @test_params.params_chan.instance_variable_defined?(:@formats)
+      formats = Hash.new() { |h,k| h[k] = @test_params.params_chan.formats }
+    elsif @test_params.params_chan.instance_variable_defined?(:@valid_formats)
+      formats = get_supported_fmts(drm_info['Connectors:'])
+    end
+  end
+
+  single_disp_modes, multi_disp_modes = get_test_modes(drm_info, formats, 'hdmi')
+  perf_data = []
+
+  video_test_file = File.join(@linux_temp_folder, 'video_tst_file.raw')
+  audio_test_file = File.join(@linux_temp_folder, 'audio_tst_file.pcm')
+
+  single_disp_modes.each do |s_mode|
+    staf_mutex('video_capture', 36000000) do
+
+      perf_data = []
+      res_string = ''
+      width, height = s_mode[0]['mode'].split('x')
+      interlace = height.include?('i') ? 'i' : 'p'
+      height = height.gsub('i','').to_i
+
+      add_equipment('hdmi_sw', hdmi_out_info, true) do |e_class, log_path|
+        e_class.new(hdmi_out_info, log_path)
+      end
+
+      video_capture = MediaCapture.new(@equipment['server1'])
+      @equipment['hdmi_sw'].disconnect_video_audio()
+      sleep 1
+      @equipment['hdmi_sw'].connect_video_audio(@equipment['dut1'].video_io_info.hdmi_outputs.values[0], @equipment['server1'].video_io_info.hdmi_inputs.values[0])
+      sleep 5
+
+      drm_info = get_properties()
+      hdmi_info = drm_info['Connectors:'].select{ |x| x['name'].match(/HDMI/) }
+
+      edid_test = false
+      edid_arr = []
+      sw_edid = video_capture.get_edid()
+      
+      hdmi_info.each do |h_inf|
+        edid_arr << h_inf['props:']['1 EDID:']['value:'].gsub(/[^\da-fA-f]+/,'')
+        edid_test |= edid_arr[-1].downcase == sw_edid.downcase
+      end
+      if !edid_test
+        total += 1
+        add_result_row(res_table, s_mode, hdmi_adev_info, false, "EDID detection failed capture card EDID #{sw_edid} not in modetest EDID(s) #{edid_arr.to_s}", '')
+        next
+      end
+
+      if !video_capture.is_capture_mode_supported(width, height, s_mode[0]['framerate'], interlace)
+        add_result_row(res_table, s_mode, hdmi_adev_info, -1, "Mode not supported by test equipment", '')
+        puts "Mode #{s_mode[0]} not supported by test equipment..."
+        next
+      end
+      plane_info_str = "Scale: #{s_mode[0]['plane']['scale']}, pix_fmt: #{s_mode[0]['plane']['format']}" if s_mode[0]['plane']
+      res = true
+      res_string = ''
+      num_comp = -1
+      fps_res, fps_data = run_perf_sync_flip_test(s_mode) do
+        begin
+          sleep 5
+          @equipment['dut1'].send_cmd("aplay -D hw:#{hdmi_adev_info['card']},#{hdmi_adev_info['device']} -d 12 #{dut_audio} &",@equipment['dut1'].prompt)
+          sleep 1
+          num_comp = video_capture.capture_media(video_test_file, audio_test_file, width, height, s_mode[0]['framerate'], interlace)
+        rescue Exception => e
+          res_string += "\n#{e.to_s}"
+        end
+      end
+      conv_file = video_test_file
+      if num_comp == 3
+        conv_file = change_uyvy_to_rgb(video_test_file, width, height) 
+      end
+
+      video_ref_files = get_ref_file(s_mode[0]['mode'], s_mode[0]['format'], s_mode[0]['plane'])
+      if video_ref_files.empty?
+        total += 1
+        add_result_row(res_table, s_mode, hdmi_adev_info, false, "Unable to get ref file for mode", '')
+        next
+      end
+      
+      qual_string= 'Could not fetch reference file(s)'
+      qual_res = false
+      result = []
+      video_ref_files.each do |v_ref|
+        qual_string= ''
+        qual_res = true 
+        result = get_psnr_ssim_argb(v_ref, conv_file, width, height, num_comp)
+        if result.empty?
+          qual_res = false
+          qual_string = "-HDMI test failed, could not asses video quality\n"
+          break
+        end
+        result.each_with_index do |h, i|
+          h['psnr'].each {|comp, val| qual_res &= val >= 40
+                                      qual_string+="-Frame ##{i}: Component #{comp} failed PSNR #{val}dB\n" if val < 40}
+          h['ssim'].each {|comp, val| qual_res &= val >= 99
+                                      qual_string+="-Frame ##{i}: Component #{comp} failed SSIM #{val}%\n" if val < 99}
+        end
+        break if qual_res
+      end
+      res &= qual_res
+      res_string += qual_string
+      if res
+        res_string += "-Video Result: #{result.length} video frames passed\n"
+      end
+      
+      #exit if !res #Uncomment this line if you want to stop on a display failure
+      
+      if @test_params.params_chan.instance_variable_defined?(:@performance)
+        metric_name = "#{s_mode[0]['type']}-#{s_mode[0]['mode']}@#{s_mode[0]['framerate']}-" \
+                  "#{s_mode[0]['format']}"
+        perf_data << {'name' => metric_name,
+                      'units' => 'fps',
+                      'values' => fps_data}
+      end
+
+      processed_audio = audio_test_file + '.processed'
+      remove_offset(audio_test_file, processed_audio,
+                    video_capture.get_recorded_sample_size(),
+                    video_capture.get_audio_sampling_rate(),
+                    video_capture.get_recorded_audio_channels())
+      audio_res = false
+      if staf_handle
+        audio_name = File.basename(ref_file,'.wav')
+        staf_req = staf_handle.submit(audio_server, "DEJAVU","MATCH FILE #{processed_audio}")
+        staf_result = STAFResult.unmarshall_response(staf_req.result)
+        if staf_req.rc == 0 && staf_result['song_name'] == audio_name
+          audio_res = true
+          res_string += "\n-Audio result: passed\n"
+        else
+          res_string += "\n-Audio result: failed (expected #{audio_name} got #{staf_result['song_name']})\n"
+        end
+      else
+        audio_res = false
+        res_string += "Unable verify recorded audio #{e.to_s}" 
+      end
+      num_passed += 1 if res && audio_res
+      total += 1
+      add_result_row(res_table, s_mode, hdmi_adev_info, res && audio_res, res_string, plane_info_str)
+    end
+  end
+
+  set_result(num_passed != total || total == 0 ? FrameworkConstants::Result[:fail] : FrameworkConstants::Result[:pass],
+             "#{num_passed}/#{total} passed ", perf_data)
+
+end
+
+def add_result_row(res_table, s_mode, hdmi_adev_info, res, res_string, plane_info_str)
+  @results_html_file.add_rows_to_table(res_table,[["#{s_mode[0]['connectors_names'][0]} (#{s_mode[0]['connectors_ids'][0]})", 
+                                           s_mode[0]['encoder'],
+                                           s_mode[0]['crtc_id'],
+                                           "#{ s_mode[0]['mode']}@#{ s_mode[0]['framerate']}",
+                                           s_mode[0]['plane'] ? plane_info_str : 'No plane',
+                                           hdmi_adev_info['card_info'],
+                                           hdmi_adev_info['device_info'],
+                                           res == -1 ? ["Not testable",{:bgcolor => "yellow"}] : 
+                                           res ? ["Passed",{:bgcolor => "green"}] : 
+                                           ["Failed",{:bgcolor => "red"}],
+                                           res_string]])
+end
+
