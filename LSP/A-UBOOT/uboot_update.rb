b3cc6b26fa3824be963508eb0319cfd91af3644b
Yan Liu <yan-liu@ti.com>
2017-02-22 21:50:08 -0500

    update_uboot.rb: Add support to boot from different media before flashing
    Since the board might not be able to boot to uboot from the boot media
    which will be flashed, a different boot method needs to be used like
    UART to boot to uboot prompt first; then flash the boot media like SPI.
    
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- LSP/A-UBOOT/uboot_update.rb
+++ LSP/A-UBOOT/uboot_update.rb
@@ -12,49 +12,65 @@ end
 
 def run
   result = 0
-  translated_boot_params = setup_host_side()
-  translated_boot_params.each{|k,v| puts "#{k}:#{v}"}
+  boot_params = setup_host_side()
+  boot_params.each{|k,v| puts "#{k}:#{v}"}
 
   flash_bootloader = @test_params.params_chan.instance_variable_defined?(:@flash_bootloader) ? @test_params.params_chan.flash_bootloader[0].downcase :
                      @test_params.instance_variable_defined?(:@var_flash_bootloader) ? @test_params.var_flash_bootloader :  'no'
   flash_kernel = @test_params.params_chan.instance_variable_defined?(:@flash_kernel) ? @test_params.params_chan.flash_kernel[0].downcase : 'no'
   flash_fs = @test_params.params_chan.instance_variable_defined?(:@flash_fs) ? @test_params.params_chan.flash_fs[0].downcase : 'no'
   verify_fs_ok = @test_params.instance_variable_defined?(:@var_verify_fs_ok) ? @test_params.var_verify_fs_ok.downcase : 'no'
+  pre_boot = @test_params.instance_variable_defined?(:@var_pre_boot) ? @test_params.var_pre_boot.downcase : 'no'
 
   if flash_bootloader == 'yes'
-    translated_boot_params['dut'].update_bootloader(translated_boot_params)
+    this_boot_params = boot_params
+    if pre_boot != 'no'
+      this_boot_params = set_pre_params(boot_params)
+      puts "=============preboot params============="
+      this_boot_params.each{|k,v| puts "#{k}:#{v}"}
+    end
 
+    this_boot_params['dut'].set_bootloader(this_boot_params) 
+    this_boot_params['dut'].boot_loader.run(this_boot_params)
+
+    puts "=============boot params============="
+    boot_params.each{|k,v| puts "#{k}:#{v}"}
+
+    boot_params['dut'].set_systemloader(boot_params.merge({'systemloader_class' => SystemLoader::UbootFlashBootloaderSystemLoader}))
+    boot_params['dut'].system_loader.run(boot_params)
+  
     # Verify if the board can boot using the updated bootloader
-    10.times {puts "Please change the switch setting to boot from #{translated_boot_params['primary_bootloader_dev']}!!!"}
+    10.times {puts "Please change the switch setting to boot from #{boot_params['primary_bootloader_dev']}!!!"}
     sleep 10
     # powercycle or reset the board to check
-    translated_boot_params['dut'].boot_to_bootloader(translated_boot_params)
-    translated_boot_params['dut'].send_cmd("env default -a -f", translated_boot_params['dut'].boot_prompt, 10)
-    translated_boot_params['dut'].send_cmd("saveenv", translated_boot_params['dut'].boot_prompt, 10)
-    translated_boot_params['dut'].send_cmd("version", translated_boot_params['dut'].boot_prompt, 10)
-    result += 1 if translated_boot_params['dut'].timeout? 
+    boot_params['dut'].boot_loader = nil
+    boot_params['dut'].boot_to_bootloader(boot_params)
+    boot_params['dut'].send_cmd("env default -a -f", boot_params['dut'].boot_prompt, 10)
+    boot_params['dut'].send_cmd("saveenv", boot_params['dut'].boot_prompt, 10)
+    boot_params['dut'].send_cmd("version", boot_params['dut'].boot_prompt, 10)
+    result += 1 if boot_params['dut'].timeout? 
   end 
   
   if flash_kernel == 'yes'
-    translated_boot_params['dut'].system_loader = nil
-    translated_boot_params['dut'].update_kernel(translated_boot_params)
+    boot_params['dut'].system_loader = nil
+    boot_params['dut'].update_kernel(boot_params)
   end
 
   if flash_fs == 'yes'
-    translated_boot_params['dut'].system_loader = nil
-    translated_boot_params['dut'].update_fs(translated_boot_params)
+    boot_params['dut'].system_loader = nil
+    boot_params['dut'].update_fs(boot_params)
   end
   
   # check if dut bootup ok using updated kernel or fs
   if verify_fs_ok == 'yes'
     if flash_kernel == 'yes' or flash_fs == 'yes'
-      translated_boot_params['fs_dev'] = ''  # to prevent fs got flashed again when calling 'boot'
-      translated_boot_params['dut'].system_loader = nil
-      translated_boot_params['dut'].boot(translated_boot_params)
+      boot_params['fs_dev'] = ''  # to prevent fs got flashed again when calling 'boot'
+      boot_params['dut'].system_loader = nil
+      boot_params['dut'].boot(boot_params)
 
       # check if the kernel boot up ok.
-      translated_boot_params['dut'].send_cmd("uname -a", translated_boot_params['dut'].prompt, 10)
-      result += 1 if translated_boot_params['dut'].timeout?
+      boot_params['dut'].send_cmd("uname -a", boot_params['dut'].prompt, 10)
+      result += 1 if boot_params['dut'].timeout?
     end
   end
 
@@ -65,3 +81,39 @@ def run
   end  
 
 end 
+
+def set_pre_params(params)
+    new_params = params.clone
+    new_params['primary_bootloader'] = new_params['pre_primary_bootloader'] ? new_params['pre_primary_bootloader'] :
+                             @test_params.instance_variable_defined?(:@pre_primary_bootloader) ? @test_params.pre_primary_bootloader :
+                             ''
+    new_params['secondary_bootloader'] = new_params['pre_secondary_bootloader'] ? new_params['pre_secondary_bootloader'] :
+                             @test_params.instance_variable_defined?(:@pre_secondary_bootloader) ? @test_params.pre_secondary_bootloader :
+                             ''
+    new_params['primary_bootloader_dev']   = new_params['pre_primary_bootloader_dev'] ? new_params['pre_primary_bootloader_dev'] :
+                             @test_params.params_chan.instance_variable_defined?(:@pre_primary_bootloader_dev) ? @test_params.params_chan.pre_primary_bootloader_dev[0] :
+                             @test_params.instance_variable_defined?(:@var_pre_primary_bootloader_dev) ? @test_params.var_pre_primary_bootloader_dev : "mmc"
+    new_params['secondary_bootloader_dev']   = new_params['pre_secondary_bootloader_dev'] ? new_params['pre_secondary_bootloader_dev'] :
+                             @test_params.params_chan.instance_variable_defined?(:@pre_secondary_bootloader_dev) ? @test_params.params_chan.pre_secondary_bootloader_dev[0] :
+                             @test_params.instance_variable_defined?(:@var_pre_secondary_bootloader_dev) ? @test_params.var_pre_secondary_bootloader_dev : "mmc"
+    new_params['primary_bootloader_src_dev']   = new_params['pre_primary_bootloader_src_dev'] ? new_params['pre_primary_bootloader_src_dev'] :
+                             @test_params.params_chan.instance_variable_defined?(:@pre_primary_bootloader_src_dev) ? @test_params.params_chan.pre_primary_bootloader_src_dev[0] :
+                             @test_params.instance_variable_defined?(:@var_pre_primary_bootloader_src_dev) ? @test_params.var_pre_primary_bootloader_src_dev :
+                             new_params['primary_bootloader'] != '' ? 'eth' : 'none'
+
+    new_params['secondary_bootloader_src_dev']   = new_params['pre_secondary_bootloader_src_dev'] ? new_params['pre_secondary_bootloader_src_dev'] :
+                             @test_params.params_chan.instance_variable_defined?(:@pre_secondary_bootloader_src_dev) ? @test_params.params_chan.pre_secondary_bootloader_src_dev[0] :
+                             @test_params.instance_variable_defined?(:@var_pre_secondary_bootloader_src_dev) ? @test_params.var_pre_secondary_bootloader_src_dev :
+                             new_params['secondary_bootloader'] != '' ? 'eth' : 'none'
+
+    new_params['primary_bootloader_image_name'] = new_params['pre_primary_bootloader_image_name'] ? new_params['pre_primary_bootloader_image_name'] :
+                             @test_params.instance_variable_defined?(:@var_pre_primary_bootloader_image_name) ? @test_params.var_pre_primary_bootloader_image_name :
+                             new_params['primary_bootloader'] != '' ? File.basename(new_params['primary_bootloader']) : 'MLO'
+
+    new_params['secondary_bootloader_image_name'] = new_params['pre_secondary_bootloader_image_name'] ? new_params['pre_secondary_bootloader_image_name'] :
+                             @test_params.instance_variable_defined?(:@var_pre_secondary_bootloader_image_name) ? @test_params.var_pre_secondary_bootloader_image_name :
+                             new_params['secondary_bootloader'] != '' ? File.basename(new_params['secondary_bootloader']) : 'u-boot.img'
+
+
+    new_params
+end
