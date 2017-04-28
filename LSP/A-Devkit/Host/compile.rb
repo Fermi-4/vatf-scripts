73d742ba469800e50ee8607b3ec954fa70530c90
Carlos Hernandez <ceh@ti.com>
2017-04-28 17:12:34 -0400

    Add Host-Side Devkit tests
    Validate:
     * toolchain download & installation
     * devkit download & installation
     * c code compilation using devkit
     * c++ code compilation using devkit
     * c code compilation and link to lpthread using devkit
     * gstreamer compilation using devkit
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

---
+++ LSP/A-Devkit/Host/compile.rb
@@ -0,0 +1,48 @@
+require File.dirname(__FILE__)+'/../../default_test_module'
+include LspTestScript
+
+def setup
+  puts "No need to boot DUT"
+end
+
+def run_and_check_cmd(cmd, equip, response, timeout=60)
+  begin
+    if equip.send_cmd(cmd, response, timeout)
+      set_result(FrameworkConstants::Result[:fail], "Error executing #{cmd}")
+      return true
+    end
+    return false
+  rescue Exception => e
+    puts e.message
+    puts e.backtrace
+    set_result(FrameworkConstants::Result[:fail], "Exception trying to execute #{cmd}")
+    return true
+  end
+end
+
+def run
+  equip = @equipment['server1']
+  toolchains_url = 'http://lcpd.gt.design.ti.com/toolchains'
+  arch = @test_params.params_control.arch[0]
+  download_cmd =  "cd ~ && wget -A xz -m -p -E -k -K -np #{toolchains_url}/#{arch}/ && echo 'DOWNLOADPASSED'"
+  return if run_and_check_cmd(download_cmd, equip, /DOWNLOADPASSED/)
+  toolchain_filename = Dir.entries("#{toolchains_url.sub('http://','')}/#{arch}")[-1]
+  toolchain_dirname = File.basename(toolchain_filename, ".tar.xz")
+  if equip.send_sudo_cmd("tar xvf ~/#{toolchains_url.sub('http://','')}/#{arch}/#{toolchain_filename} -C /opt && echo 'INSTALLATIONPASSED'", /INSTALLATIONPASSED/, 120)
+    set_result(FrameworkConstants::Result[:fail], "Error trying to install toolchain")
+    return
+  end
+  compile_test = File.join(File.dirname(__FILE__), 'compile.sh')
+  toolchain_path = "/opt/#{toolchain_dirname}/bin"
+  devkit_url =  @test_params.var_devkit_url
+  c_app_path = File.join(File.dirname(__FILE__), '..', 'hello.c')
+  cpp_app_path = File.join(File.dirname(__FILE__), '..', 'hello.cpp')
+  pthread_app_path = File.join(File.dirname(__FILE__), '..', 'thread-ex.c')
+
+  test_cmd ="#{compile_test} #{toolchain_path} #{devkit_url} " \
+            "#{File.join(File.dirname(__FILE__), '..', 'hello.c')} " \
+            "#{File.join(File.dirname(__FILE__), '..', 'hello.cpp')} " \
+            "#{File.join(File.dirname(__FILE__), '..', 'thread-ex.c')}"
+  return if run_and_check_cmd(test_cmd, equip, /All host-side devkit checks passed/, 900)
+  set_result(FrameworkConstants::Result[:pass], "Host-side devkit passed all tests")
+end
---
+++ LSP/A-Devkit/Host/compile.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -x
+
+toolchain_path=$1
+devkit_path=$2
+c_app_path=$3
+cpp_app_path=$4
+pthread_app_path=$5
+
+# Cleanup temp directory
+function finish {
+	if [ -n $tmpdir ]; then
+		rm -rf $tmpdir
+	fi
+}
+trap finish EXIT
+
+export PATH=$toolchain_path:$PATH
+tmpdir=$(mktemp -d)
+
+# Install devkit
+cd $tmpdir
+wget --no-proxy $devkit_path || exit 1
+devkit=$(ls)
+chmod +x $devkit
+yes Y | ./$devkit
+cd Y
+source ./environment-setup || exit 1
+
+# Test c compilation
+$CC $c_app_path -o hello || exit 1
+file hello | grep ARM || exit 1
+
+# Test c++ compilation
+$CXX $cpp_app_path -o hello || exit 1
+file hello | grep ARM || exit 1
+
+# Test compilation with pthread library
+$CC $pthread_app_path -o thread-ex -lpthread || exit 1
+file thread-ex | grep ARM || exit 1
+
+# Test gstreamer compilation
+wget --no-check-certificate https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-1.2.0.tar.xz || exit 1
+tar xf gst-plugins-good-1.2.0.tar.xz  || exit 1
+cd gst-plugins-good-1.2.0/ || exit 1
+./configure --host=i686 --disable-deinterlace --disable-goom  || exit 1
+make  || exit 1
+
+echo "All host-side devkit checks passed"
+exit 0
