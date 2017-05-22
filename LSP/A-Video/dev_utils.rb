d4ac0aacbaaf6adb957445998196f60237eb2668
Alejandro Hernandez <ajhernandez@ti.com>
2017-05-22 15:55:08 -0400

    A-Video: Wait for prompt when listing devices
    This patch changes the expected pattern when listing the
    type of devices in method get_type_devices. Before this
    patch the method used the default pattern /.*/ (match
    everything) which in some instances returned too soon
    and caused the test to fail
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>

--- LSP/A-Video/dev_utils.rb
+++ LSP/A-Video/dev_utils.rb
@@ -1,11 +1,11 @@
 
 #Function to fetch the test file in the dut and host
 def get_type_devices(ip_type)
-  @equipment['dut1'].send_cmd("ls /sys/class/video4linux/")
+  @equipment['dut1'].send_cmd("ls /sys/class/video4linux/",@equipment['dut1'].prompt)
   video_devs = @equipment['dut1'].response.scan(/video\d+\s+/im)
   result = []
   video_devs.each do |dev|
-    @equipment['dut1'].send_cmd("cat /sys/class/video4linux/#{dev.strip()}/name")
+    @equipment['dut1'].send_cmd("cat /sys/class/video4linux/#{dev.strip()}/name",@equipment['dut1'].prompt)
     result << dev.strip if @equipment['dut1'].response.downcase.include?(ip_type.downcase)
   end
   return result.empty? ? nil : result
