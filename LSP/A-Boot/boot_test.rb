2fd83ca86940ce9c8b92a4812630c187dfe1230a
Yan Liu <yan-liu@ti.com>
2017-11-14 13:41:31 -0500

    boot_test.rb: Added dhcp testing
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- LSP/A-Boot/boot_test.rb
+++ LSP/A-Boot/boot_test.rb
@@ -9,7 +9,10 @@ end
 def run
   counter = 0
   result = 0
+  result_dhcp = 0
+  msg = ""
   loop_count = @test_params.params_control.loop_count[0].to_i
+  test_dhcp = @test_params.params_chan.instance_variable_defined?(:@test_dhcp) ? @test_params.params_chan.test_dhcp[0].downcase : "no"
   translated_boot_params = setup_host_side()
   @equipment['dut1'].set_bootloader(translated_boot_params) if !@equipment['dut1'].boot_loader
   @equipment['dut1'].set_systemloader(translated_boot_params) if !@equipment['dut1'].system_loader
@@ -22,18 +25,26 @@ def run
     if @equipment['dut1'].timeout?
       result += 1
       #break
-    #else
-    #  result = 0
+    else
+      # test uboot
+      if test_dhcp != 'no'
+        @equipment['dut1'].send_cmd('setenv autoload no', @equipment['dut1'].boot_prompt, 3)
+        @equipment['dut1'].send_cmd('dhcp', /DHCP client bound to address.*#{@equipment['dut1'].boot_prompt}/im, 60)
+        if @equipment['dut1'].timeout?
+          result_dhcp += 1
+        end
+      end
     end
     counter += 1
     @equipment['dut1'].disconnect('serial') if @equipment['dut1'].target.serial
   end
 
-  if result == 0 
+  if result == 0 and result_dhcp == 0 
     set_result(FrameworkConstants::Result[:pass], "Boot test passed.")
   else
-    #set_result(FrameworkConstants::Result[:fail], "No uboot response on iteration #{counter}, boot test failed!")
-    set_result(FrameworkConstants::Result[:fail], "No uboot response for #{result} times out of #{loop_count}, boot test failed!")
+    msg = "Boot failed to boot to uboot prompt #{result.to_s} times out of #{loop_count.to_s}; " if result != 0
+    msg = msg + "DHCP failed #{result_dhcp.to_s} times out of #{loop_count.to_s}; " if result_dhcp != 0
+    set_result(FrameworkConstants::Result[:fail], "boot test failed! "+msg)
   end
 end
 
