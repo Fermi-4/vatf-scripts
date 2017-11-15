Fermi-4/vatf-scripts

commit 5e1d33c31c892580e2d132324cdf242397a435cb

LSP/A-Power/power_s2ram_func.rb: Handle test thread hangs
Kill test thread after timeout is reached to avoid test cases that
hang forever.

Signed-off-by: Carlos Hernandez <ceh@ti.com>

LSP/A-Power/power_s2ram_func.rb
@@ -36,11 +36,12 @@ def run
   @global_stop = true
   @stop_test = true
   puts "\n===> waiting on suspend_thr"
-  suspend_thr.join
+  suspend_thr.kill
   puts "\n===> waiting on test_thr"
+  test_thr.kill
   result = test_thr.value           # This will block (join) until test_thr completes
   puts "\n===> all threads finished"
-  set_result(result[0], result[1]) 
+  result ? set_result(result[0], result[1]) : set_result(FrameworkConstants::Result[:fail], "Test Thread timed out")
   query_pm_stats
   show_execution_logs
 end

commit 89c32df59b2f868c6c3b100fdf653c5f12fb33c0

Fix regression introduced by 5e1d33c31c
Killing test thread causes its value to always be blank.
There is already protection against commands not returning inside the
test thread, so it is not required to kill it. Instead wait for it
to gracefully finished and then collect return values.

Signed-off-by: Carlos Hernandez <ceh@ti.com>

LSP/A-Power/power_s2ram_func.rb
@@ -38,7 +38,6 @@ def run
   puts "\n===> waiting on suspend_thr"
   suspend_thr.kill
   puts "\n===> waiting on test_thr"
-  test_thr.kill
   result = test_thr.value           # This will block (join) until test_thr completes
   puts "\n===> all threads finished"
   result ? set_result(result[0], result[1]) : set_result(FrameworkConstants::Result[:fail], "Test Thread timed out")