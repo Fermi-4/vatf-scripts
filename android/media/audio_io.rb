8e2c4529208a5b7332c667d2d8441b847b99c62f
Alejandro Hernandez <ajhernandez@ti.com>
2018-01-29 14:44:51 -0500

    Android: Add audio play+record test
    Add script to validate audio play and record test. The
    test requires a loopback cable between ouput and input
    and determines pass/fail if the recorded file can be
    recognized by an audio fingerprinting app
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>

---
+++ android/media/audio_io.rb
@@ -0,0 +1,52 @@
+require File.dirname(__FILE__)+'/../android_test_module'
+require File.dirname(__FILE__)+'/f2f_utils'
+  
+include AndroidTest
+
+def run
+  audio_permissions = ["android.permission.RECORD_AUDIO",
+                       "android.permission.READ_EXTERNAL_STORAGE",
+                       "android.permission.WRITE_EXTERNAL_STORAGE",
+                       "android.permission.REQUEST_INSTALL_PACKAGES"]
+  
+  ref_file_url = @test_params.params_chan.audio_url[0]
+  ref_path, dut_src_file = get_file_from_url(ref_file_url)
+  apk_path = File.join(@linux_temp_folder, File.basename(@test_params.params_chan.apk_url[0]))
+  wget_file(@test_params.params_chan.apk_url[0], apk_path)
+  send_adb_cmd("install -r #{apk_path}")
+  pkg = 'com.ti.test.media'
+  audio_permissions.each{ |permission| send_adb_cmd("shell pm grant #{pkg} #{permission}") }
+  dut_test_file = 'test.3gp'
+  #clear the old files if any
+  send_adb_cmd("shell rm /sdcard/#{dut_test_file}")
+  local_test_file = File.join(@linux_temp_folder, dut_test_file)
+  data = send_adb_cmd("logcat  -d -c")
+  send_adb_cmd("shell am start -W -n #{pkg}/.MediaIO -a android.intent.action.MAIN -c android.intent.category.LAUNCHER --es play_file #{File.basename(dut_src_file)} --es rec_file #{dut_test_file}")
+  send_events_for(['__tab__','__tab__','__enter__'])
+  if @test_params.params_control.instance_variable_defined?(:@collect_stats)
+    start_collecting_stats(@test_params.params_control.collect_stats,2){|cmd, stat| 
+      if stat == 'proc_mem'
+        send_adb_cmd("shell #{cmd} #{process_name}")
+      else
+        send_adb_cmd("shell #{cmd}")
+      end
+    }
+  end
+  sleep (@test_params.params_chan.duration[0].to_i)
+  send_events_for(['__enter__'])
+  sys_stats = stop_collecting_stats(@test_params.params_control.collect_stats) if @test_params.params_control.instance_variable_defined?(:@collect_stats)
+  send_adb_cmd("pull -p /sdcard/#{dut_test_file} #{local_test_file}")
+  audio_server = ref_file_url.match(/tp:\/\/([^\/]+)/)[1].sub(/.*?@/,'')
+  staf_handle = STAFHandle.new("#{@staf_service_name.to_s}_audio_handle")
+  local_test_file_mp3 = local_test_file.sub(/[^\.]*$/, '') + 'mp3'
+  audio_name = File.basename(ref_file_url,'.*')
+  @equipment['server1'].send_cmd("avconv -i #{local_test_file} -acodec mp3 #{local_test_file_mp3}")
+  staf_req = staf_handle.submit(audio_server, "DEJAVU","MATCH FILE #{local_test_file_mp3}") 
+  staf_result = STAFResult.unmarshall_response(staf_req.result)
+  if staf_req.rc == 0 && staf_result['song_name'] == audio_name
+     set_result(FrameworkConstants::Result[:pass], "Recorded audio matched expected audio: expected #{audio_name}, got #{staf_result['song_name']}")
+  else
+    audio = staf_req.rc == 0 ? staf_result['song_name'] : 'no match found'
+    set_result(FrameworkConstants::Result[:fail], "Recorded audio did not match expected audio: expected #{audio_name}, got #{audio}")
+  end
+end
--- android/media/f2f_utils.rb
+++ android/media/f2f_utils.rb
@@ -6,10 +6,14 @@ def get_file_from_url(url, ref_file_url=nil, dut=@equipment['dut1'], host=@equip
   r_file_name = File.basename(r_file_url)
   host_path = File.join(@linux_temp_folder, r_file_name)
   dut_path = File.join(@linux_dst_dir, file_name)
-  host.send_cmd("wget --no-proxy --tries=1 -T10 #{r_file_url} -O #{host_path}", host.prompt, 300)
-  host.send_cmd("wget #{r_file_url} -O #{host_path}", host.prompt, 300) if host.response.match(/failed/im)
-  raise "Host is unable to fetch file from #{r_file_url}" if host.response.match(/error/im)
+  wget_file(r_file_url, host_path, host)
   response = dut.send_adb_cmd("push -p #{host_path} #{dut_path}")
  	[host_path, dut_path]
+end
+
+def wget_file(url, path, sys=@equipment['server1'])
+  sys.send_cmd("wget --no-proxy --tries=1 -T10 #{url} -O #{path}", sys.prompt, 300)
+  sys.send_cmd("wget #{url} -O #{path}", sys.prompt, 300) if sys.response.match(/failed/im)
+  raise "Host is unable to fetch file from #{url}" if sys.response.match(/error/im)
 end
 
