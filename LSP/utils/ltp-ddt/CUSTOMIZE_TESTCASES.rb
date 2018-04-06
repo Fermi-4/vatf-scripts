3bc0f206170874c499bb3c8cd5733e9bdc2882c5
Yan Liu <yan-liu@ti.com>
2018-04-06 12:30:57 -0400

    CUSTOMIZE_TESTCASES.rb: Add bootargs_append for NAND test cases
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- LSP/utils/ltp-ddt/CUSTOMIZE_TESTCASES.rb
+++ LSP/utils/ltp-ddt/CUSTOMIZE_TESTCASES.rb
@@ -4,6 +4,7 @@ $PERF_SCRIPT = 'LSP/TARGET/dev_test_perf_gov.rb'
 # Append extra parameters to params_control
 $EXTRA_PARAMS_TESTS = {
   /^SPI_/      => {'extra_params' => ['bootargs_append=spi']},
+  /^NAND_/      => {'extra_params' => ['bootargs_append=nand']},
   /REALTIME_[XSLM]{1,3}_PERF/ => {'extra_params' => ['perf_metrics_file=LSP/A-Realtime/cyclic_parser.rb'] },
 }
 
