c2374a5ed2e70e589b9d2d5436f86e0b6bef40bc
Alejandro Hernandez <ajhernandez@ti.com>
2018-05-07 18:30:37 -0400

    Android: Add soft reboot option to boot test
    This patch implements the soft reboot option in the boot
    test script
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>

--- android/boot/boottime_test.rb
+++ android/boot/boottime_test.rb
@@ -7,25 +7,31 @@ include AndroidTest
 
 def run
   time1 = 0
-  boot_timeout = 120
-  boottimes = Array.new()
-  regexp2 = /init:\s*Service\s*'bootanim'\s*\(pid\s*\d+\)\s*exited\s*with\s*status\s*0/im
+  boottimes = []
   trials = @test_params.params_chan.instance_variable_defined?(:@boot_trials) ?
-           @test_params.params_chan.boot_trials[0].to_i :
-           5
+           @test_params.params_chan.boot_trials[0].to_i : 5
+  @equipment['dut1'].power_port = nil if @test_params.params_chan.instance_variable_defined?(:@soft_reboot)
+  @android_boot_params['reboot_regex'] = /.+/
   for i in (1..trials)
+   count =  0
+   boot_timeout = 120
+   @equipment['dut1'].disconnect('serial')
    @equipment['dut1'].boot_to_bootloader(@android_boot_params)
    @equipment['dut1'].set_os_bootcmd(@android_boot_params)
-   @equipment['dut1'].send_cmd('boot',/.*/)
    time1 = Time.now
-   count =  0
-   @equipment['dut1'].wait_for(regexp2,boot_timeout)
+   @equipment['dut1'].send_cmd('boot', /.*/)
+   res = ''
+   while !res.match(/^\d+$/) && boot_timeout > 0
+      sleep 1
+      res = @equipment['dut1'].send_adb_cmd('shell getprop sys.boot_completed').strip()
+      boot_timeout -= 1
+   end
    time2 = Time.now()
    if !@equipment['dut1'].timeout? && @equipment['dut1'].at_prompt?({'prompt' => @equipment['dut1'].prompt})
      boottimes << time2 - time1
      puts @equipment['dut1'].response
    else
-     set_result(FrameworkConstants::Result[:falie], "Test case failed, boot did not complete atfer #{boot_timeout} sec")
+     set_result(FrameworkConstants::Result[:fail], "Test case failed, boot did not complete atfer #{boot_timeout} sec")
      return
    end
   end
