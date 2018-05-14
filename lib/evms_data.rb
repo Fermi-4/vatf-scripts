Fermi-4/vatf-scripts

commit 0b180aced2b75f1c5c630c04f29dfb60698364fb

evms_data: Restrict regulators ON to am5/dra7
Non am5/dra7 platforms does not have to keep regulators ON during
suspend state.

Signed-off-by: Carlos Hernandez <ceh@ti.com>

lib/evms_data.rb
@@ -320,6 +320,7 @@ module EvmData
   end
 
   def get_regulators_remain_on(params=nil)
+    return [] if not params['platform'].match(/(^am5|^dra7)/)
     machines = {}
     params = get_default_params if !params
     data = get_power_domain_data(params['platform'])['power_domains']

commit dfcb57706a8d4e50a12705a62b91fc1e9f95787e

Fix bug introduced by 0b180aced2
Handle case where no parameter is passed to get_regulators_remain_on

Signed-off-by: Carlos Hernandez <ceh@ti.com>

lib/evms_data.rb
@@ -320,9 +320,9 @@ module EvmData
   end
 
   def get_regulators_remain_on(params=nil)
-    return [] if not params['platform'].match(/(^am5|^dra7)/)
     machines = {}
     params = get_default_params if !params
+    return [] if not params['platform'].match(/(^am5|^dra7)/)
     data = get_power_domain_data(params['platform'])['power_domains']
     data.select! {|name| name.match /(CORE|MPU|DSP|IVA|GPU)/}
     machines[params['platform']]  = {'0.0' => data}