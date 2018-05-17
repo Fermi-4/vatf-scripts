351fb7a688370dd53c875deea0476c380819e109
Alejandro Hernandez <ajhernandez@ti.com>
2018-05-17 18:39:04 -0400

    Android: Make screen capture test more robust
    Add a delay between the displaying the picture and
    performing the capture and save the capture images
    for the failed capture
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>

--- android/ui/screen_capture.rb
+++ android/ui/screen_capture.rb
@@ -15,6 +15,7 @@ def run
   local_sc_file = File.join(@linux_temp_folder, 'screencap.png')
   local_sysrq_file = File.join(@linux_temp_folder, 'screencap-sysrq.png')
   send_adb_cmd("shell am start -W -a android.intent.action.VIEW -d file://#{src_file} -t 'image/*'")
+  sleep 2
   send_adb_cmd("shell screencap -p #{dut_sc_file}")
   send_adb_cmd("pull #{dut_sc_file} #{local_sc_file}")
   local_ref_file = File.join(@linux_temp_folder, 'ref_sc.png')
@@ -22,15 +23,26 @@ def run
   ref_argb = File.join(@linux_temp_folder, 'ref_sc.argb')
   @equipment['server1'].send_cmd("avconv -i #{local_ref_file} -pix_fmt argb -f rawvideo #{ref_argb}", @equipment['server1'].prompt, 60)
   qual = check_file(ref_argb, local_sc_file)
+  if qual != '' && File.exists?(local_sc_file)
+    @equipment['server1'].send_cmd("tar -Jcvf #{local_sc_file}.tar.xz #{local_sc_file}")
+    failed_sc_info = upload_file("#{local_sc_file}.tar.xz")
+    @results_html_file.add_paragraph("failed_screencap.argb.tar.xz",nil,nil,failed_sc_info[1]) if failed_sc_info
+  end
   pre_sysrq = send_adb_cmd("shell ls /sdcard/Pictures/Screenshots").split(/\s+/)
   send_events_for("__sysrq__")
   sleep 5
+  send_adb_cmd("shell sync")
   post_syrq = send_adb_cmd("shell ls /sdcard/Pictures/Screenshots").split(/\s+/)
   if (post_syrq - pre_sysrq).empty?
     sysrq_qual = "No sysrq capture file detected"
   else
     send_adb_cmd("pull /sdcard/Pictures/Screenshots/#{(post_syrq - pre_sysrq)[0]} #{local_sysrq_file}")
     sysrq_qual = check_file(ref_argb, local_sysrq_file)
+    if sysrq_qual != '' && File.exists?(local_sysrq_file)
+      @equipment['server1'].send_cmd("tar -Jcvf #{local_sysrq_file}.tar.xz #{local_sysrq_file}")
+      failed_sc_info = upload_file("#{local_sysrq_file}.tar.xz")
+      @results_html_file.add_paragraph("failed_sysrqcap.argb.tar.xz",nil,nil,failed_sc_info[1]) if failed_sc_info
+    end
   end
   if qual+sysrq_qual == ''
     set_result(FrameworkConstants::Result[:pass], 'Screen captures passed')
@@ -41,7 +53,7 @@ end
 
 def check_file(ref_argb, test_file)
   test_argb = File.join(@linux_temp_folder, 'test.argb')
-  @equipment['server1'].send_cmd("avconv -i #{test_file} -pix_fmt argb -f rawvideo #{test_argb}", @equipment['server1'].prompt, 10)
+  @equipment['server1'].send_cmd("avconv -i #{test_file} -pix_fmt argb -f rawvideo #{test_argb}", @equipment['server1'].prompt, 20)
   @equipment['server1'].send_cmd("file #{test_file}", @equipment['server1'].prompt, 10)
   width, height = @equipment['server1'].response.match(/,\s*(\d+)\s*x\s*(\d+)\s*,/).captures
   result = get_psnr_ssim_argb(ref_argb, test_argb, width, height, 4)
