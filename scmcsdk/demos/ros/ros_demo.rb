080ac9b6381286ccc8cf8e58edc9ac73669e5418
Mithila Deshmukh <x0273351@ti.com>
2018-08-22 06:29:24 -0400

    demos: Automate testcase for ROS
    PTEST-911:
     ros_demo.rb: Updated ros_demo.rb script to run ros demo
     on evm as well as Ubuntu linuxbox.
    
     Signed-off-by: Mithila Deshmukh <x0273351@ti.com>
     ---
     Sample Log Path:
     AM572X
     ROS_Demo_Listener_Evm
     http://gtsystest.telogy.design.ti.com/vatf/udgtsystest-e1/udgtsystest-e1_vatf@am57xx-evm/udgtsystest-e108_22_2018_05_41_28/files/session_iteration_1/test_4085840/ros_demo_1.html
    
     ROS_Demo_Listener_Ubuntu_Linuxbox
     http://gtsystest.telogy.design.ti.com/vatf/udgtsystest-e1/udgtsystest-e1_vatf@am57xx-evm/udgtsystest-e108_22_2018_05_19_07/files/session_iteration_1/test_4085840/ros_demo_1.html
    
    Signed-off-by: Elvita Lobo <elvita@ti.com>

--- scmcsdk/demos/ros/ros_demo.rb
+++ scmcsdk/demos/ros/ros_demo.rb
@@ -8,13 +8,13 @@ end
 
 def run
   pass_crit = @test_params.params_chan.pass_crit[0]
+  listener = @test_params.params_chan.listener[0]
   # get Server ip address
   server_ip = @equipment['server1'].telnet_ip
  begin
     dut_ip = get_dut_ip()  # get EVM ip address
-    configure_server(dut_ip, server_ip)
     configure_dut(dut_ip, server_ip)
-    run_ros(pass_crit)
+    run_ros(pass_crit, listener, dut_ip, server_ip)
     set_result(FrameworkConstants::Result[:pass], "Test Passed. Robotic Operating System Demo Passed.")
   rescue Exception => e
     set_result(FrameworkConstants::Result[:fail], "Test Failed. #{e}")
@@ -46,30 +46,70 @@ python3-config python-config.python;ls -l *python*", @equipment['dut1'].prompt, 
 end
 
 # function to run ros demo
-def run_ros(pass_crit)
+def run_ros(pass_crit, listener, dut_ip, server_ip)
   @equipment['dut1'].send_cmd("cd /opt/ros/indigo/bin", @equipment['dut1'].prompt, 10)
   @equipment['dut1'].send_cmd("roscore > roscore.log 2>&1 &", @equipment['dut1'].prompt, 10)
   sleep(20)
   @equipment['dut1'].send_cmd("rosrun roscpp_tutorials talker > ros_talker.log 2>&1 &", \
                                @equipment['dut1'].prompt, 10)
-  @equipment['dut1'].send_cmd("rosrun roscpp_tutorials listener > ros_listener.log 2>&1 &", \
-                               @equipment['dut1'].prompt, 10)
-  sleep(10)
-  @equipment['dut1'].send_cmd("kill $(jobs -p); echo 'Closing Application.'", @equipment['dut1'].prompt, 20)
-  @equipment['dut1'].send_cmd("jobs -p", @equipment['dut1'].prompt, 20)
-  sleep(10)
-  @equipment['dut1'].send_cmd("cat roscore.log && cat ros_talker.log && cat ros_listener.log", \
-                              @equipment['dut1'].prompt, 20)
-  dut_log = @equipment['dut1'].response
-  if @equipment['dut1'].timeout? or !(dut_log =~ Regexp.new("(#{pass_crit})"))
-    raise "Failed to match constraint or test timed out."
+  if listener == "dut"
+    @equipment['dut1'].send_cmd("rosrun roscpp_tutorials listener > ros_listener.log 2>&1 &", \
+                                 @equipment['dut1'].prompt, 10)
+    sleep(10)
+    cleanup_ros_jobs(listener)
+    sleep(10)
+    @equipment['dut1'].send_cmd("cat roscore.log && cat ros_talker.log && cat ros_listener.log", \
+                                @equipment['dut1'].prompt, 20)
+    dut_log = @equipment['dut1'].response
+    if @equipment['dut1'].timeout?
+      raise "Failed due to Time out"
+    elsif !(dut_log =~ Regexp.new("(#{pass_crit})"))
+      raise "Failed to match constraint or test timed out."
+    end
+  else
+    configure_server(dut_ip, server_ip)
+    @equipment['server1'].send_cmd("source /opt/ros/indigo/bin/start-ros.sh > ros_listener.log 2>&1 &", \
+                                  @equipment['server1'].prompt, 10)
+    sleep(10)
+    cleanup_ros_jobs(listener)
+    sleep(10)
+    @equipment['dut1'].send_cmd("cat roscore.log && cat ros_talker.log", @equipment['dut1'].prompt, 20)
+    dut_log = @equipment['dut1'].response
+    @equipment['server1'].send_cmd("cat ros_listener.log", @equipment['server1'].prompt, 20)
+    server_log = @equipment['server1'].response
+    if @equipment['dut1'].timeout?
+      raise "Failed due to Time out"
+    elsif !(server_log =~ /#{pass_crit}/)
+      raise "Failed to match constraint or test timed out."
+    end
   end
 end
 
 # function to configure server path
 def configure_server(dut_ip, server_ip)
-  @equipment['server1'].send_cmd("export ROS_MASTER_URI=http://#{dut_ip}:11311", @equipment['server1'].prompt, 10)
-  @equipment['server1'].send_cmd("export ROS_IP=#{server_ip}", @equipment['server1'].prompt, 10)
-  @equipment['server1'].send_cmd("export ROS_HOSTNAME=#{server_ip}", @equipment['server1'].prompt, 10)
-  @equipment['server1'].send_cmd("/opt/ros/indigo/setup.bash", @equipment['server1'].prompt, 10)
+  @equipment['server1'].send_sudo_cmd("touch /opt/ros/indigo/bin/start-ros.sh", @equipment['server1'].prompt, 10)
+  @equipment['server1'].send_sudo_cmd("chmod 777 /opt/ros/indigo/bin/start-ros.sh", @equipment['server1'].prompt, 10)
+  @equipment['server1'].send_cmd("echo \"export ROS_MASTER_URI=http://#{dut_ip}:11311\" > /opt/ros/indigo/bin/start-ros.sh",\
+                                 @equipment['server1'].prompt, 10)
+  @equipment['server1'].send_cmd("echo \"export ROS_IP=#{server_ip}\" >> /opt/ros/indigo/bin/start-ros.sh", \
+                                @equipment['server1'].prompt, 10)
+  @equipment['server1'].send_cmd("echo \"export ROS_HOSTNAME=#{server_ip}\" >> /opt/ros/indigo/bin/start-ros.sh", \
+                                @equipment['server1'].prompt, 10)
+  @equipment['server1'].send_cmd("echo \"source /opt/ros/indigo/setup.bash\" >> /opt/ros/indigo/bin/start-ros.sh", \
+                                @equipment['server1'].prompt, 10)
+  @equipment['server1'].send_cmd("echo \"cd /opt/ros/indigo/bin\" >> /opt/ros/indigo/bin/start-ros.sh", \
+                                @equipment['server1'].prompt, 10)
+  @equipment['server1'].send_cmd("echo \"rosrun roscpp_tutorials listener\" >> /opt/ros/indigo/bin/start-ros.sh", \
+                               @equipment['server1'].prompt, 10)
+  @equipment['server1'].send_cmd("cat start-ros.sh", @equipment['server1'].prompt, 10)
+end
+
+# function to Clean up ROS jobs
+def cleanup_ros_jobs(listener)
+   @equipment['dut1'].send_cmd("kill $(jobs -p); echo 'Closing Application.'", @equipment['dut1'].prompt, 20)
+   @equipment['dut1'].send_cmd("jobs -p", @equipment['dut1'].prompt, 20)
+   if listener == "server"
+     @equipment['server1'].send_cmd("kill $(jobs -p); echo 'Closing Application.'", @equipment['server1'].prompt, 20)
+     @equipment['server1'].send_cmd("jobs -p", @equipment['server1'].prompt, 20)
+   end
 end
