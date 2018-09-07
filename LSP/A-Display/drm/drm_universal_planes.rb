322248da9478057407e3e8dc3e4534f9af93633f
Alejandro Hernandez <ajhernandez@ti.com>
2018-09-06 22:26:48 -0400

    Add simple test option to universal planes test
    Perform a simple plane check when the number of planes or
    connectors is less or equal to 2.
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>

--- LSP/A-Display/drm/drm_universal_planes.rb
+++ LSP/A-Display/drm/drm_universal_planes.rb
@@ -20,11 +20,6 @@ def run
   res_table = @results_html_file.add_table([["Test String",{:bgcolor => "4863A0"}],
                                             ["Result", {:bgcolor => "4863A0"}],
                                             ["Comment", {:bgcolor => "4863A0"}]])
-  if drm_info['Planes:'].length() < 2 || drm_info['Connectors:'].length() < 2
-    set_result("Not enough Planes (#{drm_info['Planes:'].length}) to validate that planes are universal")
-    return
-  end
-  
   plane_ids = []
   drm_info['Planes:'].each{ |p| plane_ids << p['id'] }
   
@@ -38,42 +33,56 @@ def run
   
   fail_str = "TEST PASSED!!"
   fails = 0
-  connectors.each do |connector|
-    @equipment['dut1'].send_cmd("kmstest -c @#{connector['id']} 2>&1",/press enter to exit/im,30)
-    primary_plane = @equipment['dut1'].response.match(/plane\s*\d+\/@(\d+)\)/im)[1]
-    @equipment['dut1'].send_cmd("",@equipment['dut1'].prompt)
-
-    plane_width = connector['width']/plane_ids.length
-    plane_height = connector['height']/plane_ids.length
-    off_set = [plane_width, plane_height].min
-    test_pls = plane_ids - [primary_plane]
-    x,y=0,0
-    test_string = "kmstest -c @#{connector['id']}"
-    test_pls.each do |t_pid|
-      test_string+=" -p @#{t_pid}:#{x},#{y}-#{plane_width}x#{plane_height}"
-      x+=plane_width
-      if (x + plane_width) >= connector['width']
-        x=0
-        y+=plane_height
-      end
-    end
-    @equipment['dut1'].send_cmd(test_string+ ' 2>&1 &',/press enter to exit/im,30)
-    tout = @equipment['dut1'].timeout?
-    test_response = @equipment['dut1'].response
-    @equipment['dut1'].send_cmd("\nsleep 10; killall -9 kmstest",@equipment['dut1'].prompt,30)
-    test_response += @equipment['dut1'].response
-    tout |= test_response.match(/error|fail/im)
-    if tout
+  if drm_info['Planes:'].length() < 2 || drm_info['Connectors:'].length() < 2
+    puts("2 or fewer planes (#{drm_info['Planes:'].length()}) or connectors (#{drm_info['Connectors:'].length()}) detected performing a simple check")
+    plane_width = connectors[0]['width']/plane_ids.length
+    plane_height = connectors[0]['height']/plane_ids.length
+    if plane_check(plane_ids, plane_width, plane_height, connectors[0]['width'], res_table)
       fails += 1
       fail_str="#{fails}/#{connectors.length} TEST FAILED!!!\r\n #{test_response}"
     end
-    @results_html_file.add_rows_to_table(res_table,[[test_string,
-                                         !tout ? ["Passed",{:bgcolor => "green"}] : 
-                                         ["Failed",{:bgcolor => "red"}],
-                                         test_response]])
+  else
+    connectors.each do |connector|
+      @equipment['dut1'].send_cmd("kmstest -c @#{connector['id']} 2>&1",/press enter to exit/im,30)
+      primary_plane = @equipment['dut1'].response.match(/plane\s*\d+\/@(\d+)\)/im)[1]
+      @equipment['dut1'].send_cmd("",@equipment['dut1'].prompt)
+
+      plane_width = connector['width']/plane_ids.length
+      plane_height = connector['height']/plane_ids.length
+      off_set = [plane_width, plane_height].min
+      test_pls = plane_ids - [primary_plane]
+      if plane_check(test_pls, plane_width, plane_height, connector['width'], res_table, "-c @#{connector['id']}")
+        fails += 1
+        fail_str="#{fails}/#{connectors.length} TEST FAILED!!!\r\n #{test_response}"
+      end
+    end
   end
   set_result(fails > 0 ? FrameworkConstants::Result[:fail] : FrameworkConstants::Result[:pass],fail_str)
    
 
+end
+
+def plane_check(test_pls, plane_width, plane_height, connector_width, res_table, connector_option='')
+  x,y=0,0
+  test_string = "kmstest #{connector_option}"
+  test_pls.each do |t_pid|
+    test_string+=" -p @#{t_pid}:#{x},#{y}-#{plane_width}x#{plane_height}"
+    x+=plane_width
+    if (x + plane_width) >= connector_width
+      x=0
+      y+=plane_height
+    end
+  end
+  @equipment['dut1'].send_cmd(test_string+ ' 2>&1 &',/press enter to exit/im,30)
+  tout = @equipment['dut1'].timeout?
+  test_response = @equipment['dut1'].response
+  @equipment['dut1'].send_cmd("\nsleep 10; killall -9 kmstest",@equipment['dut1'].prompt,30)
+  test_response += @equipment['dut1'].response
+  tout |= test_response.match(/error|fail/im)
+  @results_html_file.add_rows_to_table(res_table,[[test_string,
+                                       !tout ? ["Passed",{:bgcolor => "green"}] : 
+                                       ["Failed",{:bgcolor => "red"}],
+                                       test_response]])
+  tout
 end
 
