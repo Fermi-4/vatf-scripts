50d582d6771dd30e00ff7ce17b6eb1c054e38e3f
Yan Liu <yan-liu@ti.com>
2019-02-04 15:24:22 -0500

    eth_multi.rb: Show the ifconfig before bring it up
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- LSP/A-ETH/eth_multi.rb
+++ LSP/A-ETH/eth_multi.rb
@@ -6,10 +6,10 @@ require File.dirname(__FILE__)+'/../network_utils'
 
 def setup
   super
+  @equipment['dut1'].send_cmd("ifconfig -a", @equipment['dut1'].prompt, 10, false)
   iface = @test_params.params_control.instance_variable_defined?(:@iface) ? @test_params.params_control.iface[0].to_s : 'eth0'
   host_env_name = iface+"_SERVER"
   server_iface = get_local_iface_name(@equipment['server1'],get_ip_address_by_interface('dut1',iface))
   server_ip_address = get_ip_address_by_interface('server1', server_iface)
   @equipment['dut1'].send_cmd("export #{host_env_name}=#{server_ip_address}")
-  @equipment['dut1'].send_cmd("ifconfig -a", @equipment['dut1'].prompt, 10, false)
 end
