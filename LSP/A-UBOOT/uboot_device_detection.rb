90eef5cec0fcf36357ba464b50b998f7afb64c2e
Yan Liu <yan-liu@ti.com>
2019-04-15 11:15:11 -0400

    Uboot_device_detection: Add usb hub and keyboard detection tests
    Add tests to test uboot usb hub with keryboard/msc and/or
    usb keyboard w/o externel hub
    
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- LSP/A-UBOOT/uboot_device_detection.rb
+++ LSP/A-UBOOT/uboot_device_detection.rb
@@ -23,12 +23,19 @@ def run
   result = 0
   result_msg = ''
 
+  # device here kind of represents the setup, ex, usbhubkeyboard
   device = @test_params.params_chan.device[0] if @test_params.params_chan.instance_variable_defined?(:@device) 
   case device.downcase
   when 'usbhost'
-    result,result_msg = check_usbhost_detection()
+    result,result_msg = check_usbhost_detection('usbmsc')
   when 'usbhost3'
-    result,result_msg = check_usbhost_detection('usbhost3')
+    result,result_msg = check_usbhost_detection('usbmsc3')
+  when 'usbhubkeyboard'
+    result,result_msg = check_usbhost_detection('keyboard')
+  when 'usbkeyboard'
+    result,result_msg = check_usbhost_detection('keyboard')
+  when 'usbhubmsc'
+    result,result_msg = check_usbhost_detection('usbmsc')
   when 'sata'
     result,result_msg = check_sata_detection()
   else
@@ -50,20 +57,49 @@ end
 def check_usbhost_detection(type=nil)
   result = 0
   msg = ''
-  i = 0
+  i = 1
   test_loop = 5
-  while i < test_loop
+  while i <= test_loop
     report_msg "Iteration #{i.to_s} ..."
+
+    puts "Checking 'usb reset' output"
+    sleep 1
     @equipment['dut1'].send_cmd("usb reset",@equipment['dut1'].boot_prompt, 20)
-    if ! @equipment['dut1'].response.match(/[1-9]+\s+Storage\s+Device.*found/i)
-      result += 1
-      msg += "No usbmsc device being detected and found in iteration #{i.to_s};"
+    case type
+    when /usbmsc/
+      if ! @equipment['dut1'].response.match(/[1-9]+\s+Storage\s+Device.*found/i)
+        result += 1
+        msg += "No usbmsc device being detected and found in iteration #{i.to_s};"
+      end
+      @equipment['dut1'].send_cmd("usb storage",@equipment['dut1'].boot_prompt, 20)
+      if ! @equipment['dut1'].response.match(/Capacity:/i)
+        result += 1
+        msg += "usb storage does not show storage device in iteration #{i.to_s};"
+      end
     end
+
+    @equipment['dut1'].send_cmd("usb info",@equipment['dut1'].boot_prompt, 20)
+    @equipment['dut1'].send_cmd("usb tree",@equipment['dut1'].boot_prompt, 20)
+    case type
+    when /keyboard/
+      if ! @equipment['dut1'].response.match(/Human\s+Interface/i)
+        result += 1
+        msg += "Keyboard is not detected in iteration #{i.to_s};"
+      end
+    when /usbmsc/
+      if ! @equipment['dut1'].response.match(/Mass\s+Storage/i)
+        result += 1
+        msg += "Mass storage is not detected in iteration #{i.to_s};"
+      end
+    else
+      raise "Not known usb type (keyboard, usbmsc)"
+    end
+
     i += 1
   end
   # check speed
   @equipment['dut1'].send_cmd("usb tree",@equipment['dut1'].boot_prompt, 20)
-  if type == 'usbhost3'
+  if type == 'usbmsc3'
     if ! @equipment['dut1'].response.match(/Mass\s+Storage\s+\(\s*5\s*[GM]b\/s,/im)
       result += 1
       msg += "Did not detect super speed usb storage device"
