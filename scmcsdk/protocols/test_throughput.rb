8ae8948db92a687ec349cec6c10215e70bb0f7cc
Rijwana Mulani <x0288058@ti.com>
2019-05-23 11:18:22 -0400

    protocols: test throughput in dual emac mode
    PTEST-1621:
    test_throughput.rb: Updated existing HSR/PRP test script
    to support throughput verification in dual emac mode.
    
    Signed-off-by: Rijwana Mulani <x0288058@ti.com>
    Signed-off-by: Jonah Willis <j-willis@ti.com>

--- scmcsdk/protocols/test_throughput.rb
+++ scmcsdk/protocols/test_throughput.rb
@@ -30,42 +30,63 @@ def run
   dan_X_1_ip = dan_X_1.params['dut1_if']
   dan_X_2_ip = dan_X_2.params['dut2_if']
 
+  # get pru port list
+  port_list = dan_X_1.params['pru_list']
+
   test_comment = ""
   begin
+    if feature == 'emac'
+       for port in port_list
+         setup_port(port_list, dan_X_1, dan_X_2, dan_X_1_ip, dan_X_2_ip, port)
+         test_throughput(dan_X_1, dan_X_2, [port], dan_X_2_ip, bandwidth, time, rx_usecs, adaptive_rx, cmd_timeout, feature)
+       end
+    else
     # get pruicss port information
-    pruicss_ports = [dan_X_1.params["#{feature}_port1"], dan_X_1.params["#{feature}_port2"]]
-    enable_feature(dan_X_1, feature, cmd, dan_X_1_ip, pruicss_ports)
-    enable_feature(dan_X_2, feature, cmd, dan_X_2_ip, pruicss_ports)
-    ping_status(dan_X_1, dan_X_2_ip)
+      pruicss_ports = [dan_X_1.params["#{feature}_port1"], dan_X_1.params["#{feature}_port2"]]
+      enable_feature(dan_X_1, feature, cmd, dan_X_1_ip, pruicss_ports)
+      enable_feature(dan_X_2, feature, cmd, dan_X_2_ip, pruicss_ports)
+      ping_status(dan_X_1, dan_X_2_ip)
 
-    test_throughput(dan_X_1, dan_X_2, pruicss_ports, dan_X_2_ip, bandwidth, time, rx_usecs, adaptive_rx, cmd_timeout)
+      test_throughput(dan_X_1, dan_X_2, pruicss_ports, dan_X_2_ip, bandwidth, time, rx_usecs, adaptive_rx, cmd_timeout, feature)
 
     # disable feature
-    disable_feature(dan_X_1, feature, pruicss_ports)
-    disable_feature(dan_X_2, feature, pruicss_ports)
+      disable_feature(dan_X_1, feature, pruicss_ports)
+      disable_feature(dan_X_2, feature, pruicss_ports)
 
-    test_comment = "Feature #{feature} verified over interface: #{pruicss_ports}."
+      test_comment = "Feature #{feature} verified over interface: #{pruicss_ports}."
+    end
     set_result(FrameworkConstants::Result[:pass], "Test Passed. #{test_comment}\n\
  Verified throughput for Bandwidth: #{bandwidth} with respect to Time: #{time}.")
-  rescue Exception => e
+ rescue Exception => e
     set_result(FrameworkConstants::Result[:fail], "Test Failed. #{e}")
   end
 end
 
+# function to enable PRU ports
+def setup_port(port_list, dan_X_1, dan_X_2, dan_X_1_ip, dan_X_2_ip, port)
+  for pru_port in port_list
+    dan_X_1.send_cmd("ifconfig #{pru_port} down", dan_X_1.prompt, 10)
+    dan_X_2.send_cmd("ifconfig #{pru_port} down", dan_X_2.prompt, 10)
+  end
+  dan_X_1.send_cmd("ifconfig #{port} #{dan_X_1_ip}", dan_X_1.prompt, 10)
+  dan_X_2.send_cmd("ifconfig #{port} #{dan_X_2_ip}", dan_X_2.prompt, 10)
+end
+
+
 # function to test throughput
-def test_throughput(dan_X_1, dan_X_2, pruicss_ports, dan_X_2_ip, bandwidth, time, rx_usecs, adaptive_rx, cmd_timeout)
-  set_interrupts(dan_X_1, pruicss_ports, cmd_timeout)
-  set_interrupts(dan_X_2, pruicss_ports, cmd_timeout)
+def test_throughput(dan_X_1, dan_X_2, pruicss_ports, dan_X_2_ip, bandwidth, time, rx_usecs, adaptive_rx, cmd_timeout, feature)
+  set_interrupts(dan_X_1, pruicss_ports, cmd_timeout, feature)
+  set_interrupts(dan_X_2, pruicss_ports, cmd_timeout, feature)
   dan_X_2.send_cmd("echo '******** RX-USECS = 0, ADAPTIVE-RX = off ********' > iperf_response.log 2>&1",\
                     dan_X_2.prompt, cmd_timeout)
   dan_X_2.send_cmd("iperf3 -s -i5 >> iperf_response.log 2>&1 &", dan_X_2.prompt, cmd_timeout)
   test_iperf(dan_X_1, dan_X_2, dan_X_2_ip, bandwidth, time, cmd_timeout)
-  set_interrupts(dan_X_1, pruicss_ports, cmd_timeout, rx_usecs)
-  set_interrupts(dan_X_2, pruicss_ports, cmd_timeout, rx_usecs)
+  set_interrupts(dan_X_1, pruicss_ports, cmd_timeout, feature, rx_usecs)
+  set_interrupts(dan_X_2, pruicss_ports, cmd_timeout, feature, rx_usecs)
   dan_X_2.send_cmd("echo '******** RX-USECS = #{rx_usecs}, ADAPTIVE-RX = off ********' >> iperf_response.log 2>&1",\
                     dan_X_2.prompt, cmd_timeout)
   test_iperf(dan_X_1, dan_X_2, dan_X_2_ip, bandwidth, time, cmd_timeout)
-  set_interrupts(dan_X_2, pruicss_ports, cmd_timeout, rx_usecs, adaptive_rx)
+  set_interrupts(dan_X_2, pruicss_ports, cmd_timeout, feature, rx_usecs, adaptive_rx)
   dan_X_2.send_cmd("echo '******** RX-USECS = #{rx_usecs}, ADAPTIVE-RX = #{adaptive_rx}  ********' >> iperf_response.log 2>&1",\
                     dan_X_2.prompt, cmd_timeout)
   test_iperf(dan_X_1, dan_X_2, dan_X_2_ip, bandwidth, time, cmd_timeout)
@@ -74,16 +95,18 @@ def test_throughput(dan_X_1, dan_X_2, pruicss_ports, dan_X_2_ip, bandwidth, time
 end
 
 # function to set rx-usecs and adaptive-rx interrupts
-def set_interrupts(dan_X_n, pruicss_ports, cmd_timeout, rx_usecs = 0, adaptive_rx = 'off')
-  dan_X_n.send_cmd("ethtool -C #{pruicss_ports[0]} rx-usecs #{rx_usecs} adaptive-rx #{adaptive_rx}", dan_X_n.prompt, cmd_timeout)
-  dan_X_n.send_cmd("ethtool -c #{pruicss_ports[0]}", dan_X_n.prompt, cmd_timeout)
-  if !( dan_X_n.response =~ /Adaptive\sRX.\s#{adaptive_rx}[\n\s\S]*rx-usecs.\s#{rx_usecs}/ )
-    raise "Failed to set rx-usecs/adaptive-rx to #{rx_usecs}/#{adaptive_rx} for #{pruicss_ports[0]}."
-  end
-  dan_X_n.send_cmd("ethtool -c #{pruicss_ports[1]}", dan_X_n.prompt, cmd_timeout)
-  if !( dan_X_n.response =~ /Adaptive\sRX.\s#{adaptive_rx}[\n\s\S]*rx-usecs.\s#{rx_usecs}/ )
-    raise "Failed to set rx-usecs/adaptive-rx to #{rx_usecs}/#{adaptive_rx} for #{pruicss_ports[1]}."
-  end
+def set_interrupts(dan_X_n, pruicss_ports, cmd_timeout, feature, rx_usecs = 0, adaptive_rx = 'off')
+    dan_X_n.send_cmd("ethtool -C #{pruicss_ports[0]} rx-usecs #{rx_usecs} adaptive-rx #{adaptive_rx}", dan_X_n.prompt, cmd_timeout)
+    dan_X_n.send_cmd("ethtool -c #{pruicss_ports[0]}", dan_X_n.prompt, cmd_timeout)
+        if !( dan_X_n.response =~ /Adaptive\sRX.\s#{adaptive_rx}[\n\s\S]*rx-usecs.\s#{rx_usecs}/ )
+          raise "Failed to set rx-usecs/adaptive-rx to #{rx_usecs}/#{adaptive_rx} for #{pruicss_ports[0]}."
+	end
+        if feature != 'emac'
+	  dan_X_n.send_cmd("ethtool -c #{pruicss_ports[1]}", dan_X_n.prompt, cmd_timeout)
+          if !( dan_X_n.response =~ /Adaptive\sRX.\s#{adaptive_rx}[\n\s\S]*rx-usecs.\s#{rx_usecs}/ )
+            raise "Failed to set rx-usecs/adaptive-rx to #{rx_usecs}/#{adaptive_rx} for #{pruicss_ports[1]}."
+          end
+        end
 end
 
 # function to run iperf3 for specified range of bandwidth wrt time
