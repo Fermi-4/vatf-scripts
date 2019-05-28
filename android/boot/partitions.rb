eb623b884e5a3bfe8cee8f20e29030d3ed9224bd
Alejandro Hernandez <ajhernandez@ti.com>
2019-05-28 15:27:49 -0400

    android: boot: Update partitions info
    Update partitions info for latest version of uboot
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>

--- android/boot/partitions.rb
+++ android/boot/partitions.rb
@@ -3,7 +3,6 @@ require File.dirname(__FILE__)+'/../android_test_module'
 include AndroidTest
 
 def run
-  android_version = @equipment['dut1'].get_android_version
   partition_map = Hash.new({1 => 'xloader',
                             2 => 'bootloader',
                             3 => 'uboot-env',
@@ -13,10 +12,20 @@ def run
                             7 => 'system',
                             8 => 'vendor',
                             9 => 'userdata'})
+  partition_map['2019.01'] = {
+	1 => 'bootloader',
+	2 => 'tiboot3',
+	3 => 'boot',
+	4 => 'vendor',
+	5 => 'system',
+	6 => 'userdata' 
+  }
   @equipment['dut1'].disconnect('serial')
   @equipment['dut1'].boot_to_bootloader(@android_boot_params)
+  @equipment['dut1'].send_cmd('version', @equipment['dut1'].boot_prompt)
+  uboot_version = /U-Boot\s+([\d\.]+)/.match(@equipment['dut1'].response).captures[0]
   @equipment['dut1'].send_cmd('mmc list', @equipment['dut1'].boot_prompt)
-  emmc_dev = @equipment['dut1'].response.match(/(\d+).*?emmc/i).captures[0]
+  emmc_dev = @equipment['dut1'].response.match(/(\d+)\s*\({0,1}emmc/i).captures[0]
   @equipment['dut1'].send_cmd("part list mmc #{emmc_dev}", @equipment['dut1'].boot_prompt)
   emmc_partitions = @equipment['dut1'].response.scan(/^\s*\d+\s+0x[0-9A-Fa-f]+\s+0x[0-9A-Fa-f]+\s[^\r\n]+/)
   result = {}
@@ -24,8 +33,8 @@ def run
     p_idx, p_name = p_info.match(/(\d+)\s+0x[0-9A-Fa-f]+\s+0x[0-9A-Fa-f]+\s"([^"]+)/).captures
     result[p_idx.to_i] = p_name
   end
-  if partition_map[android_version] != result
-    set_result(FrameworkConstants::Result[:fail], "Expected #{partition_map[android_version]} but found #{result}")
+  if partition_map[uboot_version] != result
+    set_result(FrameworkConstants::Result[:fail], "Expected #{partition_map[uboot_version]} but found #{result}")
   else
     set_result(FrameworkConstants::Result[:pass], "Expected partitions found")
   end
