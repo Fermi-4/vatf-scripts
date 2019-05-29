a79d8309f561311f7f1fe02bf055276d69a400c7
Carlos Hernandez <ceh@ti.com>
2019-05-29 10:28:06 -0400

    LSP: Hypervisor: Update detection string
    Linux 4.19 changed message printed during CPU startup slightly,
    so adjust regex to handle both old and new strings.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- LSP/A-Boot/hypervisor.rb
+++ LSP/A-Boot/hypervisor.rb
@@ -3,7 +3,7 @@ include LspTestScript
 
 def run
 
-    cpu_mode_match = @equipment['dut1'].boot_log.match(/CPU: All CPU.*? started at (\w+)/)
+    cpu_mode_match = @equipment['dut1'].boot_log.match(/CPU: All CPU.*? started (in|at)\s+(\w+)/)
     if (cpu_mode_match)
        if (cpu_mode_match.captures[0] == "EL2")
          set_result(FrameworkConstants::Result[:pass], "CPUs start in EL2 confirming basic HYP mode support")
