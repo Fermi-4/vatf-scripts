Fermi-4/vatf-scripts

commit 6966bcf67c8d56cacde699cce1738e91ccf98ba2

evms_data: Add am654x-evm platform string
Signed-off-by: Carlos Hernandez <ceh@ti.com>

lib/evms_data.rb
@@ -268,6 +268,7 @@ module EvmData
     machines['k2hk-evm'] = {'0.0' => /Machine model:.*Keystone 2 Kepler\/Hawking EVM/, }
     machines['k2e-evm'] = {'0.0' => /Machine model:.*Keystone 2 Edison EVM/, }
     machines['k2l-evm'] = {'0.0' => /Machine model:.*Keystone 2 Lamarr EVM/, }
+    machines['am654x-evm'] = {'0.0' => /Machine model: Texas Instruments AM654 Base Board/. }
                                           
     params.merge!({'dict' => machines})
     get_cmd(params)

commit e952eed3f1e30bb260384e294aebde359b0f3fa3

Fix typo introduced by 6966bcf67
Signed-off-by: Carlos Hernandez <ceh@ti.com>

lib/evms_data.rb
@@ -268,7 +268,7 @@ module EvmData
     machines['k2hk-evm'] = {'0.0' => /Machine model:.*Keystone 2 Kepler\/Hawking EVM/, }
     machines['k2e-evm'] = {'0.0' => /Machine model:.*Keystone 2 Edison EVM/, }
     machines['k2l-evm'] = {'0.0' => /Machine model:.*Keystone 2 Lamarr EVM/, }
-    machines['am654x-evm'] = {'0.0' => /Machine model: Texas Instruments AM654 Base Board/. }
+    machines['am654x-evm'] = {'0.0' => /Machine model: Texas Instruments AM654 Base Board/ }
                                           
     params.merge!({'dict' => machines})
     get_cmd(params)