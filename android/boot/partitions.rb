da4589b63a9106ae28a718009f3dbe00343c992b
Alejandro Hernandez <ajhernandez@ti.com>
2019-05-29 10:40:14 -0400

    android: boot: Make the partitions board specific
    Android's paritition table varies depending on the board
    so make the expected partition table board specific.
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>

--- android/boot/partitions.rb
+++ android/boot/partitions.rb
@@ -3,23 +3,25 @@ require File.dirname(__FILE__)+'/../android_test_module'
 include AndroidTest
 
 def run
-  partition_map = Hash.new({1 => 'xloader',
-                            2 => 'bootloader',
-                            3 => 'uboot-env',
-                            4 => 'misc',
-                            5 => 'recovery',
-                            6 => 'boot',
-                            7 => 'system',
-                            8 => 'vendor',
-                            9 => 'userdata'})
-  partition_map['2019.01'] = {
-	1 => 'bootloader',
-	2 => 'tiboot3',
-	3 => 'boot',
-	4 => 'vendor',
-	5 => 'system',
-	6 => 'userdata' 
+  partition_map = Hash.new(Hash.new({1 => 'xloader',
+									 2 => 'bootloader',
+									 3 => 'uboot-env',
+									 4 => 'misc',
+									 5 => 'recovery',
+									 6 => 'boot',
+									 7 => 'system',
+									 8 => 'vendor',
+									 9 => 'userdata'}))
+  partition_map['am654x-evm']['2019.01'] = {
+								1 => 'bootloader',
+								2 => 'tiboot3',
+								3 => 'boot',
+								4 => 'vendor',
+								5 => 'system',
+								6 => 'userdata' 
   }
+  partition_map['am654x-idk'] = partition_map['am654x-evm']
+  partition_map['am654x-hsevm'] = partition_map['am654x-evm']
   @equipment['dut1'].disconnect('serial')
   @equipment['dut1'].boot_to_bootloader(@android_boot_params)
   @equipment['dut1'].send_cmd('version', @equipment['dut1'].boot_prompt)
@@ -33,8 +35,8 @@ def run
     p_idx, p_name = p_info.match(/(\d+)\s+0x[0-9A-Fa-f]+\s+0x[0-9A-Fa-f]+\s"([^"]+)/).captures
     result[p_idx.to_i] = p_name
   end
-  if partition_map[uboot_version] != result
-    set_result(FrameworkConstants::Result[:fail], "Expected #{partition_map[uboot_version]} but found #{result}")
+  if partition_map[@equipment['dut1'].name][uboot_version] != result
+    set_result(FrameworkConstants::Result[:fail], "Expected #{partition_map[@equipment['dut1'].name][uboot_version]} but found #{result}")
   else
     set_result(FrameworkConstants::Result[:pass], "Expected partitions found")
   end
