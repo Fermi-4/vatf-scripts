0de10e329b2617dc32247bc6a5268e2efc3fbbce
Carlos Hernandez <ceh@ti.com>
2019-06-06 09:40:26 -0400

    LSP: power_functions: Collect top stats
    Collect top statistics to aid debugging failures.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- LSP/A-Power/power_functions.rb
+++ LSP/A-Power/power_functions.rb
@@ -147,7 +147,8 @@ module PowerFunctions
       "cat /sys/kernel/debug/suspend_stats",
       "find /sys/kernel/debug/pm_qos/* -exec echo '{}' \; -exec cat '{}' \;",
       "for s in `ls /sys/devices/system/cpu/cpu#{cpu}/cpuidle/`; do cat /sys/devices/system/cpu/cpu#{cpu}/cpuidle/${s}/time; done",
-      "cat /sys/devices/system/cpu/cpu#{cpu}/cpuidle/state*/usage"
+      "cat /sys/devices/system/cpu/cpu#{cpu}/cpuidle/state*/usage",
+      "top -bn1"
       ].each {|st| 
       @equipment[e].send_cmd("#{st}", @equipment[e].prompt, 3)
       @equipment[e].send_cmd("", @equipment[e].prompt, 1)
